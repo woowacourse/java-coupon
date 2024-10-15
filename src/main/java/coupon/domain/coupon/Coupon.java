d0f2d1f19676660c2e53fa18d8039407d7b50475
김민수 MINSOO KIM <alstn113@gmail.com>
2024-10-15 20:55:02 +0900

    chore: 코드 정렬

--- src/main/java/coupon/domain/coupon/Coupon.java
+++ src/main/java/coupon/domain/coupon/Coupon.java
@@ -12,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
