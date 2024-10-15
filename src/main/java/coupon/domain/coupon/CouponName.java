4ca1ce54dd5db115e43a5c07fcf38577cd9d59b6
김민수 MINSOO KIM <alstn113@gmail.com>
2024-10-15 16:16:39 +0900

    feat: CouponName 작성

---
+++ src/main/java/coupon/domain/coupon/CouponName.java
@@ -0,0 +1,34 @@
+package coupon.domain.coupon;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CouponName {
+
+    private static final int MAX_NAME_LENGTH = 30;
+
+    @Column(name = "coupon_name", nullable = false)
+    private String name;
+
+    public CouponName(String name) {
+        String strippedName = name.strip();
+
+        validateNameLength(strippedName);
+
+        this.name = strippedName;
+    }
+
+    public void validateNameLength(String name) {
+        if (name.isBlank() || name.length() > MAX_NAME_LENGTH) {
+            String message = "쿠폰 이름은 %d자 이하의 비어 있지 않은 문자열이어야 합니다.".formatted(MAX_NAME_LENGTH);
+
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
---
+++ src/test/java/coupon/domain/coupon/CouponNameTest.java
@@ -0,0 +1,46 @@
+package coupon.domain.coupon;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CouponNameTest {
+
+    @Test
+    @DisplayName("쿠폰 이름이 정상 생성된다.")
+    void createCouponName() {
+        // given
+        String name = "쿠폰 이름";
+
+        // when & then
+        assertThatCode(() -> new CouponName(name))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("쿠폰 이름은 앞뒤 공백이 제거 후 생성된다.")
+    void createCouponNameWithStrip() {
+        // given
+        String name = " 쿠폰 이름 ";
+
+        // when
+        CouponName couponName = new CouponName(name);
+
+        // then
+        assertThat(couponName.getName()).isEqualTo("쿠폰 이름");
+    }
+
+    @ParameterizedTest
+    @DisplayName("쿠폰 이름은 공백일 수 없다.")
+    @ValueSource(strings = {"", " "})
+    void couponNameShouldNotBeBlank(String name) {
+        // when & then
+        assertThatCode(() -> new CouponName(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("쿠폰 이름은 30자 이하의 비어 있지 않은 문자열이어야 합니다.");
+    }
+}
