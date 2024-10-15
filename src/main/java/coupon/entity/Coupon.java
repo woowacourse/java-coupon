ddca3039645b4df41f8ce9fee33c8040af061529
HyungHoKim00 <hkim1109@naver.com>
2024-10-15 22:13:57 +0900

    feat: 쿠폰 할인 금액 필드 생성

--- src/main/java/coupon/entity/Coupon.java
+++ src/main/java/coupon/entity/Coupon.java
@@ -6,17 +6,33 @@ import lombok.Getter;
 @Getter
 public class Coupon {
 
-    public static final int MAX_NAME_LENGTH = 30;
-    private final String name;
+    private static final int MAX_NAME_LENGTH = 30;
+    private static final int MIN_DISCOUNT = 1000;
+    private static final int MAX_DISCOUNT = 10000;
 
-    public Coupon(String name) {
-        validateName(name);
+    private final String name;
+    private final int discount;
+
+    public Coupon(String name, int discount) {
+        validate(name, discount);
         this.name = name;
+        this.discount = discount;
+    }
+
+    private void validate(String name, int discount) {
+        validateName(name);
+        validateDiscount(discount);
     }
 
     private void validateName(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new CouponException("name is too long");
+        }
+    }
+
+    private void validateDiscount(int discount) {
+        if (discount < MIN_DISCOUNT || discount > MAX_DISCOUNT) {
+            throw new CouponException("discount is out of range");
         }
     }
 }
--- src/test/java/coupon/entity/CouponTest.java
+++ src/test/java/coupon/entity/CouponTest.java
@@ -5,8 +5,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import coupon.CouponException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CouponTest {
+
+    private String validName = "name";
+    private int validAmount = 5000;
 
     @DisplayName("쿠폰의 이름은 30자 이하여야한다.")
     @Test
@@ -15,7 +20,15 @@ class CouponTest {
         String invalidName = "thisStringHasLengthWhichIs31abc";
 
         // when & then
-        assertThatThrownBy(() -> new Coupon(invalidName))
+        assertThatThrownBy(() -> new Coupon(invalidName, validAmount))
+                .isInstanceOf(CouponException.class);
+    }
+
+    @DisplayName("쿠폰 할인 금액은 1000원 이상 10000원 이하여야 한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {999, 10001})
+    void validateAmount(int amount) {
+        assertThatThrownBy(() -> new Coupon(validName, amount))
                 .isInstanceOf(CouponException.class);
     }
 }
