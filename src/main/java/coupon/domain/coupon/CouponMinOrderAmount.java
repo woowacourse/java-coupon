b2bf4a04209e4d705da77eca854bc0b8ea074135
김민수 MINSOO KIM <alstn113@gmail.com>
2024-10-15 16:24:37 +0900

    feat: CouponMinOrderAmount 작성

---
+++ src/main/java/coupon/domain/coupon/CouponMinOrderAmount.java
@@ -0,0 +1,34 @@
+package coupon.domain.coupon;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CouponMinOrderAmount {
+
+    private static final int MIN_ORDER_AMOUNT_SIZE = 5_000;
+    private static final int MAX_ORDER_AMOUNT_SIZE = 100_000;
+
+    @Column(name = "min_order_amount", nullable = false)
+    private int minOrderAmount;
+
+    public CouponMinOrderAmount(int minOrderAmount) {
+        validateOrderAmountSize(minOrderAmount);
+
+        this.minOrderAmount = minOrderAmount;
+    }
+
+    private void validateOrderAmountSize(int minOrderAmount) {
+        if (minOrderAmount < MIN_ORDER_AMOUNT_SIZE || minOrderAmount > MAX_ORDER_AMOUNT_SIZE) {
+            String message = "주문 최소 금액은 %,d원 이상 %,d원 이하만 가능합니다."
+                    .formatted(MIN_ORDER_AMOUNT_SIZE, MAX_ORDER_AMOUNT_SIZE);
+
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
---
+++ src/test/java/coupon/domain/coupon/CouponMinOrderAmountTest.java
@@ -0,0 +1,32 @@
+package coupon.domain.coupon;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CouponMinOrderAmountTest {
+
+    @Test
+    @DisplayName("쿠폰 최소 주문 금액이 정상 생성된다.")
+    void createCouponMinOrderAmount() {
+        // given
+        int minOrderAmount = 6_000;
+
+        // when & then
+        assertThatCode(() -> new CouponMinOrderAmount(minOrderAmount))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("쿠폰 최소 주문 금액은 5,000원 이상 100,000원 이하이어야 한다.")
+    @ValueSource(ints = {4_999, 100_001})
+    void couponMinOrderAmountShouldBeBetween5_000And100_000(int minOrderAmount) {
+        // when & then
+        assertThatCode(() -> new CouponMinOrderAmount(minOrderAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 최소 금액은 5,000원 이상 100,000원 이하만 가능합니다.");
+    }
+}
