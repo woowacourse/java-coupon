3049df4191acb0ea6227dd5db3660d9b0d76e531
juha <khabh@naver.com>
2024-10-16 08:47:00 +0900

    refactor: 쿠폰 생성 및 조회 로직에 캐시 적용

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -2,26 +2,41 @@ package coupon.service;
 
 import coupon.domain.Coupon;
 import coupon.domain.repository.CouponRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@AllArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class CouponService {
 
     private final CouponRepository couponRepository;
-
-    public CouponService(CouponRepository couponRepository) {
-        this.couponRepository = couponRepository;
-    }
+    private final CacheManager cacheManager;
 
     @Transactional
     public void create(Coupon coupon) {
         couponRepository.save(coupon);
+        getCouponCache().put(coupon.getId(), coupon);
     }
 
     public Coupon getCoupon(Long id) {
+        Coupon coupon = getCouponCache().get(id, Coupon.class);
+        if (coupon != null) {
+            return coupon;
+        }
+
         return couponRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("쿠폰이 존재하지 않습니다."));
+    }
+
+    private Cache getCouponCache() {
+        Cache cache = cacheManager.getCache("coupons");
+        if (cache == null) {
+            throw new IllegalStateException("쿠폰 캐시를 찾을 수 없습니다.");
+        }
+        return cache;
     }
 }
