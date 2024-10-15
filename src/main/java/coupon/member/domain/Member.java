e38a84c7e04736d4dd0776aa2cb42e9da50ab8c5
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-10-15 22:25:43 +0900

    feat: 사용자 구현

---
+++ src/main/java/coupon/member/domain/Member.java
@@ -0,0 +1,35 @@
+package coupon.member.domain;
+
+import coupon.util.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String name;
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Member(String name) {
+        this(null, name);
+    }
+}
