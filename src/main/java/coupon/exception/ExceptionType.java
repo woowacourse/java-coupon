350c2ead6e3ed68f1509271c09b3cd93ccd38bb8
Minjoo <m0522inju@gmail.com>
2024-10-16 00:49:22 +0900

    feat: 쿠폰 이름 클래스 생성

--- src/main/java/coupon/exception/ExceptionType.java
+++ src/main/java/coupon/exception/ExceptionType.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ExceptionType {
 
-    ;
+    COUPON_NAME_BLANK(HttpStatus.BAD_REQUEST, "쿠폰 이름을 입력해주세요."),
+    COUPON_NAME_LENGTH_EXCEED(HttpStatus.BAD_REQUEST, "쿠폰 이름은 30자 이하여야합니다.");
 
     private final HttpStatus status;
     private final String message;
