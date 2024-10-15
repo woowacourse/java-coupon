3396ef8151089ff0df975c6e14027a7635e10bb5
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-10-16 00:52:41 +0900

    feat: 외래키 명시

--- src/main/java/coupon/coupon/domain/MemberCoupon.java
+++ src/main/java/coupon/coupon/domain/MemberCoupon.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
@@ -27,9 +28,11 @@ public class MemberCoupon extends BaseEntity {
     private Long id;
 
     @ManyToOne(optional = false)
+    @JoinColumn(name = "member_id")
     private Member member;
 
     @ManyToOne(optional = false)
+    @JoinColumn(name = "coupon_id")
     private Coupon coupon;
 
     @Column(nullable = false)
