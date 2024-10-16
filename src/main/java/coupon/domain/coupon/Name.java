5a8ae33f8ec0bad1fed3442a6d261ad9420efcc3
임수빈 <robin980108@naver.com>
2024-10-17 01:40:34 +0900

    feat: 이름 제한 추가

--- src/main/java/coupon/domain/coupon/Name.java
+++ src/main/java/coupon/domain/coupon/Name.java
@@ -1,5 +1,6 @@
 package coupon.domain.coupon;
 
+import coupon.domain.coupon.exception.InvalidCouponNameException;
 import lombok.Getter;
 
 @Getter
@@ -8,6 +9,9 @@ public class Name {
     private final String value;
 
     public Name(String value) {
+        if (value == null || value.length() > 30) {
+            throw new InvalidCouponNameException("쿠폰 이름은 반드시 존재해야 하고 30자 이하여야 합니다.");
+        }
         this.value = value;
     }
 
