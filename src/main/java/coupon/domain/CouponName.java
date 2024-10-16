9aee903ccbe07f9ac537d25249bfa2891b50c864
Seokmyung Ham <gkatjraud1@gmail.com>
2024-10-17 02:56:00 +0900

    feat: CouponName 원시값 포장 객체 생성

---
+++ src/main/java/coupon/domain/CouponName.java
@@ -0,0 +1,38 @@
+package coupon.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CouponName {
+
+    private static final int MAX_NAME_LENGTH = 30;
+
+    private String name;
+
+    public CouponName(String name) {
+        validateCouponName(name);
+        this.name = name;
+    }
+
+    private void validateCouponName(String name) {
+        validatePresence(name);
+        validateNameLength(name);
+    }
+
+    private void validatePresence(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("쿠폰 이름은 반드시 존재해야 합니다.");
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("쿠폰 이름 길이는 최대 30자 이하여야 합니다.");
+        }
+    }
+}
