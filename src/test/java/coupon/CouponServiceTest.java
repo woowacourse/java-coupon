ee6cac33f4f961b08d1e80f99edb6b6869f451cd
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-10-17 03:02:47 +0900

    test: 복제 지연 테스트

--- src/test/java/coupon/CouponServiceTest.java
+++ src/test/java/coupon/CouponServiceTest.java
@@ -1,21 +1,25 @@
 package coupon;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import coupon.application.CouponService;
+import coupon.domain.Coupon;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class CouponServiceTest {
+class CouponServiceTest {
 
     @Autowired
     private CouponService couponService;
 
     @Test
     void 복제지연테스트() {
-//        Coupon coupon = new Coupon(1000, 10000);
-//        couponService.create(coupon);
-//        Coupon savedCoupon = couponService.getCoupon(coupon.getId());
-//        assertThat(savedCoupon).isNotNull();
+        Coupon coupon = new Coupon("CouponName", 1_000, 10_000, "가구", LocalDate.now(), LocalDate.now());
+        couponService.create(coupon);
+        Coupon savedCoupon = couponService.getCoupon(coupon.getId());
+        assertThat(savedCoupon).isNotNull();
     }
 }
