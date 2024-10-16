bff7a5c131b75b1590688bddfd75b2d9af16437d
juha <khabh@naver.com>
2024-10-16 09:26:19 +0900

    test: 쿠폰 서비스 기능 테스트

--- src/test/java/coupon/service/CouponServiceTest.java
+++ src/test/java/coupon/service/CouponServiceTest.java
@@ -2,9 +2,11 @@ package coupon.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import coupon.domain.Category;
 import coupon.domain.Coupon;
+import coupon.domain.repository.CouponRepository;
 import java.time.LocalDate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,11 +22,53 @@ class CouponServiceTest {
     private CouponService couponService;
 
     @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
     private CacheManager cacheManager;
 
     @AfterEach
     void tearDown() {
         cacheManager.getCache("coupons").clear();
+        couponRepository.deleteAll();
+    }
+
+    @DisplayName("쿠폰을 저장한다.")
+    @Test
+    void create() {
+        LocalDate now = LocalDate.now();
+        Coupon coupon = new Coupon("coupon", 1000, 10000, now, now, Category.FASHION);
+
+        couponService.create(coupon);
+
+        Coupon savedCoupon = couponService.getCoupon(coupon.getId());
+        assertAll(
+                () -> assertThat(savedCoupon.getName()).isEqualTo("coupon"),
+                () -> assertThat(savedCoupon.getDiscount()).isEqualTo(1000),
+                () -> assertThat(savedCoupon.getMinAmount()).isEqualTo(10000),
+                () -> assertThat(savedCoupon.getStartDate()).isEqualTo(now),
+                () -> assertThat(savedCoupon.getEndDate()).isEqualTo(now)
+        );
+    }
+
+    @DisplayName("쿠폰을 찾을 수 없으면 예외가 발생한다.")
+    @Test
+    void couponNotFound() {
+        assertThatThrownBy(() -> couponService.getCoupon(1L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("쿠폰이 존재하지 않습니다.");
+    }
+
+    @DisplayName("쿠폰을 조회한다.")
+    @Test
+    void getCoupon() {
+        LocalDate now = LocalDate.now();
+        Coupon coupon = new Coupon("coupon", 1000, 10000, now, now, Category.FASHION);
+        couponService.create(coupon);
+
+        Coupon foundCoupon = couponService.getCoupon(coupon.getId());
+
+        assertThat(foundCoupon).isNotNull();
     }
 
     @DisplayName("캐시가 없는 경우 복제 지연으로 인해 쿠폰을 조회하지 못한다.")
@@ -44,7 +88,9 @@ class CouponServiceTest {
     void replicationDelay() {
         Coupon coupon = new Coupon("coupon", 1000, 10000, LocalDate.now(), LocalDate.now(), Category.FOOD);
         couponService.create(coupon);
+
         Coupon savedCoupon = couponService.getCoupon(coupon.getId());
+
         assertThat(savedCoupon).isNotNull();
     }
 }
