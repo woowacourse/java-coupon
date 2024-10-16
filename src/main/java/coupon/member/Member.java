95ecb817dc9754a9e0d8360fd78f911844a2e4e2
eunjungL <dldmswjd322@gmail.com>
2024-10-17 00:59:48 +0900

    feat: Member, MemberCoupon 도메인 구현

---
+++ src/main/java/coupon/member/Member.java
@@ -0,0 +1,27 @@
+package coupon.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Member(String name) {
+        this(null, name);
+    }
+}
