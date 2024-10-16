b159add16545d25ee78a92a61deb64b723c59419
tsulocalize <tsulocalizing@gmail.com>
2024-10-16 13:23:48 +0900

    refactor: config 오류 수정

--- src/main/java/coupon/config/DataSourceConfig.java
+++ src/main/java/coupon/config/DataSourceConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -15,13 +16,13 @@ import java.util.Map;
 public class DataSourceConfig {
 
     @Bean
-    @ConfigurationProperties("spring.coupon.reader")
+    @ConfigurationProperties("coupon.datasource.reader")
     public DataSource readDataSource() {
         return DataSourceBuilder.create().type(HikariDataSource.class).build();
     }
 
     @Bean
-    @ConfigurationProperties("spring.coupon.writer")
+    @ConfigurationProperties("coupon.datasource.writer")
     public DataSource writeDataSource() {
         return DataSourceBuilder.create().type(HikariDataSource.class).build();
     }
@@ -32,14 +33,18 @@ public class DataSourceConfig {
         DataSourceRouter router = new DataSourceRouter();
 
         Map<Object, Object> dataSources = new HashMap<>();
-        dataSources.put("reader", readDataSource());
-        dataSources.put("writer", writeDataSource());
+        DataSource reader = readDataSource();
+        DataSource writer = writeDataSource();
+        dataSources.put("reader", reader);
+        dataSources.put("writer", writer);
         router.setTargetDataSources(dataSources);
+        router.setDefaultTargetDataSource(writer);
 
         return router;
     }
 
     @Bean
+    @Primary
     @DependsOn("routeDataSource")
     public DataSource dataSource() {
         return new LazyConnectionDataSourceProxy(routeDataSource());
