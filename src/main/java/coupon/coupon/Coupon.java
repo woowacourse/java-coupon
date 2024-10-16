8d825fa7f964c2b9bc31367d098dce0d832635be
eunjungL <dldmswjd322@gmail.com>
2024-10-17 00:50:30 +0900

    feat: Coupon 도메인 구현

---
+++ src/main/java/coupon/coupon/Coupon.java
@@ -0,0 +1,123 @@
+package coupon.coupon;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer discountAmount;
+
+    @Column(nullable = false)
+    private Integer discountRate;
+
+    @Column(nullable = false)
+    private Integer minOrderAmount;
+
+    @Column(nullable = false)
+    private LocalDateTime startedAt;
+
+    @Column(nullable = false)
+    private LocalDateTime endedAt;
+
+    private Coupon(
+            Long id,
+            String name,
+            Integer discountAmount,
+            Integer discountRate,
+            Integer minOrderAmount,
+            LocalDateTime startedAt,
+            LocalDateTime endedAt
+    ) {
+        validate(name, discountAmount, discountRate, minOrderAmount, startedAt, endedAt);
+        this.id = id;
+        this.name = name;
+        this.discountAmount = discountAmount;
+        this.discountRate = discountRate;
+        this.minOrderAmount = minOrderAmount;
+        this.startedAt = startedAt;
+        this.endedAt = endedAt;
+    }
+
+    private void validate(
+            String name,
+            Integer discountAmount,
+            Integer discountRate,
+            Integer minOrderAmount,
+            LocalDateTime startedAt,
+            LocalDateTime endedAt
+    ) {
+        validateName(name);
+        validateDiscountAmount(discountAmount);
+        validateDiscountRate(discountRate);
+        validateMinOrderAmount(minOrderAmount);
+        validateIssuedPeriod(startedAt, endedAt);
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 30) {
+            throw new IllegalArgumentException("쿠폰의 이름은 30자 이하여야 합니다.");
+        }
+    }
+
+    private void validateDiscountAmount(Integer discountAmount) {
+        if (discountAmount < 1000 || 10000 < discountAmount) {
+            throw new IllegalArgumentException("쿠폰의 할인 금액은 1000원 이상 10000원 이하여야 합니다.");
+        }
+
+        if (discountAmount % 500 != 0) {
+            throw new IllegalArgumentException("쿠폰의 할인 금액은 500원 단위로 설정해야 합니다.");
+        }
+    }
+
+    private void validateDiscountRate(Integer discountRate) {
+        if (discountRate < 3 || 20 < discountRate) {
+            throw new IllegalArgumentException("쿠폰의 할인율은 3% 이상 20% 이하여야 합니다.");
+        }
+    }
+
+    private void validateMinOrderAmount(Integer minOrderAmount) {
+        if (minOrderAmount < 5000 || 100000 < minOrderAmount) {
+            throw new IllegalArgumentException("쿠폰의 최소 주문 금액은 5000원 이상 100000원 이하여야 합니다.");
+        }
+    }
+
+    private void validateIssuedPeriod(LocalDateTime startedAt, LocalDateTime endedAt) {
+        if (endedAt.isBefore(startedAt)) {
+            throw new IllegalArgumentException("쿠폰의 종료일은 시작일보다 이후여야 합니다.");
+        }
+    }
+
+    public Coupon(
+            String name,
+            Integer discountAmount,
+            Integer minOrderAmount,
+            LocalDateTime startedAt,
+            LocalDateTime endedAt
+    ) {
+        this(
+                null,
+                name,
+                discountAmount,
+                Math.round((float) discountAmount / minOrderAmount),
+                minOrderAmount,
+                startedAt,
+                endedAt
+        );
+    }
+}
---
+++ src/main/java/coupon/coupon/CouponCategory.java
@@ -0,0 +1,16 @@
+package coupon.coupon;
+
+public enum CouponCategory {
+
+    FASHION("패션"),
+    FURNITURE("가전"),
+    APPLIANCE("가구"),
+    FOOD("식품"),
+    ;
+
+    private final String category;
+
+    CouponCategory(String category) {
+        this.category = category;
+    }
+}
