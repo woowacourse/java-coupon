7578d4601293e3e52176f2f220815fe674a1fe69
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-10-17 14:38:30 +0900

    refactor(WriterTransactionalAspect): ThreadLocal clear 추

--- src/main/java/aspect/WriterTransactionalAspect.java
+++ src/main/java/aspect/WriterTransactionalAspect.java
@@ -2,6 +2,8 @@ package aspect;
 
 import coupon.config.DataSourceContextHolder;
 import coupon.config.DataSourceType;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,5 +15,11 @@ public class WriterTransactionalAspect {
     @Before("@annotation(aspect.WriterTransactional)")
     public void switchToWriterDB() {
         DataSourceContextHolder.setDataSourceType(DataSourceType.WRITER);
+    }
+
+    @AfterReturning("@annotation(aspect.WriterTransactional)")
+    @AfterThrowing("@annotation(aspect.WriterTransactional)")
+    public void clearDataSourceAfterExecution() {
+        DataSourceContextHolder.clear();
     }
 }
