7bcabe942953f1bed2b46b1c636301375287030f
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-17 22:17:52 +0900

    feat(Member): Member 엔티티 구현

---
+++ src/main/java/coupon/domain/Member.java
@@ -0,0 +1,25 @@
+package coupon.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
