5417cf79298c6f6eb987e3297889014472c2e307
libienz <leekh10195@gmail.com>
2024-10-17 11:04:44 +0900

    feat: CouponException 작성

---
+++ src/main/java/coupon/coupon/exception/CouponException.java
@@ -0,0 +1,11 @@
+package coupon.coupon.exception;
+
+public class CouponException extends RuntimeException {
+    public CouponException(String message) {
+        super(message);
+    }
+
+    public CouponException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
