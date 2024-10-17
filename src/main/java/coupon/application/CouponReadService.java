97eac2bbf2bbab0abaf9e37ed12964ee2b8b458e
yoonseo choi <choiyoonseo02@gmail.com>
2024-10-17 21:51:55 +0900

    feat: 캐싱 설정

--- src/main/java/coupon/application/CouponReadService.java
+++ src/main/java/coupon/application/CouponReadService.java
@@ -1,9 +1,22 @@
 package coupon.application;
 
+import coupon.domain.coupon.Coupon;
+import coupon.domain.coupon.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class CouponReadService {
+
+    private final CouponRepository couponRepository;
+
+    @Cacheable(cacheNames = "coupon", key = "#id")
+    public Coupon getCoupon(Long id) {
+        return couponRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 쿠폰입니다."));
+    }
 }
--- src/main/java/coupon/application/CouponWriteService.java
+++ src/main/java/coupon/application/CouponWriteService.java
@@ -1,9 +1,23 @@
 package coupon.application;
 
+import coupon.domain.coupon.Coupon;
+import coupon.domain.coupon.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class CouponWriteService {
+
+    private final CouponRepository couponRepository;
+
+    @CachePut(cacheNames = "coupon", key = "#coupon.id")
+    @CacheEvict(cacheNames = "coupon", key = "'all'")
+    public Coupon createCoupon(Coupon coupon) {
+        return couponRepository.save(coupon);
+    }
 }
