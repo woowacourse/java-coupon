4e0bbce03cf4d2ca2d476ea7cc357f2cc6d0294b
Gyeongho Yang <chchch1213@naver.com>
2024-10-18 02:30:00 +0900

    refactor: remove validation on member coupon

--- src/main/java/coupon/domain/MemberCoupon.java
+++ src/main/java/coupon/domain/MemberCoupon.java
@@ -5,15 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
-import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.Set;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,14 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberCoupon {
 
-    private static final Validator validator;
     private static final int DEFAULT_AVAILABLE_DAYS = 7;
-
-    static {
-        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
-            validator = factory.getValidator();
-        }
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,7 +30,7 @@ public class MemberCoupon {
     @ManyToOne
     private Member member;
 
-    private boolean used;
+    private boolean used = false;
 
     private LocalDateTime issuedAt;
 
@@ -56,8 +42,6 @@ public class MemberCoupon {
         this.used = false;
         this.issuedAt = LocalDateTime.now();
         this.expiresAt = calculateExpiresAt(this.issuedAt);
-
-        validate();
     }
 
     private LocalDateTime calculateExpiresAt(LocalDateTime issuedAt) {
@@ -65,17 +49,5 @@ public class MemberCoupon {
                 .plusDays(DEFAULT_AVAILABLE_DAYS)
                 .atStartOfDay()
                 .minusNanos(1);
-    }
-
-    @AssertTrue(message = "발급일은 만료일보다 이전이어야 합니다.")
-    public boolean validateIssuedAt() {
-        return issuedAt.isBefore(expiresAt);
-    }
-
-    private void validate() {
-        Set<ConstraintViolation<MemberCoupon>> violations = validator.validate(this);
-        if (!violations.isEmpty()) {
-            throw new ConstraintViolationException(violations);
-        }
     }
 }
