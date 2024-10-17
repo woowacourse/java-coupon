ab5afbdd53ef62bbb36c36f03dba50ea7a734a27
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-10-17 12:07:03 +0900

    refactor: 예외가 Http 상태를 가지도록 변경

--- src/main/java/coupon/advice/BadRequestException.java
+++ src/main/java/coupon/advice/BadRequestException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class BadRequestException extends CustomException {
 
-    public BadRequestException(String message, HttpStatus status) {
-        super(message, status);
+    public BadRequestException(String message) {
+        super(message, HttpStatus.BAD_REQUEST);
     }
 }
