8c9f9df60eda697d4faa3a80a8a475fbd04dcb47
fromitive <fromitive@gmail.com>
2024-10-18 03:51:08 +0900

    feat: 쿠폰 테스트 환경 구축

---
+++ src/main/java/coupon/service/CouponService.java
@@ -0,0 +1,29 @@
+package coupon.service;
+
+import coupon.domain.Coupon;
+import coupon.repository.CouponEntity;
+import coupon.repository.CouponRepository;
+import java.util.NoSuchElementException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Transactional
+@Service
+public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    public Long create(Coupon coupon) {
+        CouponEntity savedCoupon = new CouponEntity(coupon);
+        couponRepository.save(savedCoupon);
+        return savedCoupon.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public CouponEntity getCoupon(Long id) {
+        return couponRepository.findById(id)
+                .orElseThrow(NoSuchElementException::new);
+    }
+}
