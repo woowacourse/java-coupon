0c71f4b56082117013bb5bf48bd02db7d1288300
le2sky <leehaneul990623@gmail.com>
2024-10-17 21:30:29 +0900

    feat: coupon apply vo 구현

---
+++ src/main/java/coupon/CouponDiscountApply.java
@@ -0,0 +1,46 @@
+package coupon;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+class CouponDiscountApply {
+
+    private static final BigDecimal MAXIMUM_DISCOUNT_PERCENT = new BigDecimal("20");
+    private static final BigDecimal MINIMUM_DISCOUNT_PERCENT = new BigDecimal("3");
+
+    private CouponDiscountAmount discountAmount;
+    private CouponApplicableAmount applicableAmount;
+
+    public CouponDiscountApply(BigDecimal discountAmount, BigDecimal applicableAmount) {
+        validateDiscountPercent(discountAmount, applicableAmount);
+
+        this.discountAmount = new CouponDiscountAmount(discountAmount);
+        this.applicableAmount = new CouponApplicableAmount(applicableAmount);
+    }
+
+    private void validateDiscountPercent(BigDecimal discountAmount, BigDecimal applicableAmount) {
+        BigDecimal discountPercent = calculateDiscountPercent(discountAmount, applicableAmount);
+        validateMinimumPercent(discountPercent);
+        validateMaximumPercent(discountPercent);
+    }
+
+    private BigDecimal calculateDiscountPercent(BigDecimal discountAmount, BigDecimal applicableAmount) {
+        return discountAmount
+                .divide(applicableAmount, 3, RoundingMode.FLOOR)
+                .multiply(new BigDecimal(100));
+    }
+
+    private void validateMinimumPercent(BigDecimal amount) {
+        if (amount.compareTo(MINIMUM_DISCOUNT_PERCENT) < 0) {
+            String message = "할인율은 " + MINIMUM_DISCOUNT_PERCENT + "% 이상이어야 합니다.";
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private void validateMaximumPercent(BigDecimal amount) {
+        if (amount.compareTo(MAXIMUM_DISCOUNT_PERCENT) > 0) {
+            String message = "할인율은 " + MAXIMUM_DISCOUNT_PERCENT + "% 이하여야 합니다.";
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
---
+++ src/test/java/coupon/CouponDiscountApplyTest.java
@@ -0,0 +1,43 @@
+package coupon;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CouponDiscountApplyTest {
+
+    @Test
+    @DisplayName("쿠폰 할인 적용 객체를 생성한다.")
+    void create() {
+        BigDecimal discountAmount = new BigDecimal(1000L);
+        BigDecimal applicableAmount = new BigDecimal(10000L);
+
+        assertThatCode(() -> new CouponDiscountApply(discountAmount, applicableAmount))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("할인율은 3% 이상이어야 한다.")
+    void minimumDiscountPercent() {
+        BigDecimal discountAmount = new BigDecimal(1000L);
+        BigDecimal applicableAmount = new BigDecimal(40000L);
+
+        assertThatThrownBy(() -> new CouponDiscountApply(discountAmount, applicableAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("할인율은 3% 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("할인율은 20% 이하이어야 한다.")
+    void maximumDiscountPercent() {
+        BigDecimal discountAmount = new BigDecimal(8000L);
+        BigDecimal applicableAmount = new BigDecimal(38000L);
+
+        assertThatThrownBy(() -> new CouponDiscountApply(discountAmount, applicableAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("할인율은 20% 이하여야 합니다.");
+    }
+}
