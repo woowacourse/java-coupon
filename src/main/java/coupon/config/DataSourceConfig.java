44339fcbadd82854f55349e946a820b983789b83
hjk0761 <hjk0761@naver.com>
2024-10-17 23:40:54 +0900

    feat: DataSource 설정 구현

---
+++ src/main/java/coupon/config/DataSourceConfig.java
@@ -0,0 +1,55 @@
+package coupon.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "coupon.datasource.writer")
+    public DataSource writeDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "coupon.datasource.reader")
+    public DataSource readDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    @DependsOn({"writeDataSource", "readDataSource"})
+    public DataSource routeDataSource(@Qualifier("writeDataSource") DataSource writeDataSource, @Qualifier("readDataSource") DataSource readDataSource) {
+        DataSourceRouter dataSourceRouter = new DataSourceRouter();
+        HashMap<Object, Object> dataSourceMap = new HashMap<>();
+
+        dataSourceMap.put(DataSourceType.WRITE, writeDataSource);
+        dataSourceMap.put(DataSourceType.READ, readDataSource);
+
+        dataSourceRouter.setTargetDataSources(dataSourceMap);
+        dataSourceRouter.setDefaultTargetDataSource(writeDataSource);
+        return dataSourceRouter;
+    }
+
+    @DependsOn({"routeDataSource"})
+    @Primary
+    @Bean
+    public DataSource dataSource(DataSource routeDataSource) {
+        return new LazyConnectionDataSourceProxy(routeDataSource);
+    }
+}
