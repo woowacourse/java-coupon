0f68601eb4bcd931ad3b9e4d6a1aae499260b2b8
H <dmsgktn0208@gmail.com>
2024-10-18 02:59:45 +0900

    refactor: id 전략, 부생성자 추가

--- src/main/java/coupon/coupon/domain/Coupon.java
+++ src/main/java/coupon/coupon/domain/Coupon.java
@@ -5,18 +5,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Coupon {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Embedded
@@ -32,8 +35,24 @@ public class Coupon {
     private OrderPrice orderPrice;
 
     @Enumerated(value = EnumType.STRING)
-    private CouponCategory couponCategory;
+    private CouponCategory category;
 
     @Embedded
     private IssuePeriod issuePeriod;
+
+    public Coupon(
+            CouponName name,
+            DiscountAmount discountAmount,
+            DiscountRate discountRate,
+            OrderPrice orderPrice,
+            CouponCategory category,
+            IssuePeriod issuePeriod
+    ) {
+        this.name = name;
+        this.discountAmount = discountAmount;
+        this.discountRate = discountRate;
+        this.orderPrice = orderPrice;
+        this.category = category;
+        this.issuePeriod = issuePeriod;
+    }
 }
