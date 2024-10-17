f70ac1194aac03c347a617594e0b185f4ba53027
BurningFalls <sherlocky31415@gmail.com>
2024-10-18 02:31:31 +0900

    feat: 멤버 도메인 설계

---
+++ src/main/java/coupon/member/domain/Member.java
@@ -0,0 +1,29 @@
+package coupon.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
