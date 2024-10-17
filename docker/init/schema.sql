acf44755569aeee66dcf01ff5163ebc4fae7bc7d
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-10-17 18:16:17 +0900

    chore: 도커 컴포즈 실행 시 스키마 자동 생성

--- docker/init/schema.sql
+++ docker/init/schema.sql
@@ -8,8 +8,8 @@ create table if not exists coupon
     discount_amount     DECIMAL(10, 2) NOT NULL,
     minimum_order_price DECIMAL(10, 2) NOT NULL,
     coupon_category     VARCHAR(50)    NOT NULL,
-    issue_started_at    TIMESTAMP      NOT NULL,
-    issue_ended_at      TIMESTAMP      NOT NULL
+    issue_started_at    DATETIME      NOT NULL,
+    issue_ended_at      DATETIME      NOT NULL
 ) engine = InnoDB;
 
 create table if not exists member_coupon
@@ -18,8 +18,8 @@ create table if not exists member_coupon
     coupon_id BIGINT    NOT NULL,
     member_id BIGINT    NOT NULL,
     used      BOOLEAN   NOT NULL,
-    issued_at TIMESTAMP NOT NULL,
-    expire_at TIMESTAMP NOT NULL,
+    issued_at DATETIME NOT NULL,
+    expire_at DATETIME NOT NULL,
     CONSTRAINT fk_coupon FOREIGN KEY (coupon_id) REFERENCES coupon (id) ON DELETE CASCADE
 ) engine = InnoDB;
 
