33cd8de33c82d20c9c75306dcc501bee352d0ac4
le2sky <leehaneul990623@gmail.com>
2024-10-17 19:38:47 +0900

    refactor: 상수 인라인화

--- src/main/java/coupon/MinimumApplicableMoney.java
+++ src/main/java/coupon/MinimumApplicableMoney.java
@@ -6,23 +6,17 @@ record MinimumApplicableMoney(BigDecimal value) {
 
     private static final BigDecimal MAXIMUM_APPLICABLE_MONEY = new BigDecimal("100000");
     private static final BigDecimal MINIMUM_APPLICABLE_MONEY = new BigDecimal("5000");
-    private static final String MAXIMUM_LIMIT_MESSAGE = String.format(
-            "해당 쿠폰의 최대 적용 가능 금액은 %s원 이하입니다.",
-            MAXIMUM_APPLICABLE_MONEY
-    );
-    private static final String MINIMUM_LIMIT_MESSAGE = String.format(
-            "해당 쿠폰의 최소 적용 가능 금액은 %s원 이상입니다.",
-            MINIMUM_APPLICABLE_MONEY
-
-    );
 
     MinimumApplicableMoney {
         if (value.compareTo(MINIMUM_APPLICABLE_MONEY) < 0) {
-            throw new IllegalArgumentException(MINIMUM_LIMIT_MESSAGE);
+            String message = "해당 쿠폰의 최소 적용 가능 금액은 " + MINIMUM_APPLICABLE_MONEY + "원 이상입니다.";
+            throw new IllegalArgumentException(message);
+
         }
 
         if (value.compareTo(MAXIMUM_APPLICABLE_MONEY) > 0) {
-            throw new IllegalArgumentException(MAXIMUM_LIMIT_MESSAGE);
+            String message = "해당 쿠폰의 최대 적용 가능 금액은 " + MAXIMUM_APPLICABLE_MONEY + "원 이하입니다.";
+            throw new IllegalArgumentException(message);
         }
     }
 }
