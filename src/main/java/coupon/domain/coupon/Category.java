bcdb013da47960ffa284e956272c246e83641494
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-10-18 02:26:52 +0900

    feat: Category 구현

--- src/main/java/coupon/domain/coupon/Category.java
+++ src/main/java/coupon/domain/coupon/Category.java
@@ -1,4 +1,18 @@
 package coupon.domain.coupon;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
+    FASHION("패션"),
+    FURNITURE("가구"),
+    HOME_APPLIANCE("가전"),
+    FOOD("식품"),
+    ;
+
+    private final String typeName;
+
+    Category(String typeName) {
+        this.typeName = typeName;
+    }
 }
