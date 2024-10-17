64971457540ed77d532522382e1c0f0842d7972a
BurningFalls <sherlocky31415@gmail.com>
2024-10-18 01:20:38 +0900

    feat: 카테고리 클래스 생성

---
+++ src/test/java/coupon/domain/CouponCategoryTest.java
@@ -0,0 +1,30 @@
+package coupon.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+public class CouponCategoryTest {
+
+    @DisplayName("카테고리를 가져올 수 있다.")
+    @EnumSource(CouponCategory.class)
+    @ParameterizedTest
+    void getCategoryTest(CouponCategory category) {
+        // given & when & then
+        assertDoesNotThrow(() -> CouponCategory.getCategory(category.name()));
+    }
+
+    @DisplayName("")
+    @Test
+    void getCategoryTest_WhenWrongCategory() {
+        // given
+        String wrongCategory = "ANIMAL";
+        assertThatThrownBy(() -> CouponCategory.getCategory(wrongCategory))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("일치하는 카테고리가 없습니다.");
+    }
+}
