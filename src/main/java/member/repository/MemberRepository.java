62a8f4fe65bccb2abff57de6a7540161edcf1605
fromitive <fromitive@gmail.com>
2024-10-18 03:50:53 +0900

    refactor: 불필요한 파일 제거

--- src/main/java/member/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-}
