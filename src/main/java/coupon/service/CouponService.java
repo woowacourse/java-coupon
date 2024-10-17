d6ca674dc455123eeae39a09c3112307c61487d5
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-10-18 05:05:26 +0900

    feat: repository, service 구현

---
+++ src/main/java/coupon/service/CouponService.java
@@ -0,0 +1,27 @@
+package coupon.service;
+
+import coupon.domain.coupon.Coupon;
+import coupon.repository.CouponRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    public CouponService(CouponRepository couponRepository) {
+        this.couponRepository = couponRepository;
+    }
+
+    @Transactional
+    public void create(Coupon coupon) {
+        couponRepository.save(coupon);
+    }
+
+    @Transactional(readOnly = true)
+    public Coupon getCoupon(Long couponId) {
+        return couponRepository.findById(couponId)
+                .orElse(null);
+    }
+}
