646ab863346d27fe230197beb1e955e84a7b88b8
HoeSeong123 <ghltjd3@gmail.com>
2024-10-17 14:04:06 +0900

    feat: 쿠폰 할인 금액 검증 구현

--- src/main/java/coupon/Coupon.java
+++ src/main/java/coupon/Coupon.java
@@ -3,15 +3,23 @@ package coupon;
 public class Coupon {
 
     private final String name;
+    private final Integer discountAmount;
 
-    public Coupon(String name) {
-        validate(name);
+    public Coupon(String name, Integer discountAmount) {
+        validate(name, discountAmount);
         this.name = name;
+        this.discountAmount = discountAmount;
     }
 
-    private void validate(String name) {
-        if(name == null || name.isBlank() || name.length() > 30) {
+    private void validate(String name, Integer discountAmount) {
+        if (name == null || name.isBlank() || name.length() > 30) {
             throw new IllegalArgumentException("쿠폰의 이름은 1자 이상 30자 이하여야 합니다.");
+        }
+        if (discountAmount == null || discountAmount < 1000 || discountAmount > 10000) {
+            throw new IllegalArgumentException("할인 금액은 1_000원 이상, 10_000원 이하여야 합니다.");
+        }
+        if (discountAmount % 500 != 0) {
+            throw new IllegalArgumentException("할인 금액은 500원 단위로 설정할 수 있습니다.");
         }
     }
 }
--- src/test/java/coupon/CouponTest.java
+++ src/test/java/coupon/CouponTest.java
@@ -1,26 +1,47 @@
 package coupon;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.util.stream.Stream;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CouponTest {
 
+    @Test
+    @DisplayName("쿠폰 생성 성공")
+    void couponCreateSuccess() {
+        String name = "name";
+        Integer discountAmount = 1000;
+        assertThatNoException().isThrownBy(() -> new Coupon(name, discountAmount));
+    }
+
     @ParameterizedTest
-    @DisplayName("쿠폰의 이름은 1자 이상 30자 이하여야 한다.")
-    @MethodSource("provideInvalidNames")
-    void couponHasName(String name) {
-        assertThatThrownBy(() -> new Coupon(name))
+    @DisplayName("쿠폰 생성 실패: 쿠폰의 이름은 1자 이상, 30자 이하여야 한다.")
+    @ValueSource(strings = {"", " ", "0123456789012345678901234567890"})
+    void nameRange(String name) {
+        assertThatThrownBy(() -> new Coupon(name, 1000))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("쿠폰의 이름은 1자 이상 30자 이하여야 합니다.");
     }
 
-    private static Stream<String> provideInvalidNames() {
-        return Stream.of(null, "", " ", "0123456789012345678901234567890");
+    @Test
+    @DisplayName("쿠폰 생성 실패: 할인 금액은 500원 단위로 설정할 수 있다.")
+    void discountAmountUnit() {
+        Integer discountAmount = 1234;
+        assertThatThrownBy(() -> new Coupon("coupon", discountAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("할인 금액은 500원 단위로 설정할 수 있습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("쿠폰 생성 실패: 할인 금액은 1000원 이상, 10000원 이하여야 한다.")
+    @ValueSource(ints = {500, 10500})
+    void discountAmountRange(Integer discountAmount) {
+        assertThatThrownBy(() -> new Coupon("coupon", discountAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("할인 금액은 1_000원 이상, 10_000원 이하여야 합니다.");
     }
 }
