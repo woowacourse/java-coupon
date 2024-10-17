ed1c4dd89d981d933600a96ce5619aba8b561af7
masonkimseoul <masonkimseoul@gmail.com>
2024-10-18 00:14:40 +0900

    feat: 읽기, 쓰기용 DataSource 분리

---
+++ src/main/java/coupon/infrastructure/DataSourceRouter.java
@@ -0,0 +1,15 @@
+package coupon.infrastructure;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class DataSourceRouter extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            return DataSourceType.READ;
+        }
+        return DataSourceType.WRITE;
+    }
+}
