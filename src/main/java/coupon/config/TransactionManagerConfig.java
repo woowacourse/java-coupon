3bd2d23c29aa23ae808d052cf1a5905bc691bb2b
jongmee <ra123221@gmail.com>
2024-10-17 20:27:31 +0900

    feat: mysql replica set 설정

---
+++ src/main/java/coupon/config/TransactionManagerConfig.java
@@ -0,0 +1,18 @@
+package coupon.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class TransactionManagerConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+        return jpaTransactionManager;
+    }
+}
