4442e0b83b22149d09a1a5c3a9c9ee7045aa54b8
H <dmsgktn0208@gmail.com>
2024-10-18 03:00:39 +0900

    feat: Member 엔티티 추가

---
+++ src/main/java/coupon/member/domain/Member.java
@@ -0,0 +1,25 @@
+package coupon.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
