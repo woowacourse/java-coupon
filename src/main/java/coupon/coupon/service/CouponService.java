5ada527e1e8b09fcb54a8610d5cc0893b3543634
BurningFalls <sherlocky31415@gmail.com>
2024-10-18 07:18:47 +0900

    feat: 쿠폰 생성 & 조회 기능 구현 + 복제 지연으로 인한 이슈 확인

---
+++ src/main/java/coupon/coupon/service/CouponService.java
@@ -0,0 +1,27 @@
+package coupon.coupon.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import coupon.coupon.domain.Coupon;
+import coupon.coupon.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    @Transactional
+    public Long create(Coupon coupon) {
+        Coupon savedCoupon = couponRepository.save(coupon);
+        return savedCoupon.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Coupon readById(Long couponId) {
+        return couponRepository.findById(couponId)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 ID의 쿠폰이 없습니다."));
+    }
+}
---
+++ src/test/java/coupon/coupon/service/CouponServiceTest.java
@@ -0,0 +1,29 @@
+package coupon.coupon.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import coupon.coupon.domain.Coupon;
+
+@SpringBootTest
+public class CouponServiceTest {
+
+    @Autowired
+    private CouponService couponService;
+
+    @DisplayName("복제지연테스트")
+    @Test
+    void testReplicationDelay() throws InterruptedException {
+        Coupon coupon = new Coupon("name", 1000L, 10000L, "FOOD", LocalDateTime.now(), LocalDateTime.now());
+        couponService.create(coupon);
+
+        Coupon savedCoupon = couponService.readById(coupon.getId());
+        assertThat(savedCoupon).isNotNull();
+    }
+}
