3663727d49ef75c10dcae11a77122efb4f9eea64
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-10-18 01:49:27 +0900

    feat(domain): 회원 도메인 구현

---
+++ src/main/java/coupon/member/domain/Member.java
@@ -0,0 +1,42 @@
+package coupon.member.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import coupon.base.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id", callSuper = false)
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "user_id", unique = true, nullable = false)
+    private String userId;
+
+    public Member(String name, String password, String userId) {
+        this(null, name, password, userId);
+    }
+}
