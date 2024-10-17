c18442eed5142fbfcf1de0d9b4a0b0e3fa7a7181
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-17 22:31:53 +0900

    feat(ExpirationDate): 회원 쿠폰 만료일 VO 구현

---
+++ src/main/java/coupon/domain/ExpirationDate.java
@@ -0,0 +1,20 @@
+package coupon.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record ExpirationDate(LocalDate date) {
+
+    public boolean isNotExpiate(final LocalDateTime dateTime) {
+        // TODO: 구리다.
+        final LocalDateTime nextDay = date.plusDays(1).atStartOfDay();
+        if (dateTime.isBefore(nextDay)) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/coupon/domain/ExpirationDateTest.java
@@ -0,0 +1,20 @@
+package coupon.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ExpirationDateTest {
+
+    @Test
+    @DisplayName("만료 일의 23:59:59.999999 까지 사용 가능")
+    void isExpired() {
+        final ExpirationDate expirationDate = new ExpirationDate(LocalDate.parse("2024-01-01"));
+
+        assertTrue(expirationDate.isNotExpiate(LocalDateTime.parse("2024-01-01T00:00:00.000000")));
+    }
+}
