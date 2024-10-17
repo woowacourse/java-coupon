eb42c3fc8187fa474d30735e423fb243735f277f
HyungHoKim00 <hkim1109@naver.com>
2024-10-17 12:29:57 +0900

    chore: db 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,9 +5,9 @@ create table if not exists coupon
 (
     id            BIGINT AUTO_INCREMENT PRIMARY KEY,
     name          VARCHAR(255) NOT NULL,
-    discount      INT         NOT NULL,
-    minimum_order INT         NOT NULL,
-    category      VARCHAR(255),
-    start         DATE        NOT NULL,
-    end           DATE        NOT NULL
+    discount      INT          NOT NULL,
+    minimum_order INT          NOT NULL,
+    category      VARCHAR(255) NOT NULL,
+    start         DATE         NOT NULL,
+    end           DATE         NOT NULL
 );
