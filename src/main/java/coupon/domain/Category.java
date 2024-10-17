59c02bf823295e844747a5fa076ade02448373c4
unifolio0 <boho82ha@gmail.com>
2024-10-17 15:58:43 +0900

    refactor: 한글 이름 추가

--- src/main/java/coupon/domain/Category.java
+++ src/main/java/coupon/domain/Category.java
@@ -2,8 +2,15 @@ package coupon.domain;
 
 public enum Category {
 
-    FASHION,
-    ELECTRONICS,
-    FURNITURE,
-    FOOD
+    FASHION("패션"),
+    ELECTRONICS("가전"),
+    FURNITURE("가구"),
+    FOOD("식품"),
+    ;
+
+    private final String description;
+
+    Category(String description) {
+        this.description = description;
+    }
 }
