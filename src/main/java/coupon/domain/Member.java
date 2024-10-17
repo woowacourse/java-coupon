0b9ab6a15dd53a51a1ebb39a0291e11d7ab09881
Gyeongho Yang <chchch1213@naver.com>
2024-10-18 02:29:36 +0900

    feat: add member name and constructors

--- src/main/java/coupon/domain/Member.java
+++ src/main/java/coupon/domain/Member.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Member {
 
     @Id
@@ -18,4 +20,8 @@ public class Member {
     private Long id;
 
     private String name;
+
+    public Member(String name) {
+        this(null, name);
+    }
 }
