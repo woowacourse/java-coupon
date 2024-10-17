7352a59fedcc63ad5eebbb77293636e68ec4675b
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-17 21:57:26 +0900

    feat(DiscountRate): 할인율 VO 구현

---
+++ src/main/java/coupon/domain/DiscountRate.java
@@ -0,0 +1,20 @@
+package coupon.domain;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record DiscountRate(int rate) {
+
+    private static final int MINIMUM_DISCOUNT_RATE = 3;
+    private static final int MAXIMUM_DISCOUNT_RATE = 20;
+
+    public DiscountRate {
+        validateRateRange(rate);
+    }
+
+    public void validateRateRange(int rate) {
+        if (rate < MINIMUM_DISCOUNT_RATE || rate > MAXIMUM_DISCOUNT_RATE) {
+            throw new IllegalArgumentException("할인율은 3% 이상 20% 이하의 값이어야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/coupon/domain/DiscountRateTest.java
@@ -0,0 +1,19 @@
+package coupon.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DiscountRateTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 21})
+    void createRateWhenInvalidRange(final int rate) {
+        assertThatThrownBy(() -> new DiscountRate(rate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("할인율은 3% 이상 20% 이하의 값이어야 합니다.");
+    }
+}
