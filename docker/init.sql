004b2ea1dc0d62386b66aa008d92672d55807444
fromitive <fromitive@gmail.com>
2024-10-18 03:47:44 +0900

    chore: init.sql 변경 및 테이블 검증 설정 변경

--- docker/init.sql
+++ docker/init.sql
@@ -2,14 +2,15 @@ CREATE DATABASE IF NOT EXISTS coupon;
 
 USE coupon;
 
-create table if not exist coupon
+create table coupon
 (
-    id             bigint auto_increment primary key,
-    name           varchar(255)                                            not null,
-    discount_money bigint                                                  not null,
-    discount_rate  bigint                                                  not null,
-    order_price    bigint                                                  not null,
-    category       enum ('fashions', 'electronics', 'furnitures', 'foods') not null,
-    start          datetime                                                not null,
-    end            datetime                                                not null
-);
+    id             bigint                                               not null auto_increment,
+    discount_money bigint,
+    discount_rate  bigint                                               not null,
+    end            datetime(6)                                          not null,
+    order_price    bigint                                               not null,
+    start          datetime(6)                                          not null,
+    name           varchar(255)                                         not null,
+    category       enum ('FASHIONS','ELECTRONICS','FURNITURES','FOODS') not null,
+    primary key (id)
+) engine = InnoDB
