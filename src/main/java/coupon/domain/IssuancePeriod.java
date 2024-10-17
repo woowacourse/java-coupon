aba82fa948b70dea91f9eda392e0ea2c34babcfa
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-17 22:16:55 +0900

    feat(IssuancePeriod): 발급 기간 VO 구현

---
+++ src/main/java/coupon/domain/IssuancePeriod.java
@@ -0,0 +1,23 @@
+package coupon.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record IssuancePeriod(LocalDate startDate, LocalDate endDate) {
+
+    public boolean canIssue(final LocalDateTime dateTime) {
+        LocalDateTime issuedStartDateTime = startDate.atStartOfDay();
+        LocalDateTime issuedEndDateTime = endDate.atStartOfDay().plusDays(1);
+
+        boolean isEqualOrAfterThanStartDateTime = dateTime.isEqual(issuedStartDateTime) || dateTime.isAfter(issuedStartDateTime);
+        if (isEqualOrAfterThanStartDateTime && dateTime.isBefore(issuedEndDateTime)) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
---
+++ src/test/java/coupon/domain/IssuancePeriodTest.java
@@ -0,0 +1,19 @@
+package coupon.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+class IssuancePeriodTest {
+
+    @Test
+    void canIssue() {
+        IssuancePeriod issuancePeriod = new IssuancePeriod(LocalDate.parse("2024-01-01"), LocalDate.parse("2025-01-01"));
+
+        assertTrue(issuancePeriod.canIssue(LocalDateTime.parse("2024-01-01T00:00:00.000000")));
+        assertTrue(issuancePeriod.canIssue(LocalDateTime.parse("2025-01-01T23:59:59.999999")));
+    }
+}
