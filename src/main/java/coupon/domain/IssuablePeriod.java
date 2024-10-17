aeb23bd59db8bf737e7fe2c29e1f2f81856226c3
donghoony <aru0504@naver.com>
2024-10-17 20:41:30 +0900

    feat: 쿠폰 발급 기간

---
+++ src/main/java/coupon/domain/IssuablePeriod.java
@@ -0,0 +1,34 @@
+package coupon.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class IssuablePeriod {
+
+    private static final LocalTime START_TIME = LocalTime.of(0, 0);
+    private static final LocalTime END_TIME = LocalTime.of(23, 59, 59, 999_999);
+
+    private final LocalDate issuanceDate;
+    private final LocalDate expirationDate;
+
+    public IssuablePeriod(LocalDate issuanceDate, LocalDate expirationDate) {
+        if (issuanceDate == null || expirationDate == null) {
+            throw new IllegalArgumentException("발급일과 만료일은 필수입니다.");
+        }
+        if (issuanceDate.isAfter(expirationDate)) {
+            throw new IllegalArgumentException("발급일은 만료일 이전이어야 합니다.");
+        }
+        this.issuanceDate = issuanceDate;
+        this.expirationDate = expirationDate;
+    }
+
+    public boolean isIssuable(LocalDateTime time) {
+        LocalDateTime startPeriod = issuanceDate.atTime(START_TIME);
+        LocalDateTime endPeriod = expirationDate.atTime(END_TIME);
+        if (startPeriod.isEqual(time) || endPeriod.isEqual(time)) {
+            return true;
+        }
+        return time.isAfter(startPeriod) && time.isBefore(endPeriod);
+    }
+}
---
+++ src/test/java/coupon/domain/IssuablePeriodTest.java
@@ -0,0 +1,39 @@
+package coupon.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+class IssuablePeriodTest {
+
+    @Test
+    void 시작일과_종료일을_통해_발급_가능_기간을_생성한다() {
+        LocalDate startDate = LocalDate.of(2021, 1, 1);
+        LocalDate endDate = LocalDate.of(2021, 1, 31);
+        assertThatCode(() -> new IssuablePeriod(startDate, endDate))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 발급일이_만료일보다_이후인_경우_예외를_발생한다() {
+        LocalDate startDate = LocalDate.of(2021, 1, 31);
+        LocalDate endDate = LocalDate.of(2021, 1, 30);
+        assertThrows(IllegalArgumentException.class, () -> new IssuablePeriod(startDate, endDate));
+    }
+
+    @Test
+    void 발급일과_만료일이_같은_경우_해당_일에만_발급할_수_있다() {
+        LocalDate date = LocalDate.of(2021, 1, 31);
+        LocalDate startDate = date;
+        LocalDate endDate = date;
+        IssuablePeriod issuablePeriod = new IssuablePeriod(startDate, endDate);
+        assertAll(
+                () -> assertThat(issuablePeriod.isIssuable(date.atTime(0, 0))).isTrue(),
+                () -> assertThat(issuablePeriod.isIssuable(date.plusDays(1).atStartOfDay())).isFalse()
+        );
+    }
+}
