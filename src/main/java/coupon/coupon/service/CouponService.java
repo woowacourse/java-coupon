fbacb8d2f40f387c0b090a279b3f5c4bfe27486d
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-10-17 10:23:06 +0900

    feat: 쿠폰 생성 및 조회 서비스

---
+++ src/main/java/coupon/coupon/service/CouponService.java
@@ -0,0 +1,25 @@
+package coupon.coupon.service;
+
+import coupon.coupon.domain.Coupon;
+import coupon.coupon.domain.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    @Transactional
+    public void createCoupon(Coupon coupon) {
+        couponRepository.save(coupon);
+    }
+
+    @Transactional(readOnly = true)
+    public Coupon getCoupon(Long couponId) {
+        return couponRepository.findById(couponId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 쿠폰입니다. couponId: %d ".formatted(couponId)));
+    }
+}
---
+++ src/test/java/coupon/coupon/service/CouponServiceTest.java
@@ -0,0 +1,39 @@
+package coupon.coupon.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import coupon.coupon.domain.Coupon;
+import coupon.coupon.domain.CouponCategory;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceTest {
+
+    @Autowired
+    private CouponService couponService;
+
+    @Test
+    void 복제지연테스트() {
+        // given
+        String name = "냥인의쿠폰";
+        BigDecimal discountAmount = BigDecimal.valueOf(1_000);
+        BigDecimal minimumOrderPrice = BigDecimal.valueOf(5_000);
+        CouponCategory couponCategory = CouponCategory.FOOD;
+        LocalDateTime issueStartedAt = LocalDateTime.of(2024, 10, 16, 0, 0, 0, 000_000_000);
+        LocalDateTime issueEndedAt = LocalDateTime.of(2024, 10, 26, 23, 59, 59, 999_999_000);
+        Coupon coupon = new Coupon(
+                name, discountAmount, minimumOrderPrice, couponCategory, issueStartedAt, issueEndedAt
+        );
+
+        // when
+        couponService.createCoupon(coupon);
+        Coupon savedCoupon = couponService.getCoupon(coupon.getId());
+
+        // then
+        assertThat(savedCoupon).isNotNull();
+    }
+}
