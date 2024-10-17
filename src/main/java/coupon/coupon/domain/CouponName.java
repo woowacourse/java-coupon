ddab8722782d6c97170085d3a79bd7f54181ff44
masonkimseoul <masonkimseoul@gmail.com>
2024-10-18 01:34:57 +0900

    feat: CouponName 클래스 생성

---
+++ src/main/java/coupon/coupon/domain/CouponName.java
@@ -0,0 +1,38 @@
+package coupon.coupon.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class CouponName {
+
+    private static final int MAX_COUPON_NAME_LENGTH = 30;
+
+    @Column(name = "name")
+    private String value;
+
+    public CouponName() {
+    }
+
+    public CouponName(String value) {
+        validateEmptyValue(value);
+        validateCouponNameLength(value);
+        this.value = value;
+    }
+
+    private void validateEmptyValue(String value) {
+        if (value.isBlank()) {
+            throw new IllegalArgumentException("쿠폰 이름이 비어있습니다.");
+        }
+    }
+
+    private void validateCouponNameLength(String value) {
+        if (value.length() > MAX_COUPON_NAME_LENGTH) {
+            throw new IllegalArgumentException("쿠폰 이름의 길이가 30자를 초과합니다. 쿠폰 이름 : " + value);
+        }
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
