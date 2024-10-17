9a217311c2430552f0ac55777b0cf863c4077c4e
Gyeongho Yang <chchch1213@naver.com>
2024-10-18 02:31:09 +0900

    test: assert not null when getting after creating

--- src/test/java/coupon/service/CouponServiceTest.java
+++ src/test/java/coupon/service/CouponServiceTest.java
@@ -1,8 +1,9 @@
 package coupon.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import coupon.domain.Coupon;
-import coupon.repository.CouponRepository;
-import java.util.List;
+import coupon.util.CouponFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,19 +13,20 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CouponServiceTest {
 
-
     @Autowired
-    private CouponRepository couponRepository;
+    private CouponService couponService;
 
     @Test
     @DisplayName("복제 지연 테스트")
-    void lazyCopy() {
+    void replicationDelay() {
         // given
-        List<Coupon> coupons = couponRepository.findAll();
+        Coupon coupon = CouponFixture.createCoupon();
 
         // when
+        couponService.create(coupon);
+        Coupon savedCoupon = couponService.getCoupon(coupon.getId());
 
         // then
-        System.out.println("coupons = " + coupons);
+        assertThat(savedCoupon).isNotNull();
     }
 }
