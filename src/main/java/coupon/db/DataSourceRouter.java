edd43e417e02146863687106a42fd8cf972890cc
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-17 20:59:29 +0900

    feat: dataSourcerouter 로깅 추가

--- src/main/java/coupon/db/DataSourceRouter.java
+++ src/main/java/coupon/db/DataSourceRouter.java
@@ -1,15 +1,20 @@
 package coupon.db;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 public class DataSourceRouter extends AbstractRoutingDataSource {
 
+    private static final Logger logger = LoggerFactory.getLogger(DataSourceRouter.class);
 
     @Override
     protected Object determineCurrentLookupKey() {
         boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
         boolean isTransactionActive = TransactionSynchronizationManager.isActualTransactionActive();
-        return DataSourceType.mapTo(isReadOnly, isTransactionActive);
+        DataSourceType dataSourceType = DataSourceType.mapTo(isReadOnly, isTransactionActive);
+        logger.info("readOnly : {}, isTransactionActive : {}, dataSourceType : {}", isReadOnly, isTransactionActive, dataSourceType);
+        return dataSourceType;
     }
 }
