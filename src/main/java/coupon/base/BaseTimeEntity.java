d05afefbfd329e63781b0d3707dfbf58393c24bf
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-10-18 01:45:42 +0900

    feat(base): 생성, 수정 시간 컬럼 추가 로직 구현

---
+++ src/main/java/coupon/base/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package coupon.base;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+}
