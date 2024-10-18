b44eb8e7d9b262c1b2b9480a353909be74a2669e
tsulocalize <tsulocalizing@gmail.com>
2024-10-18 11:04:14 +0900

    feat: coupon 도메인 검증 구현

--- src/main/java/coupon/domain/Coupon.java
+++ src/main/java/coupon/domain/Coupon.java
@@ -7,9 +7,11 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class Coupon {
 
     @Id
@@ -19,13 +21,10 @@ public class Coupon {
     @Column(nullable = false, length = 30)
     private String name;
 
-    // 1000 <= x <= 10000, unit : 500
     private Integer discount;
 
-    // 3% <= x <= 20%
     private Integer discountRate;
 
-    // 5000 <= x <= 100000
     private Integer minOrderPrice;
 
     @Enumerated(EnumType.STRING)
@@ -38,6 +37,11 @@ public class Coupon {
 
     public Coupon(String name, Integer discount, Integer discountRate, Integer minOrderPrice, Category category,
                   LocalDateTime issuableFrom, LocalDateTime issuableTo) {
+        validateName(name);
+        validateDiscount(discount);
+        validateDiscountRate(discountRate);
+        validateMinOrderPrice(minOrderPrice);
+        validateIssuable(issuableFrom, issuableTo);
         this.name = name;
         this.discount = discount;
         this.discountRate = discountRate;
@@ -54,67 +58,37 @@ public class Coupon {
 
     protected Coupon() {}
 
-    public Long getId() {
-        return id;
+    private void validateName(String name) {
+        if (name == null || name.length() > 30) {
+            throw new IllegalArgumentException("잘못된 이름입니다: " + name);
+        }
     }
 
-    public String getName() {
-        return name;
+    private void validateDiscount(Integer discount) {
+        // 1000 <= x <= 10000, unit : 500
+        if (discount < 1000 || discount > 10000) {
+            throw new IllegalArgumentException("할인 금액 오류입니다: " + discount);
+        }
+        if (discount % 500 != 0) {
+            throw new IllegalArgumentException("할인 금액 단위 오류입니다: " + discount);
+        }
     }
 
-    public Integer getDiscount() {
-        return discount;
+    private void validateDiscountRate(Integer discountRate) {
+        if (discountRate < 3 || discountRate > 20) {
+            throw new IllegalArgumentException("할인률 오류입니다: " + discountRate);
+        }
     }
 
-    public Integer getDiscountRate() {
-        return discountRate;
+    private void validateMinOrderPrice(Integer minOrderPrice) {
+        if (minOrderPrice < 5_000 || minOrderPrice > 100_000) {
+            throw new IllegalArgumentException("최소 주문 금액 오류입니다: " + minOrderPrice);
+        }
     }
 
-    public Integer getMinOrderPrice() {
-        return minOrderPrice;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public LocalDateTime getIssuableFrom() {
-        return issuableFrom;
-    }
-
-    public LocalDateTime getIssuableTo() {
-        return issuableTo;
+    private void validateIssuable(LocalDateTime issuableFrom, LocalDateTime issuableTo) {
+        if (issuableFrom.isEqual(issuableTo) || issuableFrom.isAfter(issuableTo)) {
+            throw new IllegalArgumentException("발급 가능 날짜 오류입니다: " + issuableFrom + " ~ " + issuableTo);
+        }
     }
 }
-
-
-/*
-쿠폰
-회원에게 발급하는 쿠폰의 이름, 할인 금액, 최소 주문 금액 등 쿠폰의 설정에 해당하는 내용을 관리한다. 다음과 같은 제약사항을 갖는다.
-이름
-이름은 반드시 존재해야 한다.
-이름의 길이는 최대 30자 이하여야 한다.
-할인 금액
-할인 금액 제약
-할인 금액은 1,000원 이상이어야 한다.
-할인 금액은 10,000원 이하여야 한다.
-할인 금액은 500원 단위로 설정할 수 있다.
-할인율 제약
-할인율은 할인금액 / 최소 주문 금액 식을 사용하여 계산하며, 소수점은 버림 한다.
-예를 들어, 최소 주문 금액이 30,000원일 때 할인 금액이 1,000원이라면 쿠폰의 할인율은 3%(1000 / 30000 = 3.333...%)가 된다.
-할인율은 3% 이상이어야 한다.
-할인율은 20% 이하여야 한다.
-최소 주문 금액
-최소 주문 금액은 5,000원 이상이어야 한다.
-최소 주문 금액은 100,000원 이하여야 한다.
-카테고리
-서비스의 카테고리는 다음과 같이 네 종류가 있고, 이 중 하나의 카테고리만 선택할 수 있다.
-패션
-가전
-가구
-식품
-발급 기간
-시작일은 종료일보다 이전이어야 한다. 시작일과 종료일이 같다면, 해당 일에만 발급할 수 있는 쿠폰이 된다.
-시작일 00:00:00.000000 부터 발급할 수 있다.
-종료일 23:59:59.999999 까지 발급할 수 있다.
- */
