b1195156bd45fc3232c48465982d3bc80a012e3a
nhlee98 <e5slnh9876@gmail.com>
2024-10-18 15:06:01 +0900

    fix: 복제 지연 문제 해결

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -30,7 +30,11 @@ public class CouponService {
         return CouponResponse.from(newCoupon);
     }
 
-    @Transactional(readOnly = true)
+    /**
+     * 이 메서드는 DB가 reader와 writer로 분리된 환경에서 복제 지연(Replication Lag) 때문에 readOnly=true를 사용하지 않았습니다. readOnly를 사용하면 Reader
+     * DB에서만 조회하게 되어, Writer DB에 아직 반영되지 않은 최신 데이터를 조회하지 못할 수 있기 때문입니다.
+     */
+    @Transactional
     public CouponResponse getCoupon(Long couponId) {
         Coupon coupon = couponRepository.findById(couponId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 쿠폰입니다."));
