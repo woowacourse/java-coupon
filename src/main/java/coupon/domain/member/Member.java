f7e4713378192c9d4709a85adc4d19824b61da42
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-10-18 10:47:26 +0900

    feat(Member): 멤버 도메인 구현

---
+++ src/main/java/coupon/domain/member/Member.java
@@ -0,0 +1,31 @@
+package coupon.domain.member;
+
+import coupon.domain.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Member(String name) {
+        this(null, name);
+    }
+}
