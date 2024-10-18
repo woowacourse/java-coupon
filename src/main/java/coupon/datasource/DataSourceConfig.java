0af6ba335b3e05c70873f2b5632cfe73e7e92397
KIMGYUTAE <kiimwf0819@gmail.com>
2024-10-18 17:16:15 +0900

    feat: DataSourceConfig 설정

---
+++ src/main/java/coupon/datasource/DataSourceConfig.java
@@ -0,0 +1,54 @@
+package coupon.datasource;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+@Configuration
+public class DataSourceConfig {
+
+    private static final String WRITER_DATA_SOURCE = "writerDataSource";
+    private static final String READER_DATA_SOURCE = "readerDataSource";
+    private static final String DATA_SOURCE_ROUTER = "dataSourceRouter";
+
+    @Bean(name = WRITER_DATA_SOURCE)
+    @ConfigurationProperties("coupon.datasource.writer")
+    public DataSource writerDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = READER_DATA_SOURCE)
+    @ConfigurationProperties("coupon.datasource.reader")
+    public DataSource readerDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = DATA_SOURCE_ROUTER)
+    public DataSource dataSource(
+            @Qualifier(WRITER_DATA_SOURCE) DataSource writerDataSource,
+            @Qualifier(READER_DATA_SOURCE) DataSource readerDataSource
+    ) {
+        Map<Object, Object> dataSourceMap = new HashMap<>();
+        dataSourceMap.put(DataSourceType.WRITER, writerDataSource);
+        dataSourceMap.put(DataSourceType.READER, readerDataSource);
+
+        DataSourceRouter dataSourceRouter = new DataSourceRouter();
+        dataSourceRouter.setTargetDataSources(dataSourceMap);
+        dataSourceRouter.setDefaultTargetDataSource(writerDataSource);
+
+        return dataSourceRouter;
+    }
+
+    @Primary
+    @Bean
+    public DataSource lazyDataSource(@Qualifier(DATA_SOURCE_ROUTER) DataSource dataSourceRouter) {
+        return new LazyConnectionDataSourceProxy(dataSourceRouter);
+    }
+}
