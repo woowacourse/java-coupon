2d477c9dd5c35d3e64e686ddfa8b389452cfafab
hyxrxn <gpfls981220@gmail.com>
2024-10-18 16:07:18 +0900

    feat: 복제 지연 시 writer db 사용

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -2,15 +2,22 @@ package coupon.service;
 
 import coupon.domain.coupon.Coupon;
 import coupon.repository.CouponRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
-@RequiredArgsConstructor
 public class CouponService {
 
     private final CouponRepository couponRepository;
+    private final TransactionTemplate transactionTemplate;
+
+    public CouponService(CouponRepository couponRepository, PlatformTransactionManager transactionManager) {
+        this.couponRepository = couponRepository;
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
+    }
 
     @Transactional
     public void create(Coupon coupon) {
@@ -20,6 +27,18 @@ public class CouponService {
     @Transactional(readOnly = true)
     public Coupon getCoupon(Long id) {
         return couponRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 아이디의 쿠폰이 존재하지 않습니다."));
+                .orElseGet(() -> getCouponOnWriter(id));
+    }
+
+    private Coupon getCouponOnWriter(Long id) {
+        int originalPropagationBehavior = transactionTemplate.getPropagationBehavior();
+
+        try {
+            transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+            return transactionTemplate.execute(status -> couponRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 아이디의 쿠폰이 존재하지 않습니다.")));
+        } finally {
+            transactionTemplate.setPropagationBehavior(originalPropagationBehavior);
+        }
     }
 }
