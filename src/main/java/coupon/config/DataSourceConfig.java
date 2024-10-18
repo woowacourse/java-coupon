a4c5623c99064b7d8059559af8ba7d59c3cc0836
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-10-18 11:31:18 +0900

    feat(config): 데이터소스 설정 파일 구현

---
+++ src/main/java/coupon/config/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package coupon.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "writerDataSource")
+    @ConfigurationProperties(prefix = "coupon.datasource.writer")
+    public DataSource writerDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "readerDataSource")
+    @ConfigurationProperties(prefix = "coupon.datasource.reader")
+    public DataSource readerDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
