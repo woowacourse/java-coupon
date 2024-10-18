19f32619cc1411ed87c513c876f1666200da3a4b
Gyeongho Yang <chchch1213@naver.com>
2024-10-18 10:40:48 +0900

    refactor: use lombok as slf4j

--- src/main/java/coupon/config/RoutingDataSource.java
+++ src/main/java/coupon/config/RoutingDataSource.java
@@ -1,24 +1,23 @@
 package coupon.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
 
     public static final String READER_SERVER = "reader";
     public static final String WRITER_SERVER = "writer";
-    private static final Logger logger = LoggerFactory.getLogger(RoutingDataSource.class);
 
     @Override
     protected Object determineCurrentLookupKey() {
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
-            logger.debug("Using Reader DataSource");
+            log.debug("Using Reader DataSource");
             return READER_SERVER;
         }
 
-        logger.debug("Using Writer DataSource");
+        log.debug("Using Writer DataSource");
         return WRITER_SERVER;
     }
 }
