021887474bc1038a219154a6dace64f1cdce6209
hjk0761 <hjk0761@naver.com>
2024-10-18 14:00:52 +0900

    feat: 복제 지연 확인

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -3,6 +3,7 @@ package coupon.service;
 import coupon.domain.Coupon;
 import coupon.repository.CouponRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CouponService {
@@ -13,10 +14,12 @@ public class CouponService {
         this.couponRepository = couponRepository;
     }
 
+    @Transactional
     public void create(Coupon coupon) {
         couponRepository.save(coupon);
     }
 
+    @Transactional(readOnly = true)
     public Coupon getCoupon(Long id) {
         return couponRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException(String.format("Coupon with id %d does not exists", id)));
---
+++ src/test/java/coupon/service/CouponServiceTest.java
@@ -0,0 +1,29 @@
+package coupon.service;
+
+import coupon.domain.Category;
+import coupon.domain.Coupon;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+public class CouponServiceTest {
+
+    @Autowired
+    private CouponService couponService;
+
+    @Disabled
+    @Test
+    void replicationLagTest() {
+        Coupon coupon = new Coupon("coupon", 1000, 10000, Category.FOODS, LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1));
+        couponService.create(coupon);
+        assertThatThrownBy(() -> couponService.getCoupon(coupon.getId()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
