89335f195d9a9576a392b5cb48cb75e12cd26869
HoeSeong123 <ghltjd3@gmail.com>
2024-10-18 11:54:23 +0900

    refactor: 불필요한 래퍼 클래스 삭제

--- src/main/java/coupon/Coupon.java
+++ src/main/java/coupon/Coupon.java
@@ -1,15 +1,57 @@
 package coupon;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Coupon {
 
-    private final Name name;
-    private final DiscountAmount discountAmount;
-    private final PurchaseAmount purchaseAmount;
-    private final Category category;
-    private final IssuancePeriod issuancePeriod;
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer discountAmount;
+
+    @Column(nullable = false)
+    private Integer purchaseAmount;
+
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    public Coupon(
+            Long id,
+            String name,
+            Integer discountAmount,
+            Integer purchaseAmount,
+            Category category,
+            LocalDate startDate,
+            LocalDate endDate
+    ) {
+        validate(name, discountAmount, purchaseAmount, startDate, endDate);
+        this.id = id;
+        this.name = name;
+        this.discountAmount = discountAmount;
+        this.purchaseAmount = purchaseAmount;
+        this.category = category;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 
     public Coupon(
             String name,
@@ -19,18 +61,54 @@ public class Coupon {
             LocalDate startDate,
             LocalDate endDate
     ) {
-        validateDiscountRate(discountAmount, purchaseAmount);
-        this.name = new Name(name);
-        this.discountAmount = new DiscountAmount(discountAmount);
-        this.purchaseAmount = new PurchaseAmount(purchaseAmount);
-        this.category = category;
-        this.issuancePeriod = new IssuancePeriod(startDate, endDate);
+        this(null, name, discountAmount, purchaseAmount, category, startDate, endDate);
     }
 
-    private static void validateDiscountRate(Integer discountAmount, Integer purchaseAmount) {
+    private void validate(
+            String name,
+            Integer discountAmount,
+            Integer purchaseAmount,
+            LocalDate startDate,
+            LocalDate endDate
+    ) {
+        validateName(name);
+        validateDiscountAmount(discountAmount);
+        validatePurchaseAmount(purchaseAmount);
+        validateDiscountRate(discountAmount, purchaseAmount);
+        validateIssuancePeriod(startDate, endDate);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank() || name.length() > 30) {
+            throw new IllegalArgumentException("쿠폰의 이름은 1자 이상 30자 이하여야 합니다.");
+        }
+    }
+
+    private void validateDiscountAmount(Integer discountAmount) {
+        if (discountAmount == null || discountAmount < 1_000 || discountAmount > 10_000) {
+            throw new IllegalArgumentException("할인 금액은 1_000원 이상, 10_000원 이하여야 합니다.");
+        }
+        if (discountAmount % 500 != 0) {
+            throw new IllegalArgumentException("할인 금액은 500원 단위로 설정할 수 있습니다.");
+        }
+    }
+
+    private static void validatePurchaseAmount(Integer purchaseAmount) {
+        if (purchaseAmount == null || purchaseAmount < 5_000 || purchaseAmount > 100_000) {
+            throw new IllegalArgumentException("최소 주문 금액은 5_000원 이상 100_000원 이하여야 합니다.");
+        }
+    }
+
+    private void validateDiscountRate(Integer discountAmount, Integer purchaseAmount) {
         int purchaseRate = discountAmount * 100 / purchaseAmount;
         if (purchaseRate < 3 || purchaseRate > 20) {
             throw new IllegalArgumentException("할인율은 3% 이상, 20% 이하여야 합니다.");
+        }
+    }
+
+    private void validateIssuancePeriod(LocalDate startDate, LocalDate endDate) {
+        if(startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("시작일은 종료일보다 이전이어야 합니다.");
         }
     }
 }
