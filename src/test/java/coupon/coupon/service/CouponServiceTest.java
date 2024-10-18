58bb66be07b9de69fa04b88ddcb805fab353d674
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-18 14:20:00 +0900

    test(CouponServiceTest): 쿠폰 복제 지연 테스트

---
+++ src/test/java/coupon/coupon/service/CouponServiceTest.java
@@ -0,0 +1,25 @@
+package coupon.coupon.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import coupon.coupon.domain.Coupon;
+import coupon.fixture.CouponFixture;
+
+@SpringBootTest
+class CouponServiceTest {
+
+    @Autowired
+    private CouponService couponService;
+
+    @Test
+    void 복제지연테스트() {
+        Coupon coupon = CouponFixture.createCoupon();
+        couponService.create(coupon);
+        Coupon savedCoupon = couponService.getCoupon(coupon.getId());
+        assertThat(savedCoupon).isNotNull();
+    }
+}
