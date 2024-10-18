b64f0ab6a444094f864c6c6277f417dc35e429b7
tkdgur0906 <tkdgur0906@naver.com>
2024-10-18 15:32:35 +0900

    feat: 쿠폰 조회 시 replica에 존재하지 않는 경우 소스 서버에서 다시 조회하도록 구현

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -1,30 +1,26 @@
 package coupon.service;
 
-import coupon.domain.Category;
 import coupon.domain.Coupon;
 import coupon.dto.CouponRequest;
-import coupon.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class CouponService {
 
-    private final CouponRepository couponRepository;
+    private final CouponCommandService couponCommandService;
+    private final CouponQueryService couponQueryService;
 
-    @Transactional
     public Long create(CouponRequest request) {
-        Coupon coupon = new Coupon(request.name(), request.minimumOrderAmount(), request.discountAmount(),
-                Category.valueOf(request.category()),
-                request.issueStartDate(), request.issueEndDate());
-        return couponRepository.save(coupon).getId();
+        return couponCommandService.create(request);
     }
 
-    @Transactional(readOnly = true)
     public Coupon getCoupon(Long id) {
-        return couponRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 ID(" + id + ")의 쿠폰을 찾을 수 없습니다."));
+        try {
+            return couponQueryService.getCoupon(id);
+        } catch (IllegalArgumentException e) {
+            return couponCommandService.getCoupon(id);
+        }
     }
 }
