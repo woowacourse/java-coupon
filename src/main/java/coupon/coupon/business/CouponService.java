05a4bf78fbe64f053bd36614f29c80a1a7b8c676
김민겸 <mingyum119@naver.com>
2024-10-18 10:46:18 +0900

    refactor: 메서드 별 트랜잭션 분리

--- src/main/java/coupon/coupon/business/CouponService.java
+++ src/main/java/coupon/coupon/business/CouponService.java
@@ -8,15 +8,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class CouponService {
 
     private final CouponRepository couponRepository;
 
+    @Transactional
     public void create(Coupon coupon) {
         couponRepository.save(coupon);
     }
 
+    @Transactional(readOnly = true)
     public Coupon getCoupon(long id) {
         return couponRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 쿠폰 조회 요청입니다. id = " + id));
