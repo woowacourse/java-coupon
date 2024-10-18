0d9cfa7e050d6081e5c5d766c7a2decf6f81fe17
seunghye218 <seunghye@student.42seoul.kr>
2024-10-18 10:34:14 +0900

    refactor(IssuancePeriod): 발급 기간 필드명 변경
    - 필요없는 메서드 삭제

--- src/main/java/coupon/domain/IssuancePeriod.java
+++ src/main/java/coupon/domain/IssuancePeriod.java
@@ -15,30 +15,25 @@ import lombok.NoArgsConstructor;
 public class IssuancePeriod {
 
     @Column(nullable = false)
-    private LocalDate issuedAt;
+    private LocalDate issuanceStart;
 
     @Column(nullable = false)
-    private LocalDate expiredAt;
+    private LocalDate issuanceEnd;
 
-    public IssuancePeriod(LocalDate issuedAt, LocalDate expiredAt) {
-        validatePeriod(issuedAt, expiredAt);
-        this.issuedAt = issuedAt;
-        this.expiredAt = expiredAt;
+    public IssuancePeriod(LocalDate issuanceStart, LocalDate issuanceEnd) {
+        validatePeriod(issuanceStart, issuanceEnd);
+        this.issuanceStart = issuanceStart;
+        this.issuanceEnd = issuanceEnd;
     }
 
-    private void validatePeriod(LocalDate issuedAt, LocalDate expiredAt) {
-        if (issuedAt.isAfter(expiredAt) || issuedAt.isEqual(expiredAt)) {
+    private void validatePeriod(LocalDate issuanceStart, LocalDate issuanceEnd) {
+        if (issuanceStart.isAfter(issuanceEnd) || issuanceStart.isEqual(issuanceEnd)) {
             throw new CouponException("발급일은 만료일 보다 같거나 이전이어야 합니다.");
         }
     }
 
     public boolean isIssuable(LocalDateTime dateTime) {
         LocalDate date = dateTime.toLocalDate();
-        return !issuedAt.isAfter(date) && !expiredAt.isBefore(date);
-    }
-
-    public boolean isExpired(LocalDateTime dateTime) {
-        LocalDate date = dateTime.toLocalDate();
-        return expiredAt.isBefore(date);
+        return !issuanceStart.isAfter(date) && !issuanceEnd.isBefore(date);
     }
 }
