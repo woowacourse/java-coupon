d40c6fc42775ed41344f7ded6e44d86fcc1c6ab2
lemone <lejinhy@gmail.com>
2024-10-18 13:16:10 +0900

    feat: 쿠폰 엔티티 & 레포지토리 추가

---
+++ src/main/java/coupon/service/CouponRepository.java
@@ -0,0 +1,10 @@
+package coupon.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import coupon.repository.CouponEntity;
+
+@Repository
+public interface CouponRepository extends JpaRepository<CouponEntity, Long> {
+}
