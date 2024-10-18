9d8379fd21468e78d6003da68ac91eac0447d83f
mzeong <mjeong@sookmyung.ac.kr>
2024-10-18 15:13:30 +0900

    feat: `Coupon` 이름 검증

--- src/main/java/coupon/domain/coupon/Name.java
+++ src/main/java/coupon/domain/coupon/Name.java
@@ -10,9 +10,32 @@ import lombok.NoArgsConstructor;
 @Getter
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Name {
+
+    private static final int MAX_LENGTH = 30;
 
     @Column(name = "name", nullable = false)
     private String value;
+
+    public Name(String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(String value) {
+        validateExist(value);
+        validateLength(value);
+    }
+
+    private void validateExist(String value) {
+        if (value == null || value.trim().isEmpty()) {
+            throw new IllegalArgumentException("이름은 반드시 존재해야 합니다.");
+        }
+    }
+
+    private void validateLength(String value) {
+        if (value.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("이름의 길이는 최대 30자 이하여야 합니다.");
+        }
+    }
 }
---
+++ src/test/java/coupon/domain/coupon/NameTest.java
@@ -0,0 +1,49 @@
+package coupon.domain.coupon;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@Slf4j
+class NameTest {
+
+    @DisplayName("이름은 반드시 존재해야 한다.")
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "   "})
+    void validateExistException(String value) {
+        assertThatThrownBy(() -> new Name(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Nested
+    @DisplayName("이름의 길이는 최대 30자 이하여야 한다.")
+    class validateLength {
+
+        @ParameterizedTest
+        @ValueSource(ints = {29, 30})
+        void validateLengthSuccess(int length) {
+            String value = generateValueWithLength(length);
+            assertThatCode(() -> new Name(value))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void validateLengthException() {
+            String value = generateValueWithLength(31);
+            assertThatThrownBy(() -> new Name(value))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        private String generateValueWithLength(int length) {
+            return "a".repeat(length);
+        }
+    }
+}
