7261596545bcc96de336eb57ad39ad298ba68f9c
donghoony <aru0504@naver.com>
2024-10-18 13:00:00 +0900

    refactor: 가독성 개선

--- src/main/java/coupon/coupon/aop/CouponWriteAspect.java
+++ src/main/java/coupon/coupon/aop/CouponWriteAspect.java
@@ -16,32 +16,34 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 @RequiredArgsConstructor
 public class CouponWriteAspect {
 
+    private static final String COUPON_SERVICE_POINTCUT = "execution(* coupon.coupon.service.CouponService.*(..))";
     private static final String TABLE_NAME = "coupon";
 
     private final ApplicationEventPublisher applicationEventPublisher;
     private final WriteTimeChecker writeTimeChecker;
 
-    @Before("execution(* coupon.coupon.service.CouponService.*(..))")
+    @Before(COUPON_SERVICE_POINTCUT)
     public void before() {
-        // Read-only인 경우에만 write으로 라우팅할지를 판단하면 된다. Write인 경우 따로 처리할 필요가 없다.
-        if (!TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
-            return;
-        }
-        if (writeTimeChecker.isAvailableToRead(TABLE_NAME)) {
+        // Read-only인 경우에만 write으로 라우팅할지 판단한다. Write인 경우 따로 처리할 필요가 없다.
+        if (isCurrentTransactionReadOnly() && writeTimeChecker.isAvailableToRead(TABLE_NAME)) {
             RoutingContext.setRoutableToReadSource();
         }
     }
 
-    @AfterReturning("execution(* coupon.coupon.service.CouponService.*(..))")
+    @AfterReturning(COUPON_SERVICE_POINTCUT)
     public void afterReturning() {
-        if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+        if (isCurrentTransactionReadOnly()) {
             return;
         }
         applicationEventPublisher.publishEvent(new WriteEvent());
     }
 
-    @After("execution(* coupon.coupon.service.CouponService.*(..))")
+    @After(COUPON_SERVICE_POINTCUT)
     public void after() {
         RoutingContext.clear();
+    }
+
+    private boolean isCurrentTransactionReadOnly() {
+        return TransactionSynchronizationManager.isCurrentTransactionReadOnly();
     }
 }
