a86773ed194c63c2ae86359829ddbeda837740f5
libienz <leekh10195@gmail.com>
2024-10-18 10:18:42 +0900

    feat: CouponService 작성

---
+++ src/main/java/coupon/coupon/service/CouponService.java
@@ -0,0 +1,26 @@
+package coupon.coupon.service;
+
+import coupon.coupon.domain.Coupon;
+import coupon.coupon.domain.CouponRepository;
+import coupon.coupon.exception.CouponException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    @Transactional(readOnly = true)
+    public Coupon getCoupon(Long couponId) {
+        return couponRepository.findById(couponId)
+                .orElseThrow(() -> new CouponException("쿠폰이 존재하지 않습니다."));
+    }
+
+    @Transactional
+    public Coupon createCoupon(Coupon coupon) {
+        return couponRepository.save(coupon);
+    }
+}
