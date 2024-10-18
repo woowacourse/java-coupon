559c1ab2d3355d4f21c21112b5276da1dc4c75f2
pricelees <priceelees@gmail.com>
2024-10-18 14:57:36 +0900

    refactor: MemberCoupon에서의 연관관계 수정

--- src/main/java/coupon/entity/MemberCoupon.java
+++ src/main/java/coupon/entity/MemberCoupon.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
@@ -23,11 +24,11 @@ public class MemberCoupon {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "members_id")
     private Member member;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "coupon_id")
     private Coupon coupon;
 
