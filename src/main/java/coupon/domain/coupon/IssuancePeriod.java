30bcb3311d751572ee3becc6d5921275fdbfdd67
kelly6bf <kelly6bf@gmail.com>
2024-10-19 03:04:09 +0900

    refactor: IssuancePeriod 메서드명 변경

--- src/main/java/coupon/domain/coupon/IssuancePeriod.java
+++ src/main/java/coupon/domain/coupon/IssuancePeriod.java
@@ -11,25 +11,21 @@ public class IssuancePeriod {
     private final LocalDateTime endDate;
 
     public IssuancePeriod(final LocalDateTime startDate, final LocalDateTime endDate) {
-        validateStartDate(startDate, endDate);
-        validateEndDate(endDate);
+        validateDateIsNull(startDate, endDate);
+        validateEndDateIsBeforeThenStartDate(startDate, endDate);
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
-    private void validateStartDate(final LocalDateTime startDate, final LocalDateTime endDate) {
-        if (startDate == null) {
-            throw new IllegalArgumentException("시작일로 null 값을 입력할 수 없습니다.");
-        }
-
-        if (!startDate.isBefore(endDate)) {
-            throw new IllegalArgumentException("시작일은 종료일보다 이전이어야 합니다. - " + startDate + ", " + endDate);
+    private void validateDateIsNull(final LocalDateTime startDate, final LocalDateTime endDate) {
+        if (startDate == null || endDate == null) {
+            throw new IllegalArgumentException("시작일 혹은 종료일은 null 값을 입력할 수 없습니다.");
         }
     }
 
-    private void validateEndDate(final LocalDateTime endDate) {
-        if (endDate == null) {
-            throw new IllegalArgumentException("종료일로 null 값을 입력할 수 없습니다.");
+    private void validateEndDateIsBeforeThenStartDate(final LocalDateTime startDate, final LocalDateTime endDate) {
+        if (endDate.isBefore(startDate)) {
+            throw new IllegalArgumentException("종료일이 시작일보다 이전일 수 없습니다. - " + startDate + ", " + endDate);
         }
     }
 }
