7b0a1641096c5201f1f48608629fa3872d8ae386
masonkimseoul <masonkimseoul@gmail.com>
2024-10-18 14:26:31 +0900

    refactor: 할인율 관련 로직 DiscountAmount 도메인에서 제거

--- src/main/java/coupon/coupon/domain/DiscountAmount.java
+++ src/main/java/coupon/coupon/domain/DiscountAmount.java
@@ -9,8 +9,7 @@ public class DiscountAmount {
     private static final int MIN_DISCOUNT_AMOUNT = 1000;
     private static final int MAX_DISCOUNT_AMOUNT = 10000;
     private static final int DISCOUNT_UNIT = 500;
-    private static final int MIN_DISCOUNT_RATE = 3;
-    private static final int MAX_DISCOUNT_RATE = 20;
+
 
     @Column(name = "discount_amount")
     private int value;
@@ -18,10 +17,9 @@ public class DiscountAmount {
     public DiscountAmount(){
     }
 
-    public DiscountAmount(int discountAmount, int minimumOrderAmount) {
+    public DiscountAmount(int discountAmount) {
         validateAmount(discountAmount);
         validateUnit(discountAmount);
-        validateDiscountRate(discountAmount, minimumOrderAmount);
         this.value = discountAmount;
     }
 
@@ -37,16 +35,6 @@ public class DiscountAmount {
     private void validateUnit(int value) {
         if (value % DISCOUNT_UNIT != 0) {
             throw new IllegalArgumentException("할인 금액은 500원 단위로만 설정할 수 있습니다.");
-        }
-    }
-
-    private void validateDiscountRate(int discountAmount, int minimumOrderAmount) {
-        int discountRate = discountAmount * 100 / minimumOrderAmount;
-        if (discountRate < MIN_DISCOUNT_RATE) {
-            throw new IllegalArgumentException("할인율은 3% 이상이어야 합니다.");
-        }
-        if (discountRate > MAX_DISCOUNT_RATE) {
-            throw new IllegalArgumentException("할인율은 20% 이하여야 합니다.");
         }
     }
 
--- src/test/java/coupon/coupon/domain/DiscountAmountTest.java
+++ src/test/java/coupon/coupon/domain/DiscountAmountTest.java
@@ -1,7 +1,6 @@
 package coupon.coupon.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,10 +12,9 @@ class DiscountAmountTest {
     void should_throw_exception_when_discount_amount_less_than_1000() {
         // given
         int discountAmount = 999;
-        int minimumOrderAmount = 10000;
 
         // when & then
-        assertThatThrownBy(() -> new DiscountAmount(discountAmount, minimumOrderAmount))
+        assertThatThrownBy(() -> new DiscountAmount(discountAmount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("할인 금액은 1,000원 이상이어야 합니다.");
     }
@@ -26,10 +24,9 @@ class DiscountAmountTest {
     void should_throw_exception_when_discount_amount_more_than_10000() {
         // given
         int discountAmount = 10001;
-        int minimumOrderAmount = 100000;
 
         // when & then
-        assertThatThrownBy(() -> new DiscountAmount(discountAmount, minimumOrderAmount))
+        assertThatThrownBy(() -> new DiscountAmount(discountAmount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("할인 금액은 10,000원 이하여야 합니다.");
     }
@@ -39,14 +36,14 @@ class DiscountAmountTest {
     void should_throw_exception_when_discount_amount_unit_not_500() {
         // given
         int discountAmount = 9999;
-        int minimumOrderAmount = 100000;
 
         // when & then
-        assertThatThrownBy(() -> new DiscountAmount(discountAmount, minimumOrderAmount))
+        assertThatThrownBy(() -> new DiscountAmount(discountAmount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("할인 금액은 500원 단위로만 설정할 수 있습니다.");
     }
 
+    /*
     @Test
     @DisplayName("할인율이 3% 미만이면 예외가 발생한다.")
     void should_throw_exception_when_discount_rate_less_than_3_percent() {
@@ -72,4 +69,5 @@ class DiscountAmountTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("할인율은 20% 이하여야 합니다.");
     }
+    */
 }
