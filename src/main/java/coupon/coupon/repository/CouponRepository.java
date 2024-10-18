1e46d4d53427a6e8b63484ba05715a3cdef3b552
masonkimseoul <masonkimseoul@gmail.com>
2024-10-18 14:42:16 +0900

    feat: CouponRepository 생성

---
+++ src/main/java/coupon/coupon/repository/CouponRepository.java
@@ -0,0 +1,9 @@
+package coupon.coupon.repository;
+
+import coupon.coupon.domain.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+}
