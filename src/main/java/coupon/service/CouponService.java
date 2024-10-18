8bcaf03a046dcda863610234dce2d3c270aced6c
3juhwan <13selfesteem91@naver.com>
2024-10-18 16:53:50 +0900

    feat: 쿠폰 서비스, 리포지토리 추가

---
+++ src/main/java/coupon/service/CouponService.java
@@ -0,0 +1,25 @@
+package coupon.service;
+
+import coupon.domain.Coupon;
+import coupon.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    @Transactional
+    public void create(Coupon coupon) {
+        couponRepository.save(coupon);
+    }
+
+    @Transactional
+    public Coupon getCoupon(Long id) {
+        return couponRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 쿠폰입니다."));
+    }
+}
