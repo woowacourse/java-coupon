7b5de2e112b1d7745a1a272b9cf98edaa7009db2
eun-byeol <joeunbyeol98@gmail.com>
2024-10-18 14:13:35 +0900

    chore: 로깅 추가

--- src/main/java/coupon/config/ReadOnlyDataSourceRouter.java
+++ src/main/java/coupon/config/ReadOnlyDataSourceRouter.java
@@ -1,15 +1,19 @@
 package coupon.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 public class ReadOnlyDataSourceRouter extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            log.info("-- READ DB 연결");
             return DataSourceType.READER;
         }
+        log.info("-- WRITE DB 연결");
         return DataSourceType.WRITER;
     }
 }
