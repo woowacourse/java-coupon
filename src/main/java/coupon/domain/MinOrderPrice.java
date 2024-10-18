a7aa332632381a4a9202aeee90185b4a9df69abe
devhoya97 <diajbh123@gmail.com>
2024-10-18 11:16:17 +0900

    feat: 범위 조건을 고려한 최소 주문 금액 구현

---
+++ src/main/java/coupon/domain/MinOrderPrice.java
@@ -0,0 +1,24 @@
+package coupon.domain;
+
+public class MinOrderPrice {
+
+    private static final int MIN_VALUE = 5000;
+    private static final int MAX_VALUE = 100000;
+
+    private final int price;
+
+    public MinOrderPrice(int price) {
+        validateRange(price);
+        this.price = price;
+    }
+
+    private void validateRange(int price) {
+        if (price < MIN_VALUE || price > MAX_VALUE) {
+            throw new IllegalArgumentException("최소 주문 금액은 5,000원 이상 100,000원 이하여야 합니다.");
+        }
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
