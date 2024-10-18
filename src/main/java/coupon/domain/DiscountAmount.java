285a2a0365718b757a7aa5540a58e12a14f64d1c
ay-eonii <naayen@naver.com>
2024-10-18 09:51:50 +0900

    feat: 할인금액 단위 제약조건 추가

--- src/main/java/coupon/domain/DiscountAmount.java
+++ src/main/java/coupon/domain/DiscountAmount.java
@@ -4,17 +4,25 @@ public class DiscountAmount {
 
     private static final int MIN_DISCOUNT_AMOUNT_RANGE = 1000;
     private static final int MAX_DISCOUNT_AMOUNT_RANGE = 10000;
+    private static final int DISCOUNT_AMOUNT_UNIT = 500;
 
     private final int amount;
 
     public DiscountAmount(int amount) {
         validateRange(amount);
+        validateUnit(amount);
         this.amount = amount;
     }
 
     private void validateRange(int amount) {
         if (amount < MIN_DISCOUNT_AMOUNT_RANGE || amount > MAX_DISCOUNT_AMOUNT_RANGE) {
             throw new IllegalArgumentException("유효하지 않은 할인 금액입니다.");
+        }
+    }
+
+    private void validateUnit(int amount) {
+        if (amount % DISCOUNT_AMOUNT_UNIT != 0) {
+            throw new IllegalArgumentException("유효하지 않은 할인 금액 단위입니다.");
         }
     }
 }
--- src/test/java/coupon/domain/DiscountAmountTest.java
+++ src/test/java/coupon/domain/DiscountAmountTest.java
@@ -10,10 +10,19 @@ class DiscountAmountTest {
 
     @DisplayName("할인 금액의 범위를 넘거가면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(ints = {999, 10001})
+    @ValueSource(ints = {500, 10500})
     void throwException_outRange(int amount) {
         assertThatThrownBy(() -> new DiscountAmount(amount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("유효하지 않은 할인 금액입니다.");
+    }
+
+    @DisplayName("유효하지 않은 단위의 할인금액인 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1001, 9999})
+    void throwException_invalidUnit(int amount) {
+        assertThatThrownBy(() -> new DiscountAmount(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효하지 않은 할인 금액 단위입니다.");
     }
 }
