7da06bb95f2fc0744ab04ebc47d5b22caa1dc1da
Gyeongho Yang <chchch1213@naver.com>
2024-10-18 10:38:41 +0900

    feat: add debug log for checking determining

--- src/main/java/coupon/config/RoutingDataSource.java
+++ src/main/java/coupon/config/RoutingDataSource.java
@@ -1,5 +1,7 @@
 package coupon.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
@@ -7,13 +9,16 @@ public class RoutingDataSource extends AbstractRoutingDataSource {
 
     public static final String READER_SERVER = "reader";
     public static final String WRITER_SERVER = "writer";
+    private static final Logger logger = LoggerFactory.getLogger(RoutingDataSource.class);
 
     @Override
     protected Object determineCurrentLookupKey() {
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            logger.debug("Using Reader DataSource");
             return READER_SERVER;
         }
 
+        logger.debug("Using Writer DataSource");
         return WRITER_SERVER;
     }
 }
