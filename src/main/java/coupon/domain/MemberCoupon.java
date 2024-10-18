3dde60ffdfdc7092eb1265105c64ca5e8a725335
3juhwan <13selfesteem91@naver.com>
2024-10-18 16:22:01 +0900

    feat: 필요한 도메인 추가

---
+++ src/main/java/coupon/domain/MemberCoupon.java
@@ -0,0 +1,53 @@
+package coupon.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member_coupon")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberCoupon {
+
+    /*
+    회원에게 발급된 쿠폰은 발급일 포함 7일 동안 사용 가능하다. 만료 일의 23:59:59.999999 까지 사용할 수 있다.
+     */
+    private static final int EXPIRATION_DAYS = 7;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Coupon coupon;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Member member;
+
+    @Column(name = "used", columnDefinition = "boolean")
+    private boolean used;
+
+    @Column(name = "issued_at", columnDefinition = "datetime(6)")
+    private LocalDateTime issuedAt;
+
+    @Column(name = "expired_at", columnDefinition = "datetime(6)")
+    private LocalDateTime expiredAt;
+
+    public MemberCoupon(Coupon coupon, Member member) {
+        this.coupon = coupon;
+        this.member = member;
+        this.used = false;
+        this.issuedAt = LocalDateTime.now();
+        this.expiredAt = issuedAt.plusDays(EXPIRATION_DAYS - 1).with(LocalDateTime.MAX);
+    }
+}
