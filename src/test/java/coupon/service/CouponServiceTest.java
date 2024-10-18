4c616f7b0b4c132ea0cdc9656966b98f8d6fa222
Choo <choo000407@naver.com>
2024-10-18 16:36:12 +0900

    test: 복제 지연으로 인한 데이터 조회 실패 테스트 추가

--- src/test/java/coupon/service/CouponServiceTest.java
+++ src/test/java/coupon/service/CouponServiceTest.java
@@ -4,12 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import coupon.entity.Coupon;
 import coupon.entity.CouponCategory;
+import coupon.exception.CouponNotFoundException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 
 public class CouponServiceTest extends ServiceTest {
+
+    private static final Logger log = LoggerFactory.getLogger(CouponServiceTest.class);
 
     @Autowired
     private CouponService couponService;
@@ -28,5 +35,39 @@ public class CouponServiceTest extends ServiceTest {
 
         // when & then
         assertThat(couponService.create(coupon)).isEqualTo(1L);
+    }
+
+    @Test
+    void 생성하자마자_쿠폰을_조회할_때_복제_지연이_발생한다() {
+        // given
+        LocalDateTime stamp = LocalDateTime.now();
+        String rightName = "복제지연" + stamp;
+        Coupon coupon = new Coupon(
+                rightName,
+                BigDecimal.valueOf(1000),
+                BigDecimal.valueOf(10000),
+                CouponCategory.FOOD,
+                LocalDate.of(2000, 4, 7),
+                LocalDate.of(2000, 4, 12)
+        );
+        couponService.create(coupon);
+
+        // when && then
+        /**
+         * read db는 초기화 바로 안되서 이전 생성 결과 남아있을 수 있음
+         * 이전 생성 결과에 영향 받지 않기 위해 조회 혹시 되는 경우엔 stamp 값으로 이름 확인함
+         * */
+        try {
+            Coupon savedCoupon = couponService.getCoupon(coupon.getId());
+            String wrongName = savedCoupon.getName().getName();
+            assertThat(wrongName).isNotEqualTo(rightName);
+            log.info("EntityManager가 인지한 id 값: " + savedCoupon.getId());
+            log.info("복제지연 테스트 성공: 30초 전에 생성한 결과 남아있어서 조회는 되지만, 방금 저장한 값이 아님");
+            log.info("readDB에 남아있어 잘못 조회된 값: " + wrongName + " 방금 저장한 값: " + rightName);
+        } catch (InvalidDataAccessResourceUsageException e) {
+            log.info("복제지연 테스트 성공: 최초 실행이라 테이블이 아예 없는 상태임");
+        } catch (CouponNotFoundException e) {
+            log.info("복제지연 테스트 성공: 테이블 초기화 성공한 상태라 테이블에 값이 아예 없는 상태임");
+        }
     }
 }
