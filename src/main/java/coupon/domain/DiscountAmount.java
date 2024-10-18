8ee813e16a48d0b91db30f09d3c9e194c489175b
gyungchan Jo <namejgc@naver.com>
2024-10-18 11:24:32 +0900

    feat: 할인 금액 단위 검증 기능 구현

--- src/main/java/coupon/domain/DiscountAmount.java
+++ src/main/java/coupon/domain/DiscountAmount.java
@@ -6,6 +6,8 @@ public class DiscountAmount {
 
     private static final int MIN_DISCOUNT_AMOUNT = 1_000;
     private static final int MAX_DISCOUNT_AMOUNT = 10_000;
+    private static final int DISCOUNT_AMOUNT_UNIT = 500;
+    private static final int VALID_REMAINING_DISCOUNT_AMOUNT = 0;
 
     private final int discountAmount;
 
@@ -18,6 +20,13 @@ public class DiscountAmount {
         if (discountAmount < MIN_DISCOUNT_AMOUNT || MAX_DISCOUNT_AMOUNT < discountAmount) {
             throw new IllegalArgumentException("할인 금액은 %,d원 이상 ~ %,d원 이하여야 합니다.".formatted(MIN_DISCOUNT_AMOUNT, MAX_DISCOUNT_AMOUNT));
         }
+        if (hasInvalidDiscountAmountUnit(discountAmount)) {
+            throw new IllegalArgumentException("할인 금액은 %,d원 단위어야 합니다.".formatted(DISCOUNT_AMOUNT_UNIT));
+        }
+    }
+
+    private boolean hasInvalidDiscountAmountUnit(int discountAmount) {
+        return discountAmount % DISCOUNT_AMOUNT_UNIT != VALID_REMAINING_DISCOUNT_AMOUNT;
     }
 
     @Override
--- src/test/java/coupon/domain/DiscountAmountTest.java
+++ src/test/java/coupon/domain/DiscountAmountTest.java
@@ -1,12 +1,12 @@
 package coupon.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DiscountAmountTest {
 
@@ -26,5 +26,21 @@ class DiscountAmountTest {
         assertThatThrownBy(() -> new DiscountAmount(amount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("할인 금액은 1,000원 이상 ~ 10,000원 이하여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("할인 금액은 500원 단위로 설정할 수 있다.")
+    void validDiscountAmountUnit() {
+        DiscountAmount discountAmount = new DiscountAmount(1500);
+
+        assertThat(discountAmount).isEqualTo(new DiscountAmount(1500));
+    }
+
+    @Test
+    @DisplayName("할인 금액이 500원 단위가 아니라면 예외가 발생한다.")
+    void invalidDiscountAmountUnit() {
+        assertThatThrownBy(() -> new DiscountAmount(1100))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("할인 금액은 500원 단위어야 합니다.");
     }
 }
