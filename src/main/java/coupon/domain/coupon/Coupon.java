8a21b068be9b4071e3278c7df35b87ef6d0ca397
slimsha2dy <chy2581@naver.com>
2024-10-18 16:46:41 +0900

    feat: 할인율 검증 추가

--- src/main/java/coupon/domain/coupon/Coupon.java
+++ src/main/java/coupon/domain/coupon/Coupon.java
@@ -15,6 +15,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Coupon {
 
+    private static final int MINIMUM_DISCOUNT_RATE = 3;
+    private static final int MAXIMUM_DISCOUNT_RATE = 20;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,10 +33,19 @@ public class Coupon {
 
     public Coupon(CouponName couponName, DiscountMount discountMount, MinimumMount minimumMount, Category category,
                   Period period) {
+        validateDiscountRate(discountMount, minimumMount);
         this.couponName = couponName;
         this.discountMount = discountMount;
         this.minimumMount = minimumMount;
         this.category = category;
         this.period = period;
+    }
+
+    private void validateDiscountRate(DiscountMount discountMount, MinimumMount minimumMount) {
+        float discountRate = (float) discountMount.getDiscountMount() / minimumMount.getMinimumMount() * 100;
+        int truncatedDiscountRate = (int) discountRate;
+        if (truncatedDiscountRate < MINIMUM_DISCOUNT_RATE || truncatedDiscountRate > MAXIMUM_DISCOUNT_RATE) {
+            throw new IllegalArgumentException("쿠폰 할인율은 3% 이상, 20% 이하여야 합니다.");
+        }
     }
 }
