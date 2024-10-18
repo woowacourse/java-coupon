922a6161e09baa02ee7fc7720faa46b68695703e
seunghye218 <seunghye@student.42seoul.kr>
2024-10-18 16:52:44 +0900

    chore: 패키지 변경

--- src/main/java/coupon/domain/CouponRepository.java
+++
@@ -1,9 +0,0 @@
-package coupon.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-
-}
