bdf6a750e2ced54e56d676000f74032b6465c7b3
ikjo39 <chi4321@naver.com>
2024-10-18 11:57:45 +0900

    feat: 각 datasource 에 대한 로그 추가

--- src/main/java/coupon/config/RoutingDataSource.java
+++ src/main/java/coupon/config/RoutingDataSource.java
@@ -1,15 +1,21 @@
 package coupon.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 public class RoutingDataSource extends AbstractRoutingDataSource {
 
+    private static final Logger log = LoggerFactory.getLogger(RoutingDataSource.class);
+
     @Override
     protected Object determineCurrentLookupKey() {
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            log.info("USE REPLICA");
             return DataSourceConfig.REPLICA_SERVER;
         }
+        log.info("USE SOURCE");
         return DataSourceConfig.SOURCE_SERVER;
     }
 }
