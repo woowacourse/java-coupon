fe89c89fe7243f12e82b0a3800884cb71714ecf7
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-18 15:52:03 +0900

    feat(TransactionRouter): WriteDB로 요청을 보내는 객체 추가

---
+++ src/main/java/coupon/support/datasource/TransactionRouter.java
@@ -0,0 +1,16 @@
+package coupon.support.datasource;
+
+import java.util.function.Supplier;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class TransactionRouter {
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public <T> T routeWrite(final Supplier<T> action) {
+        return action.get();
+    }
+}
