4d3908ab2f2fba9e5bc3ef50628d4396edc5cada
ay-eonii <naayen@naver.com>
2024-10-18 09:29:20 +0900

    docs: 기능 요구 사항 작성

---
+++ src/test/java/coupon/CouponServiceTest.java
@@ -0,0 +1,21 @@
+//package coupon;
+//
+//import coupon.domain.Coupon;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class CouponServiceTest {
+//
+//    @Autowired
+//    private CouponService couponService;
+//
+//    @Test
+//    void 복제지연테스트() {
+//        Coupon coupon = new Coupon(1000, 10000);
+//        couponService.create(coupon);
+//        Coupon savedCoupon = couponService.getCoupon(coupon.getId());
+//        assertThat(savedCoupon).isNotNull();
+//    }
+//}
