678ef500816ab60e7885bcc892c66c527a185814
gyungchan Jo <namejgc@naver.com>
2024-10-18 12:25:34 +0900

    feat: 카테고리 구현

---
+++ src/main/java/coupon/domain/Category.java
@@ -0,0 +1,29 @@
+package coupon.domain;
+
+import java.util.Arrays;
+
+public enum Category {
+
+    FASHION("패션"),
+    HOME_APPLIANCES("가전"),
+    FURNITURE("가구"),
+    FOOD("식품"),
+    ;
+
+    private final String category;
+
+    Category(String category) {
+        this.category = category;
+    }
+
+    public static Category from(String category) {
+        return Arrays.stream(values())
+                .filter(c -> c.isCategoryMatch(category))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("일치하는 카테고리가 없습니다. 입력된 카테고리 : %s".formatted(category)));
+    }
+
+    private boolean isCategoryMatch(String category) {
+        return this.category.equals(category);
+    }
+}
