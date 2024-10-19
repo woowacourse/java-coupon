232e7b479fe4ba4dccdc5d8c20c004712bd83037
leegwichan <cnddkscndgus@naver.com>
2024-10-20 01:14:44 +0900

    fix: DataSourceAspect 를 Bean 으로 등록하도록 수정

--- src/main/java/coupon/config/datasource/DataSourceAspect.java
+++ src/main/java/coupon/config/datasource/DataSourceAspect.java
@@ -2,18 +2,20 @@ package coupon.config.datasource;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Component
 @Aspect
 public class DataSourceAspect {
 
-    @Before("@annotation(coupon.config.datasource.UsingWriterSource)")
+    @Before("@annotation(org.springframework.transaction.annotation.Transactional) && @annotation(coupon.config.datasource.UsingWriterSource)")
     public void setWriterDataSource() {
         DataSourceContextHolder.setDataSourceType(DataSourceType.WRITER);
     }
 
     @Before("@annotation(org.springframework.transaction.annotation.Transactional) && !@annotation(coupon.config.datasource.UsingWriterSource)")
-    public void setReadDataSource() {
+    public void setTransactionDataSource() {
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
             DataSourceContextHolder.setDataSourceType(DataSourceType.READER);
             return;
