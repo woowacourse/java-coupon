e6048a839629c3fff3f2a40af0b488ed385ddaf7
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-10-20 18:07:51 +0900

    refactor: 서비스 테스트에서 웹 서버 실행 방지

--- src/test/java/coupon/coupon/service/CouponServiceTest.java
+++ src/test/java/coupon/coupon/service/CouponServiceTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class CouponServiceTest {
 
     @Autowired
