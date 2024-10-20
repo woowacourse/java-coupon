b2a3772df72ea832f18362b2c5835b2c1547da1b
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-21 03:57:56 +0900

    test: 용어 수정 할인비율 > 할인율

--- src/test/java/coupon/domain/DiscountRatioTest.java
+++ src/test/java/coupon/domain/DiscountRatioTest.java
@@ -11,21 +11,21 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class DiscountRatioTest {
 
-    @DisplayName("최대 할인 비율을 초과할 수 없다")
+    @DisplayName("최대 할인율을 초과할 수 없다")
     @Test
     void throwIllegalArgumentException_When_ExceededThanMaxRatio() {
         assertThatThrownBy(() -> new DiscountRatio(21))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("최소 할인 비율 미만일 수 없다")
+    @DisplayName("최소 할인율 미만일 수 없다")
     @Test
     void throwIllegalArgumentException_When_ShorterThanMinRatio() {
         assertThatThrownBy(() -> new DiscountRatio(2))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("성공: 할인 비율을 생성할 수 있다")
+    @DisplayName("성공: 할인율을 생성할 수 있다")
     @ValueSource(ints = {20, 3, 19, 4})
     @ParameterizedTest
     void createSalePrice(int validSaleRatio) {
