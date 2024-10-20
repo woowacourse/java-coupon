c65f51ab42167c23446999264817ec21f2cc50f4
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-21 05:02:39 +0900

    chore: init.sql 수정

--- docker/init/init.sql
+++ docker/init/init.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS coupon
 (
     id               BIGINT AUTO_INCREMENT PRIMARY KEY,
     coupon_name      varchar(30)  NOT NULL,
-    discount_price       INT          NOT NULL,
+    discount_price   INT          NOT NULL,
     category         varchar(256) NOT NULL,
     sale_order_price INT          NOT NULL,
-    start_at       datetime     NOT NULL,
-    end_at         datetime     NOT NULL
+    start_at         datetime     NOT NULL,
+    end_at           datetime     NOT NULL
 );
