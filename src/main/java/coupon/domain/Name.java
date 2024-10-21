5c5c7810723199b18855422b50b1d92de197619c
woojong <dwax1324@gmail.com>
2024-10-21 21:52:39 +0900

    feat: 이름 도메인 추가

---
+++ src/main/java/coupon/domain/Name.java
@@ -0,0 +1,24 @@
+package coupon.domain;
+
+import coupon.exception.NameLengthExceedException;
+import coupon.exception.NameNotExistException;
+
+public class Name {
+    private static final int MAX_LENGTH = 30;
+
+    private final String value;
+
+    public Name(String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(String value) {
+        if (value == null || value.isBlank()) {
+            throw new NameNotExistException();
+        }
+        if (value.length() > MAX_LENGTH) {
+            throw new NameLengthExceedException(MAX_LENGTH);
+        }
+    }
+}
