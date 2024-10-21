02117f845ddf8cbd0793f7a60c85543672b0f147
fromitive <fromitive@gmail.com>
2024-10-21 16:32:43 +0900

    feat: 패스워드 해시화 객체 생성

---
+++ src/main/java/member/domain/HashedPasswordCreator.java
@@ -0,0 +1,25 @@
+package member.domain;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class HashedPasswordCreator {
+
+    private static final String HASH_ALGORITHM_TYPE = "SHA-256";
+
+    public Password createPassword(String plainPassword, String salt) {
+        return new Password(hashPassword(plainPassword, salt));
+    }
+
+    private String hashPassword(String password, String salt) {
+        try {
+            String saltedPassword = password + salt;
+            MessageDigest md = MessageDigest.getInstance(HASH_ALGORITHM_TYPE);
+            byte[] hashedPassword = md.digest(saltedPassword.getBytes());
+            return Base64.getEncoder().encodeToString(hashedPassword);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
