c679f7acec8a362f19346cbe1fc096f7c6379ffa
fromitive <fromitive@gmail.com>
2024-10-21 16:38:24 +0900

    feat: salt 생성 기능 구현

---
+++ src/main/java/member/domain/SaltGenerator.java
@@ -0,0 +1,21 @@
+package member.domain;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class SaltGenerator {
+
+    private static final int SALT_BYTE_LENGTH = 16;
+
+    public String generate() {
+        byte[] salt = new byte[SALT_BYTE_LENGTH];
+        try {
+            SecureRandom sr = SecureRandom.getInstanceStrong();
+            sr.nextBytes(salt);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return Base64.getEncoder().encodeToString(salt);
+    }
+}
