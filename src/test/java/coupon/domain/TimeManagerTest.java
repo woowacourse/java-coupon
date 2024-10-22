2a725ca734664e61c80ddfc5a8b70c163fe9e476
woojong <dwax1324@gmail.com>
2024-10-22 11:05:41 +0900

    test: 이후의 날에 발급이 가능하다

--- src/test/java/coupon/domain/TimeManagerTest.java
+++ src/test/java/coupon/domain/TimeManagerTest.java
@@ -6,13 +6,25 @@ import java.util.Date;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class TimeManagerTest {
 
     @DisplayName("같은 날에는 발급이 가능하다")
     @Test
-    void isEffective() {
+    void isEffectiveSameDay() {
         TimeManager timeManager = new TimeManager(LocalDate.of(2024, 10, 22), LocalDate.of(2024, 10, 22));
         Assertions.assertThat(timeManager.isIssuable()).isTrue();
     }
+
+    @DisplayName("이후의 날에 발급이 가능하다")
+    @ParameterizedTest
+    @CsvSource(value = {"2024-10-22","2024-10-23","2024-12-31","2042-1-1"},delimiter = '-')
+    void isEffectiveAfter(int year, int month, int day) {
+        TimeManager timeManager = new TimeManager(LocalDate.of(2024, 10, 22), LocalDate.of(year, month, day));
+        Assertions.assertThat(timeManager.isIssuable()).isTrue();
+    }
+
+
 }
