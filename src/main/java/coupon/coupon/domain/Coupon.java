496f3b59cb47634c402279aa551cafc43afdcf90
eunjungL <dldmswjd322@gmail.com>
2024-10-22 13:21:59 +0900

    refactor: Coupon 제약 조건 상수화

--- src/main/java/coupon/coupon/domain/Coupon.java
+++ src/main/java/coupon/coupon/domain/Coupon.java
@@ -15,11 +15,20 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Coupon {
 
+    private static final int MAX_NAME_LENGTH = 30;
+    private static final int MIN_DISCOUNT_AMOUNT = 1000;
+    private static final int MAX_DISCOUNT_AMOUNT = 10000;
+    private static final int DISCOUNT_AMOUNT_UNIT = 500;
+    private static final int MIN_ORDER_AMOUNT = 5000;
+    private static final int MAX_ORDER_AMOUNT = 100000;
+    private static final int MIN_DISCOUNT_RATE = 3;
+    private static final int MAX_DISCOUNT_RATE = 20;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 30, nullable = false)
+    @Column(length = MAX_NAME_LENGTH, nullable = false)
     private String name;
 
     @Column(nullable = false)
@@ -71,30 +80,30 @@ public class Coupon {
     }
 
     private void validateName(String name) {
-        if (name.length() > 30) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("쿠폰의 이름은 30자 이하여야 합니다.");
         }
     }
 
     private void validateDiscountAmount(Integer discountAmount) {
-        if (discountAmount < 1000 || 10000 < discountAmount) {
+        if (discountAmount < MIN_DISCOUNT_AMOUNT || MAX_DISCOUNT_AMOUNT < discountAmount) {
             throw new IllegalArgumentException("쿠폰의 할인 금액은 1000원 이상 10000원 이하여야 합니다.");
         }
 
-        if (discountAmount % 500 != 0) {
+        if (discountAmount % DISCOUNT_AMOUNT_UNIT != 0) {
             throw new IllegalArgumentException("쿠폰의 할인 금액은 500원 단위로 설정해야 합니다.");
         }
     }
 
     private void validateMinOrderAmount(Integer minOrderAmount) {
-        if (minOrderAmount < 5000 || 100000 < minOrderAmount) {
+        if (minOrderAmount < MIN_ORDER_AMOUNT || MAX_ORDER_AMOUNT < minOrderAmount) {
             throw new IllegalArgumentException("쿠폰의 최소 주문 금액은 5000원 이상 100000원 이하여야 합니다.");
         }
     }
 
     private void validateDiscountRate(int discountAmount, int minOrderAmount) {
         long discountRate = Math.round((double) discountAmount / minOrderAmount * 100);
-        if (discountRate < 3 || 20 < discountRate) {
+        if (discountRate < MIN_DISCOUNT_RATE || MAX_DISCOUNT_RATE < discountRate) {
             throw new IllegalArgumentException("쿠폰의 할인율은 3% 이상 20% 이하여야 합니다.");
         }
     }
