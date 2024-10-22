066ccdee88073e97c952168835f4685f0c3fc37d
juha <khabh@naver.com>
2024-10-22 10:59:12 +0900

    refactor: CouponService에 couponLookupService 적용

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -12,23 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class CouponService {
 
     private final CouponRepository couponRepository;
-    private final CouponCache couponCache;
+    private final CouponLookupService couponLookupService;
 
     @Transactional
     public void create(Coupon coupon) {
         couponRepository.save(coupon);
-        couponCache.cache(coupon);
     }
 
-    public Coupon getCoupon(Long id) {
-        Coupon cachedCoupon = couponCache.get(id);
-        if (cachedCoupon != null) {
-            return cachedCoupon;
-        }
-
-        Coupon coupon = couponRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("쿠폰이 존재하지 않습니다."));
-        couponCache.cache(coupon);
-        return coupon;
+    public Coupon getCoupon(Long couponId) {
+        return couponLookupService.findById(couponId);
     }
 }
