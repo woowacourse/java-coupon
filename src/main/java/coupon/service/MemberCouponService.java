27d3a5bc8d203eceff9055114b13ac3780fc8c9c
juha <khabh@naver.com>
2024-10-22 11:05:03 +0900

    feat: 회원 쿠폰 조회 기능 구현

--- src/main/java/coupon/service/MemberCouponService.java
+++ src/main/java/coupon/service/MemberCouponService.java
@@ -1,12 +1,14 @@
 package coupon.service;
 
+import coupon.domain.Coupon;
 import coupon.domain.MemberCoupon;
-import coupon.domain.repository.CouponRepository;
 import coupon.domain.repository.MemberCouponRepository;
 import coupon.service.dto.MemberCouponDto;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
-import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,8 +20,7 @@ public class MemberCouponService {
     private static final int MAX_MEMBER_COUPON_COUNT = 5;
 
     private MemberCouponRepository memberCouponRepository;
-    private CouponRepository couponRepository;
-    private CacheManager cacheManager;
+    private CouponLookupService couponLookupService;
 
     public void create(MemberCoupon memberCoupon) {
         validateMemberIssueLimit(memberCoupon);
@@ -35,5 +36,30 @@ public class MemberCouponService {
         if (memberCouponCount >= MAX_MEMBER_COUPON_COUNT) {
             throw new IllegalArgumentException("한 명의 회원은 동일한 쿠폰을 최대 5장까지 발급할 수 있습니다.");
         }
+    }
+
+    public List<MemberCouponDto> findByMemberId(Long memberId) {
+        List<MemberCoupon> memberCoupons = memberCouponRepository.findAllByMemberId(memberId);
+        return toMemberCouponDtos(memberCoupons);
+    }
+
+    private List<MemberCouponDto> toMemberCouponDtos(List<MemberCoupon> memberCoupons) {
+        Map<Long, Coupon> couponsWithId = getCouponsWithId(memberCoupons);
+        return convertToMemberCouponDtos(memberCoupons, couponsWithId);
+    }
+
+    private Map<Long, Coupon> getCouponsWithId(List<MemberCoupon> memberCoupons) {
+        return couponLookupService.findByMemberCoupons(memberCoupons)
+                .stream()
+                .collect(Collectors.toMap(Coupon::getId, Function.identity()));
+    }
+
+    private List<MemberCouponDto> convertToMemberCouponDtos(List<MemberCoupon> memberCoupons, Map<Long, Coupon> coupons) {
+        return memberCoupons.stream()
+                .map(memberCoupon -> {
+                    Coupon coupon = coupons.get(memberCoupon.getCouponId());
+                    return MemberCouponDto.from(memberCoupon, coupon);
+                })
+                .toList();
     }
 }
