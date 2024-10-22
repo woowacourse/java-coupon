749d270d75feea1cb1d7eed3f4b2fe0c154f6984
Eugene Jang <kyunellroll@gmail.com>
2024-10-22 12:42:56 +0900

    test(PriceInformationTest): 선언된 상수를 사용하도록 수정, 할인율 범위 확인

--- src/test/java/coupon/domain/coupon/PriceInformationTest.java
+++ src/test/java/coupon/domain/coupon/PriceInformationTest.java
@@ -5,14 +5,18 @@ import coupon.common.exception.CouponException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-class PriceInformationTest {
+class
+
+PriceInformationTest {
 
     @Test
     @DisplayName("총 할인율이 가능한 범위가 아닌 1% 인 경우 에러를 발생한다.")
     void priceInformation_WhenNoRate() {
-        assertThatThrownBy(() -> new PriceInformation(1000, 100000))
+        assertThatThrownBy(() -> new PriceInformation(DiscountPrice.MINIMUM_PRICE, MinimumPrice.MAXIMUM_PRICE))
                 .isInstanceOf(CouponException.class)
                 .hasMessage(ErrorConstant.COUPON_PRICE_RATE_NOT_IN_RANGE.getMessage());
     }
@@ -20,8 +24,19 @@ class PriceInformationTest {
     @Test
     @DisplayName("총 할인율이 가능한 범위가 아닌 100% 인 경우 에러를 발생한다.")
     void priceInformation_When100Rate() {
-        assertThatThrownBy(() -> new PriceInformation(10000, 10000))
+        assertThatThrownBy(() -> new PriceInformation(DiscountPrice.MAXIMUM_PRICE, DiscountPrice.MAXIMUM_PRICE))
                 .isInstanceOf(CouponException.class)
                 .hasMessage(ErrorConstant.COUPON_PRICE_RATE_NOT_IN_RANGE.getMessage());
+    }
+
+    @Test
+    @DisplayName("총 할인율이 가능한 범위인 경우 에러를 발생하지 않는다.")
+    void priceInformation_WhenAvailableRate() {
+        PriceInformation priceInformation = new PriceInformation(DiscountPrice.MINIMUM_PRICE, MinimumPrice.MINIMUM_PRICE);
+
+        assertAll(
+                () -> assertThat(priceInformation.getMinimumPrice()).isEqualTo(MinimumPrice.MINIMUM_PRICE),
+                () -> assertThat(priceInformation.getDiscountPrice()).isEqualTo(DiscountPrice.MINIMUM_PRICE)
+        );
     }
 }
