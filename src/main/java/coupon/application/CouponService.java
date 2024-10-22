688ab98dfe80c8589a8a387f6f56774e9f7dc0e4
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-10-22 22:15:28 +0900

    refactor: 레디스 캐시 적용

--- src/main/java/coupon/application/CouponService.java
+++ src/main/java/coupon/application/CouponService.java
@@ -1,10 +1,11 @@
 package coupon.application;
 
 import coupon.domain.Coupon;
-import coupon.domain.CouponCache;
 import coupon.domain.CouponRepository;
 import coupon.dto.CouponResponse;
+import coupon.replication.ReplicationLag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,26 +14,29 @@ import org.springframework.transaction.annotation.Transactional;
 public class CouponService {
 
     private final CouponRepository couponRepository;
-    private final CouponCache couponCache;
 
     @Transactional
     public Long create(Coupon coupon) {
         Coupon savedCoupon = couponRepository.save(coupon);
-        couponCache.save(savedCoupon);
-
         return savedCoupon.getId();
     }
 
+    @ReplicationLag
     @Transactional(readOnly = true)
-    public CouponResponse getCoupon(Long couponId) {
-        Coupon coupon = couponCache.findById(couponId)
-                .orElseGet(() -> findCoupon(couponId));
+    public CouponResponse getCouponByAdmin(Long id) {
+        Coupon coupon = couponRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("쿠폰이 존재하지 않습니다."));
 
         return CouponResponse.from(coupon);
     }
 
-    private Coupon findCoupon(Long couponId) {
-        return couponRepository.findById(couponId)
+    @ReplicationLag
+    @Cacheable(value = "coupons", key = "#couponId")
+    @Transactional(readOnly = true)
+    public CouponResponse getCoupon(Long couponId) {
+        Coupon coupon = couponRepository.findById(couponId)
                 .orElseThrow(() -> new IllegalArgumentException("쿠폰이 존재하지 않습니다."));
+
+        return CouponResponse.from(coupon);
     }
 }
--- src/test/java/coupon/application/CouponServiceTest.java
+++ src/test/java/coupon/application/CouponServiceTest.java
@@ -5,14 +5,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import coupon.domain.Coupon;
-import coupon.domain.CouponCache;
+import coupon.domain.CouponRepository;
 import coupon.dto.CouponResponse;
 import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootTest
 class CouponServiceTest {
 
@@ -20,13 +25,25 @@ class CouponServiceTest {
     private CouponService couponService;
 
     @Autowired
-    private CouponCache couponCache;
+    private CacheManager cacheManager;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @BeforeEach
+    void setUp() {
+        couponRepository.deleteAll();
+        Cache cache = cacheManager.getCache("coupons");
+        if (cache != null) {
+            cache.clear();
+        }
+    }
 
     @Test
     void 복제지연테스트() throws Exception {
         Coupon coupon = new Coupon("CouponName", 1_000, 10_000, "가구", LocalDate.now(), LocalDate.now());
         couponService.create(coupon);
-        CouponResponse savedCoupon = couponService.getCoupon(coupon.getId());
+        CouponResponse savedCoupon = couponService.getCouponByAdmin(coupon.getId());
         assertThat(savedCoupon).isNotNull();
     }
 
@@ -38,22 +55,44 @@ class CouponServiceTest {
                 .hasMessage("쿠폰이 존재하지 않습니다.");
     }
 
-    @DisplayName("쿠폰을 생성할 때 캐시에도 저장한다.")
+    @DisplayName("쿠폰 첫 조회시 캐시에 저장된다.")
+    @Test
+    void saveCacheFirst() {
+        Coupon coupon = new Coupon("CouponName", 1_000, 10_000, "가구", LocalDate.now(), LocalDate.now());
+        Long id = couponService.create(coupon);
+        CouponResponse coupon1 = couponService.getCoupon(id);
+
+        CouponResponse cacheCoupon = cacheManager.getCache("coupons").get(id, CouponResponse.class);
+
+        assertAll(
+                () -> assertThat(cacheCoupon.id()).isEqualTo(id),
+                () -> assertThat(cacheCoupon.category()).isEqualTo(coupon.getCategory()),
+                () -> assertThat(cacheCoupon.discountAmount()).isEqualTo(coupon.getDiscountAmount()),
+                () -> assertThat(cacheCoupon.minOrderAmount()).isEqualTo(coupon.getMinOrderAmount()),
+                () -> assertThat(cacheCoupon.name()).isEqualTo(coupon.getName()),
+                () -> assertThat(cacheCoupon.startDate()).isEqualTo(coupon.getStartDate()),
+                () -> assertThat(cacheCoupon.endDate()).isEqualTo(coupon.getEndDate())
+        );
+    }
+
+    @DisplayName("쿠폰 조회 2번째부터는 캐시에서 조회되므로 DB가 삭제되어도 캐싱된 값을 반환한다.")
     @Test
     void saveCache() {
         Coupon coupon = new Coupon("CouponName", 1_000, 10_000, "가구", LocalDate.now(), LocalDate.now());
         Long id = couponService.create(coupon);
+        CouponResponse coupon1 = couponService.getCoupon(id);
+        couponRepository.deleteAll();
 
-        Coupon cacheCoupon = couponCache.findById(id).get();
+        CouponResponse cacheCoupon = couponService.getCoupon(id);
 
         assertAll(
-                () -> assertThat(cacheCoupon.getId()).isEqualTo(id),
-                () -> assertThat(cacheCoupon.getCategory()).isEqualTo(coupon.getCategory()),
-                () -> assertThat(cacheCoupon.getDiscountAmount()).isEqualTo(coupon.getDiscountAmount()),
-                () -> assertThat(cacheCoupon.getMinOrderAmount()).isEqualTo(coupon.getMinOrderAmount()),
-                () -> assertThat(cacheCoupon.getName()).isEqualTo(coupon.getName()),
-                () -> assertThat(cacheCoupon.getStartDate()).isEqualTo(coupon.getStartDate()),
-                () -> assertThat(cacheCoupon.getEndDate()).isEqualTo(coupon.getEndDate())
+                () -> assertThat(cacheCoupon.id()).isEqualTo(id),
+                () -> assertThat(cacheCoupon.category()).isEqualTo(coupon.getCategory()),
+                () -> assertThat(cacheCoupon.discountAmount()).isEqualTo(coupon.getDiscountAmount()),
+                () -> assertThat(cacheCoupon.minOrderAmount()).isEqualTo(coupon.getMinOrderAmount()),
+                () -> assertThat(cacheCoupon.name()).isEqualTo(coupon.getName()),
+                () -> assertThat(cacheCoupon.startDate()).isEqualTo(coupon.getStartDate()),
+                () -> assertThat(cacheCoupon.endDate()).isEqualTo(coupon.getEndDate())
         );
     }
 }
