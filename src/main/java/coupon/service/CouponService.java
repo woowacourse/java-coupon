0d3b3a0d881608603dd76efd30430b37c496d48e
Gyeongho Yang <chchch1213@naver.com>
2024-10-23 21:58:34 +0900

    feat: cache coupon when cache miss

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -1,11 +1,11 @@
 package coupon.service;
 
 import coupon.domain.Coupon;
-import coupon.exception.NotFoundException;
 import coupon.repository.CouponRepository;
-import coupon.util.TransactionExecutor;
+import java.util.Optional;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,14 +15,12 @@ public class CouponService {
     private static final String CACHE_KEY_COUPON = "coupons";
 
     private final CouponRepository couponRepository;
-    private final TransactionExecutor transactionExecutor;
 
-    public CouponService(CouponRepository couponRepository, TransactionExecutor transactionExecutor) {
+    public CouponService(CouponRepository couponRepository) {
         this.couponRepository = couponRepository;
-        this.transactionExecutor = transactionExecutor;
     }
 
-    @CachePut(value = CACHE_KEY_COUPON, key = "#coupon.id")
+    @CachePut(value = CACHE_KEY_COUPON, key = "#result.id")
     @Transactional
     public Coupon create(Coupon coupon) {
         return couponRepository.save(coupon);
@@ -30,15 +28,7 @@ public class CouponService {
 
     @Cacheable(value = CACHE_KEY_COUPON, key = "#id")
     @Transactional(readOnly = true)
-    public Coupon getCoupon(long id) {
-        return couponRepository.findById(id)
-                .orElseGet(() -> findCouponWithNonReadonlyTransaction(id));
-    }
-
-    private Coupon findCouponWithNonReadonlyTransaction(long id) {
-        return transactionExecutor.executeOnWriter(
-                () -> couponRepository.findById(id)
-                        .orElseThrow(() -> new NotFoundException("쿠폰이 존재하지 않습니다. id: " + id))
-        );
+    public Optional<Coupon> getCoupon(@Param("id") long id) {
+        return couponRepository.findById(id);
     }
 }
