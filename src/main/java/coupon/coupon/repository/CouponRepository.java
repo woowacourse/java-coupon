c38379951472d6fdbf757773662a3974db3ad885
masonkimseoul <masonkimseoul@gmail.com>
2024-10-24 02:27:39 +0900

    refactor: 불필요한 어노테이션 제거

--- src/main/java/coupon/coupon/repository/CouponRepository.java
+++ src/main/java/coupon/coupon/repository/CouponRepository.java
@@ -2,8 +2,6 @@ package coupon.coupon.repository;
 
 import coupon.coupon.domain.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 }
