5a92642d1d268d86decf0058c2ccfa3466a11040
김민수 MINSOO KIM <alstn113@gmail.com>
2024-10-23 14:08:03 +0900

    feat: CacheManager 작성

--- src/main/java/coupon/coupon/domain/CouponRepository.java
+++ src/main/java/coupon/coupon/domain/CouponRepository.java
@@ -1,13 +1,12 @@
 package coupon.coupon.domain;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface CouponRepository extends Repository<Coupon, Long> {
 
-    void save(Coupon coupon);
+    Coupon save(Coupon coupon);
 
     Optional<Coupon> findById(Long id);
 
