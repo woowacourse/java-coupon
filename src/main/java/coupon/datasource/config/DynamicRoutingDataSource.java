12c7e45ca62d0752acee2034a799b7a6307286da
eunjungL <dldmswjd322@gmail.com>
2024-10-23 18:59:07 +0900

    feat: DataSource routing 로깅 정보 추가

--- src/main/java/coupon/datasource/config/DynamicRoutingDataSource.java
+++ src/main/java/coupon/datasource/config/DynamicRoutingDataSource.java
@@ -1,21 +1,26 @@
 package coupon.datasource.config;
 
 import coupon.datasource.DataSourceRoutingContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 public class DynamicRoutingDataSource extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
         if (DataSourceRoutingContext.isWrite()) {
+            log.info("Routing at writer");
             return "writer";
         }
 
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            log.info("Routing at reader");
             return "reader";
         }
 
+        log.info("Routing at writer");
         return "writer";
     }
 }
