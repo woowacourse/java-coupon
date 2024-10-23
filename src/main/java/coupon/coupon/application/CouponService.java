b3f4c93a6df96a6d1172cb9f62694bd4782bb9fa
김민수 MINSOO KIM <alstn113@gmail.com>
2024-10-23 15:24:02 +0900

    feat: CacheFindByIdsInResponse 작성

--- src/main/java/coupon/coupon/application/CouponService.java
+++ src/main/java/coupon/coupon/application/CouponService.java
@@ -1,12 +1,14 @@
 package coupon.coupon.application;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import coupon.coupon.domain.Coupon;
 import coupon.coupon.domain.CouponRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +18,7 @@ public class CouponService {
 
     private final CouponRepository couponRepository;
     private final CouponMapper couponMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Transactional(readOnly = true)
     public List<CouponResponse> getCoupons() {
@@ -25,27 +28,62 @@ public class CouponService {
     }
 
     @Transactional(readOnly = true)
+    @Cacheable(value = "coupon", key = "#couponId")
     public CouponResponse getCoupon(Long couponId) {
-        String message = "존재하지 않는 쿠폰입니다. id: %d".formatted(couponId);
+        Coupon coupon = couponRepository.findById(couponId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 쿠폰입니다. id: %d".formatted(couponId)));
 
-        return couponRepository.findById(couponId)
-                .map(couponMapper::toResponse)
-                .orElseThrow(() -> new IllegalArgumentException(message));
+        return couponMapper.toResponse(coupon);
     }
 
     @Transactional(readOnly = true)
     public Map<Long, Coupon> getCouponMapEachById(List<Long> couponIds) {
+        CacheFindByIdsInResponse<Coupon> cacheFindByIdsInResponse = findByIdsIn("coupon", couponIds);
+        Map<Long, Coupon> couponMap = new HashMap<>(cacheFindByIdsInResponse.cachedData());
+        List<Long> notCachedIds = cacheFindByIdsInResponse.notCachedIds();
+        Map<Long, Coupon> couponsFromDB = getCouponsFromDB(notCachedIds);
+        couponMap.putAll(couponsFromDB);
+
+        return couponMap;
+    }
+
+    public <T> CacheFindByIdsInResponse<T> findByIdsIn(String value, List<Long> ids) {
+        Map<Long, T> cached = new HashMap<>();
+        List<Long> notCachedIds = new ArrayList<>();
+
+        List<String> keys = ids.stream()
+                .map(id -> "%s:%d".formatted(value, id))
+                .toList();
+        List<Object> fromCache = redisTemplate.opsForValue().multiGet(keys);
+        for (int i = 0; i < ids.size(); i++) {
+            Long id = ids.get(i);
+            Object data = fromCache.get(i);
+            if (data != null) {
+                cached.put(id, (T) data);
+                continue;
+            }
+            notCachedIds.add(id);
+        }
+
+        return new CacheFindByIdsInResponse<>(cached, notCachedIds);
+    }
+
+    private Map<Long, Coupon> getCouponsFromDB(List<Long> couponIds) {
+        Map<Long, Coupon> couponMap = new HashMap<>();
+
         List<Coupon> coupons = couponRepository.findAllByIdIn(couponIds);
 
-        return coupons.stream()
-                .collect(Collectors.toMap(Coupon::getId, Function.identity()));
+        return couponMap;
     }
 
     @Transactional
     public Long createCoupon(CreateCouponRequest request) {
         Coupon coupon = couponMapper.toCoupon(request);
+        Coupon savedCoupon = couponRepository.save(coupon);
 
-        couponRepository.save(coupon);
+        String key = "coupon:%d".formatted(savedCoupon.getId());
+        CouponResponse cachingData = couponMapper.toResponse(savedCoupon);
+        redisTemplate.opsForValue().set(key, cachingData);
 
         return coupon.getId();
     }
