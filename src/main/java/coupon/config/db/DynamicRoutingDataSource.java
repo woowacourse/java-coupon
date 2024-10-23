cb50112a74fd043aba1585b9cc760a34359fd623
yoonjuho <dbswn990@naver.com>
2024-10-23 12:45:56 +0900

    refactor: 개행 처리

--- src/main/java/coupon/config/db/DynamicRoutingDataSource.java
+++ src/main/java/coupon/config/db/DynamicRoutingDataSource.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import static org.springframework.transaction.support.TransactionSynchronizationManager.isCurrentTransactionReadOnly;
 
 public class DynamicRoutingDataSource extends AbstractRoutingDataSource {
+
     @Override
     protected Object determineCurrentLookupKey() {
         if (isCurrentTransactionReadOnly()) {
