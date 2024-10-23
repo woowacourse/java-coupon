00b80941ef6e6650a17de06dd9831ad886f5d9aa
Gyeongho Yang <chchch1213@naver.com>
2024-10-23 21:08:53 +0900

    feat: read cache when getting coupon

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -5,11 +5,14 @@ import coupon.exception.NotFoundException;
 import coupon.repository.CouponRepository;
 import coupon.util.TransactionExecutor;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CouponService {
+
+    private static final String CACHE_KEY_COUPON = "coupons";
 
     private final CouponRepository couponRepository;
     private final TransactionExecutor transactionExecutor;
@@ -19,12 +22,13 @@ public class CouponService {
         this.transactionExecutor = transactionExecutor;
     }
 
-    @CachePut(value = "coupons", key = "#coupon.id")
+    @CachePut(value = CACHE_KEY_COUPON, key = "#coupon.id")
     @Transactional
     public Coupon create(Coupon coupon) {
         return couponRepository.save(coupon);
     }
 
+    @Cacheable(value = CACHE_KEY_COUPON, key = "#id")
     @Transactional(readOnly = true)
     public Coupon getCoupon(long id) {
         return couponRepository.findById(id)
