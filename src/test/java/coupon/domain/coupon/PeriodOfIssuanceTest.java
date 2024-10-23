1676abd6e35ef72f3949e626ed7b45df82e77fdb
hyxrxn <gpfls981220@gmail.com>
2024-10-23 18:10:47 +0900

    refactor: SoftAssertions 적용

--- src/test/java/coupon/domain/coupon/PeriodOfIssuanceTest.java
+++ src/test/java/coupon/domain/coupon/PeriodOfIssuanceTest.java
@@ -1,10 +1,9 @@
 package coupon.domain.coupon;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.time.LocalDate;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,26 +12,34 @@ class PeriodOfIssuanceTest {
     @Test
     @DisplayName("발급 기간이 잘 생성된다.")
     void periodOfIssuance() {
-        assertAll(
-                () -> assertDoesNotThrow(() -> new PeriodOfIssuance(LocalDate.now(), LocalDate.now())),
-                () -> assertDoesNotThrow(() -> new PeriodOfIssuance(LocalDate.now(), LocalDate.now().plusDays(1)))
-        );
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions.assertThatCode(() -> new PeriodOfIssuance(LocalDate.now(), LocalDate.now()))
+                .doesNotThrowAnyException();
+        softAssertions.assertThatCode(() -> new PeriodOfIssuance(LocalDate.now(), LocalDate.now().plusDays(1)))
+                .doesNotThrowAnyException();
+
+        softAssertions.assertAll();
     }
 
     @Test
     @DisplayName("날짜 정보가 없을 경우 예외가 발생한다.")
     void validateNotNull() {
-        assertAll(
-                () -> assertThatThrownBy(() -> new PeriodOfIssuance(null, LocalDate.now()))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("날짜 정보가 존재하지 않습니다."),
-                () -> assertThatThrownBy(() -> new PeriodOfIssuance(LocalDate.now(), null))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("날짜 정보가 존재하지 않습니다."),
-                () -> assertThatThrownBy(() -> new PeriodOfIssuance(null, null))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("날짜 정보가 존재하지 않습니다.")
-        );
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions.assertThatThrownBy(() -> new PeriodOfIssuance(null, LocalDate.now()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("날짜 정보가 존재하지 않습니다.");
+
+        softAssertions.assertThatThrownBy(() -> new PeriodOfIssuance(LocalDate.now(), null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("날짜 정보가 존재하지 않습니다.");
+
+        softAssertions.assertThatThrownBy(() -> new PeriodOfIssuance(null, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("날짜 정보가 존재하지 않습니다.");
+
+        softAssertions.assertAll();
     }
 
     @Test
