01dd7c4ac7830a11687b0bb6d9d4f093c33b90c0
hyxrxn <gpfls981220@gmail.com>
2024-10-24 21:19:14 +0900

    feat: 트랜잭션 관리 역할 분리

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -3,20 +3,18 @@ package coupon.service;
 import coupon.domain.coupon.Coupon;
 import coupon.repository.CouponRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
 public class CouponService {
 
     private final CouponRepository couponRepository;
-    private final TransactionTemplate transactionTemplate;
+    private final TransactionHandler transactionHandler;
 
-    public CouponService(CouponRepository couponRepository, PlatformTransactionManager transactionManager) {
+    public CouponService(CouponRepository couponRepository, TransactionHandler transactionHandler) {
         this.couponRepository = couponRepository;
-        this.transactionTemplate = new TransactionTemplate(transactionManager);
+        this.transactionHandler = transactionHandler;
     }
 
     @Transactional
@@ -31,14 +29,10 @@ public class CouponService {
     }
 
     private Coupon getCouponOnWriter(Long id) {
-        int originalPropagationBehavior = transactionTemplate.getPropagationBehavior();
-
-        try {
-            transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
-            return transactionTemplate.execute(status -> couponRepository.findById(id)
-                    .orElseThrow(() -> new IllegalArgumentException("해당 아이디의 쿠폰이 존재하지 않습니다.")));
-        } finally {
-            transactionTemplate.setPropagationBehavior(originalPropagationBehavior);
-        }
+        return transactionHandler.runInNewTransaction(
+                TransactionDefinition.PROPAGATION_REQUIRES_NEW,
+                () -> couponRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("해당 아이디의 쿠폰이 존재하지 않습니다."))
+        );
     }
 }
