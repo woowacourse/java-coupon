dd4855103c708b160723cfbb894dbdc4f1387d01
3juhwan <13selfesteem91@naver.com>
2024-10-24 19:59:23 +0900

    test: 테스트 데이터베이스 클리너 추가

--- src/test/java/coupon/service/CouponServiceTest.java
+++ src/test/java/coupon/service/CouponServiceTest.java
@@ -4,12 +4,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import coupon.config.DataSourceHelper;
 import coupon.domain.Coupon;
+import coupon.support.CacheCleanerExtension;
+import coupon.support.DatabaseCleanerExtension;
 import coupon.support.Fixture;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
+@ExtendWith(value = {DatabaseCleanerExtension.class, CacheCleanerExtension.class})
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 public class CouponServiceTest {
 
--- src/test/java/coupon/support/CacheCleanerExtension.java
+++ src/test/java/coupon/support/CacheCleanerExtension.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-public class DatabaseCleanerExtension implements AfterEachCallback {
+public class CacheCleanerExtension implements AfterEachCallback {
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
-        DatabaseCleaner cleaner = SpringExtension.getApplicationContext(context).getBean(DatabaseCleaner.class);
+        CacheCleaner cleaner = SpringExtension.getApplicationContext(context).getBean(CacheCleaner.class);
         cleaner.execute();
     }
 }
