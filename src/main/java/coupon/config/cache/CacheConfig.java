46a8b8ebdf3ecd53d24013303613ab526f466c77
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-10-25 21:26:46 +0900

    feat: CacheConfig 추가

---
+++ src/main/java/coupon/config/cache/CacheConfig.java
@@ -0,0 +1,9 @@
+package coupon.config.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
