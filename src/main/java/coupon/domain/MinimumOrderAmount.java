a598d17443366b6157b5b7a851ba7e3b28871278
Seokmyung Ham <gkatjraud1@gmail.com>
2024-10-25 22:31:11 +0900

    refactor: 검증 메서드 응집화를 통한 가독성 개선

--- src/main/java/coupon/domain/MinimumOrderAmount.java
+++ src/main/java/coupon/domain/MinimumOrderAmount.java
@@ -23,17 +23,9 @@ public class MinimumOrderAmount {
     }
 
     private void validateAmount(int amount) {
-        validateMinimumAmount(amount);
-        validateMaximumAmount(amount);
-    }
-
-    private void validateMinimumAmount(int amount) {
         if (amount < MIN_ORDER_AMOUNT) {
             throw new IllegalArgumentException("최소 주문 금액은 " + MIN_ORDER_AMOUNT + "원 이상이어야 합니다.");
         }
-    }
-
-    private void validateMaximumAmount(int amount) {
         if (amount > MAX_ORDER_AMOUNT) {
             throw new IllegalArgumentException("최소 주문 금액은 " + MAX_ORDER_AMOUNT + "원 이하여야 합니다.");
         }
