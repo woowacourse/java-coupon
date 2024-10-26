fbd81a1ed6b10300937cd9828a01c0ce53c6c3b8
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-26 18:33:07 +0900

    feat: cache 설정

--- src/main/java/coupon/service/CouponDBService.java
+++ src/main/java/coupon/service/CouponDBService.java
@@ -3,10 +3,14 @@ package coupon.service;
 import coupon.db.TransactionRouter;
 import coupon.domain.coupon.Coupon;
 import java.util.NoSuchElementException;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CouponDBService {
+
+    private static final String CACHE_NAME = "coupon";
 
     private final CouponWriter couponWriter;
     private final CouponReader couponReader;
@@ -18,13 +22,15 @@ public class CouponDBService {
         this.transactionRouter = transactionRouter;
     }
 
+    @CachePut(value = CACHE_NAME, key = "#coupon.id")
     public Coupon create(Coupon coupon) {
         return couponWriter.save(coupon);
     }
 
+    @Cacheable(value = CACHE_NAME, key = "#id")
     public Coupon findById(long id) {
         return couponReader.findCoupon(id)
                 .or(() -> transactionRouter.route(() -> couponReader.findCoupon(id)))
-                .orElseThrow(() -> new NoSuchElementException("coupon with id " + id + " not found")); //reader에서 못찾으면 write db 조회
+                .orElseThrow(() -> new NoSuchElementException("coupon with id " + id + " not found")); //read db에서 못찾으면 write db 조회
     }
 }
