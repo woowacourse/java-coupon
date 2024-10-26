3b7fe38fac92206dbb7bbed441e7bf9997adbab3
hoyeonyy <highoption@naver.com>
2024-10-27 01:16:40 +0900

    refactor: 금액 검증 추가

--- src/main/java/coupon/domain/Discount.java
+++ src/main/java/coupon/domain/Discount.java
@@ -9,10 +9,17 @@ public class Discount {
 	private int percent;
 
 	public Discount(int price, int minimumOrderAmount) {
+		validatePrice(price);
 		this.price = price;
 		this.percent = price / minimumOrderAmount;
 	}
 
 	protected Discount() {
+	}
+
+	private void validatePrice(int price) {
+		if(price < 5000 || price > 100000) {
+			throw new IllegalArgumentException("금액은 5,000원 이상, 100,000원 이하 부터 가능합니다.");
+		}
 	}
 }
