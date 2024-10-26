9bd10163c1eb5911d236b38110d13c4315be3bb3
eun-byeol <joeunbyeol98@gmail.com>
2024-10-26 16:46:14 +0900

    chore: init.sql 추가 및 ddl auto validate 설정

---
+++ docker/init/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS coupon;
+
+USE coupon;
+
+CREATE TABLE IF NOT EXISTS coupon (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(30) NOT NULL,
+    discount_amount BIGINT NOT NULL,
+    min_order_amount BIGINT NOT NULL,
+    category ENUM('FASHION', 'ELECTRONICS', 'FURNITURE', 'FOOD') NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL
+);
