2c59424144ddf9dd5851a12522d799fe242d5773
yechop <channetattoo@gmail.com>
2024-10-26 19:48:03 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/coupon/domain/coupon/DiscountAmount.java
+++ src/main/java/coupon/domain/coupon/DiscountAmount.java
@@ -19,10 +19,6 @@ public class DiscountAmount {
     }
 
     private void validate(int value) {
-        if (value > Integer.MAX_VALUE) {
-            throw new IllegalArgumentException("최대 입력 가능한 금액을 초과했습니다.");
-        }
-
         if (value < 1000 || value > 10000 || value % 500 != 0) {
             throw new IllegalArgumentException("할인 금액은 1000원 이상 10000원 이하의 500으로 나눠지는 금액만 가능합니다.");
         }
