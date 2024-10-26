d0ff7b8b9432ba40e48374e303ad3167084437b1
Nam Gi Beom <rlqja0523@naver.com>
2024-10-26 19:14:30 +0900

    feat: getter 추가

--- src/main/java/coupon/domain/member/Member.java
+++ src/main/java/coupon/domain/member/Member.java
@@ -5,9 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
