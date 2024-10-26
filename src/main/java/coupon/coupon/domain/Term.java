67470c0a2bcce707f68c14ed76f88613131a99c1
linirini <2001yerin@naver.com>
2024-10-27 08:26:42 +0900

    feat: 할인 기간의 시간 설정 구현

--- src/main/java/coupon/coupon/domain/Term.java
+++ src/main/java/coupon/coupon/domain/Term.java
@@ -1,6 +1,7 @@
 package coupon.coupon.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import coupon.coupon.CouponException;
@@ -11,21 +12,25 @@ public class Term {
     private static final String TERM_MESSAGE = "종료일이 시작일보다 앞설 수 없습니다.";
 
     @Column(nullable = false)
-    private LocalDate startAt;
+    private LocalDateTime startAt;
 
     @Column(nullable = false)
-    private LocalDate endAt;
+    private LocalDateTime endAt;
 
     protected Term() {
     }
 
     public Term(LocalDate startAt, LocalDate endAt) {
+        this(startAt.atTime(0, 0, 0, 0), endAt.atTime(23, 59, 59, 999_999_000));
+    }
+
+    private Term(LocalDateTime startAt, LocalDateTime endAt) {
         validateTerm(startAt, endAt);
         this.startAt = startAt;
         this.endAt = endAt;
     }
 
-    private void validateTerm(LocalDate startAt, LocalDate endAt) {
+    private void validateTerm(LocalDateTime startAt, LocalDateTime endAt) {
         if (endAt.isBefore(startAt)) {
             throw new CouponException(TERM_MESSAGE);
         }
--- src/test/java/coupon/coupon/domain/TermTest.java
+++ src/test/java/coupon/coupon/domain/TermTest.java
@@ -1,11 +1,13 @@
 package coupon.coupon.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import coupon.coupon.CouponException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class TermTest {
 
@@ -20,5 +22,20 @@ class TermTest {
         assertThatThrownBy(() -> new Term(startAt, endAt))
                 .isInstanceOf(CouponException.class)
                 .hasMessage("종료일이 시작일보다 앞설 수 없습니다.");
+    }
+
+    @DisplayName("시작일과 종료일의 시간은 각각 00:00:00.000 및 23:59:59.999로 설정된다.")
+    @Test
+    void termStartAndEndTimeAreSetProperly() {
+        // given
+        LocalDate startAt = LocalDate.of(2024, 10, 1);
+        LocalDate endAt = LocalDate.of(2024, 10, 2);
+
+        // when
+        Term term = new Term(startAt, endAt);
+
+        // then
+        assertThat(term).extracting("startAt").isEqualTo(LocalDateTime.of(2024, 10, 1, 0, 0, 0, 0));
+        assertThat(term).extracting("endAt").isEqualTo(LocalDateTime.of(2024, 10, 2, 23, 59, 59, 999_999_000));
     }
 }
