40fd4d3bf21a546d40207b653f1a0ee542805f4c
hoyeonyy <highoption@naver.com>
2024-10-27 01:20:34 +0900

    refactor: 할인률 검증 추가

--- src/main/java/coupon/domain/Discount.java
+++ src/main/java/coupon/domain/Discount.java
@@ -12,6 +12,7 @@ public class Discount {
 		validatePrice(price);
 		this.price = price;
 		this.percent = price / minimumOrderAmount;
+		validatePercent(percent);
 	}
 
 	protected Discount() {
@@ -20,6 +21,12 @@ public class Discount {
 	private void validatePrice(int price) {
 		if(price < 5000 || price > 100000) {
 			throw new IllegalArgumentException("금액은 5,000원 이상, 100,000원 이하 부터 가능합니다.");
+		}
+	}
+
+	private void validatePercent(int price) {
+		if(percent < 3 || percent > 20) {
+			throw new IllegalArgumentException("할인율은 3% 이상, 20% 이하여야 합니다.");
 		}
 	}
 }
