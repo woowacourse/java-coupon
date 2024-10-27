eb14ba471dbbf4392254c6177cf7d2b349d98e74
재우 <Akaps6768@gmail.com>
2024-10-27 12:37:25 +0900

    remove unneeded fetch type attributes

--- src/main/java/coupon/domain/MemberCoupon.java
+++ src/main/java/coupon/domain/MemberCoupon.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,11 +29,11 @@ public class MemberCoupon {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne
     @JoinColumn(name = "coupon_id", nullable = false)
     private Coupon coupon;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
