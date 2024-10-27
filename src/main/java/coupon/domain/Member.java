129405ae8d75fc25938c8c4063257b3cb42fd690
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-10-27 22:57:49 +0900

    refactor(Member): 회원당 쿠폰 발급 개수 검증 추가

--- src/main/java/coupon/domain/Member.java
+++ src/main/java/coupon/domain/Member.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Entity
 @Getter
 @Setter
@@ -27,4 +28,17 @@ public class Member {
 
     @OneToMany(mappedBy = "member")
     private List<MemberCoupon> memberCoupons;
+
+    public void incrementCouponCount() {
+        if (couponCount >= 5) {
+            throw new IllegalArgumentException("한 회원은 최대 5장의 쿠폰만 발급받을 수 있습니다.");
+        }
+        couponCount++;
+    }
+
+    public void decrementCouponCount() {
+        if (couponCount > 0) {
+            couponCount--;
+        }
+    }
 }
