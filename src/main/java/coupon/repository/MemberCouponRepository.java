32751be39b615f36efaf26ec6ac09a9ebbda44a6
재우 <Akaps6768@gmail.com>
2024-10-27 14:45:53 +0900

    회원 쿠폰 발급 기능 구현

--- src/main/java/coupon/repository/MemberCouponRepository.java
+++ src/main/java/coupon/repository/MemberCouponRepository.java
@@ -2,7 +2,9 @@ package coupon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import coupon.domain.Member;
 import coupon.domain.MemberCoupon;
 
 public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long> {
+    int countByMember(Member member);
 }
--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -1,10 +1,16 @@
 package coupon.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import coupon.domain.Coupon;
+import coupon.domain.DefaultMemberCouponPolicy;
+import coupon.domain.Member;
+import coupon.domain.MemberCoupon;
 import coupon.repository.CouponRepository;
+import coupon.repository.MemberCouponRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -13,6 +19,7 @@ public class CouponService {
 
     private final CouponRepository couponRepository;
     private final TransactionService transactionService;
+    private final MemberCouponRepository memberCouponRepository;
 
     @Transactional
     public Coupon create(Coupon coupon) {
@@ -27,5 +34,14 @@ public class CouponService {
 
     public Coupon getCouponFromWriter(long id) {
         return transactionService.run(() -> couponRepository.fetchCouponById(id));
+    }
+
+    @Transactional
+    public MemberCoupon issueMemberCoupon(Coupon coupon, Member member) {
+        var memberCouponCount = memberCouponRepository.countByMember(member);
+        var memberCouponIssuePolicy = new DefaultMemberCouponPolicy(memberCouponCount);
+        memberCouponIssuePolicy.validate();
+        var memberCoupon = new MemberCoupon(coupon, member, LocalDateTime.now());
+        return memberCouponRepository.save(memberCoupon);
     }
 }
