c8f4b41eccb53ba1a477e965933204b94590b5a4
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-10-27 23:02:36 +0900

    feat(CouponService): 회원에게 쿠폰 발급하는 기능 추가

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -1,7 +1,12 @@
 package coupon.service;
 
 import coupon.domain.Coupon;
+import coupon.domain.Member;
+import coupon.domain.MemberCoupon;
 import coupon.domain.repository.CouponRepository;
+import coupon.domain.repository.MemberCouponRepository;
+import coupon.domain.repository.MemberRepository;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class CouponService {
 
     private final CouponRepository couponRepository;
+    private final MemberCouponRepository memberCouponRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
     public void createCoupon(Coupon coupon) {
@@ -27,6 +34,22 @@ public class CouponService {
     public Coupon getCouponFromWriter(Long couponId) {
         return couponRepository.findById(couponId)
                 .orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Transactional
+    public void issueCouponToMember(Long memberId, Long couponId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("회원이 존재하지 않습니다."));
+        Coupon coupon = couponRepository.findById(couponId)
+                .orElseThrow(() -> new IllegalArgumentException("쿠폰이 존재하지 않습니다."));
+
+        if (member.getCouponCount() >= 5) {
+            throw new IllegalArgumentException("한 회원은 최대 5장의 쿠폰만 발급받을 수 있습니다.");
+        }
+
+        coupon.decrementAvailableCount();
+        MemberCoupon memberCoupon = new MemberCoupon(coupon, member, LocalDateTime.now(), LocalDateTime.now().plusDays(7));
+        memberCouponRepository.save(memberCoupon);
     }
 }
 
