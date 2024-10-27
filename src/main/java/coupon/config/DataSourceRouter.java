0ca4910623339aa6ba02c3ff5deb91acb2ea4bf9
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-10-27 15:24:51 +0900

    refactor(DataSourceRouter): 로그 추가

--- src/main/java/coupon/config/DataSourceRouter.java
+++ src/main/java/coupon/config/DataSourceRouter.java
@@ -3,16 +3,20 @@ package coupon.config;
 import static coupon.config.DataSourceConfig.READ_DATASOURCE;
 import static coupon.config.DataSourceConfig.WRITE_DATASOURCE;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 public class DataSourceRouter extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            logger.debug("Current DataSource key: readDataSource");
             return READ_DATASOURCE;
         }
+        logger.debug("Current DataSource key: writeDataSource");
         return WRITE_DATASOURCE;
     }
 }
