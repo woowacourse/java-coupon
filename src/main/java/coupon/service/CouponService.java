67abe9c8b5fb984a9a01752711a3db4eeaf7900e
lemone <lejinhy@gmail.com>
2024-10-27 16:31:31 +0900

    feat: 회원 쿠폰 조회 / 관리자 쿠폰 조회 메서드 분리

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -1,5 +1,7 @@
 package coupon.service;
 
+import static java.lang.Thread.sleep;
+
 import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
 
 import org.springframework.stereotype.Service;
@@ -18,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class CouponService {
 
+    public static final int MAX_READ_ATTEMPT = 2;
     private final CouponRepository couponRepository;
 
     @Transactional
@@ -37,8 +40,36 @@ public class CouponService {
         );
     }
 
+    // 회원을 위한 쿠폰 조회
     @Transactional(readOnly = true)
-    public GetCouponResponse getCoupon(final long id) {
+    public GetCouponResponse getCouponForMember(final long id) {
+        int attempts = 0;
+        while (attempts < MAX_READ_ATTEMPT) {
+            try {
+                final CouponEntity couponEntity = couponRepository.findById(id)
+                        .orElseThrow(() -> new NotFoundCouponException("존재하지 않는 쿠폰입니다."));
+                return GetCouponResponse.from(couponEntity.toDomain());
+            } catch (NotFoundCouponException e) {
+                attempts++;
+                sleep();
+            }
+        }
+        throw new NotFoundCouponException("쿠폰을 찾을 수 없습니다.");
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("쿠폰 조회 대기 중 예외가 발생했습니다.", ie);
+        }
+    }
+
+
+    // 관리자를 위한 쿠폰 조회
+    @Transactional(readOnly = true)
+    public GetCouponResponse getCouponForAdmin(final long id) {
         final CouponEntity couponEntity = couponRepository.findById(id)
                 .orElseGet(() -> findCouponEntity(id));
         return GetCouponResponse.from(couponEntity.toDomain());
