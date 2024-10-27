e14d9222c3b0cd9a96ccc240aff80bb648b36acd
eun-byeol <joeunbyeol98@gmail.com>
2024-10-27 13:57:47 +0900

    test: 관련 테스트 Nested 처리

--- src/test/java/coupon/service/MemberCouponServiceTest.java
+++ src/test/java/coupon/service/MemberCouponServiceTest.java
@@ -10,6 +10,7 @@ import coupon.domain.MemberCoupon;
 import coupon.repository.CouponRepository;
 import coupon.repository.MemberRepository;
 import coupon.support.Fixture;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,49 +27,53 @@ class MemberCouponServiceTest {
     @Autowired
     private CouponRepository couponRepository;
 
-    @Test
-    void 발급시_존재하지_않는_coupon_이면_예외가_발생한다() {
-        long invalidCouponId = 0;
-        long validMemberId = memberRepository.save(Fixture.createMember()).getId();
+    @Nested
+    class 회원_쿠폰_발급 {
 
-        assertThatThrownBy(() -> memberCouponService.issue(invalidCouponId, validMemberId))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("존재하지 않는 couponId 입니다.");
-    }
+        @Test
+        void 존재하지_않는_coupon_이면_예외가_발생한다() {
+            long invalidCouponId = 0;
+            long validMemberId = memberRepository.save(Fixture.createMember()).getId();
 
-    @Test
-    void 발급시_존재하지_않는_member_이면_예외가_발생한다() {
-        long validCouponId = couponRepository.save(Fixture.createCoupon()).getId();
-        long invalidMemberId = 0;
-
-        assertThatThrownBy(() -> memberCouponService.issue(validCouponId, invalidMemberId))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("존재하지 않는 memberId 입니다.");
-    }
-
-    @Test
-    void 발급시_회원의_쿠폰이_5개_미만이면_성공한다() {
-        Coupon coupon = couponRepository.save(Fixture.createCoupon());
-        Member member = memberRepository.save(Fixture.createMember());
-
-        MemberCoupon memberCoupon = memberCouponService.issue(coupon.getId(), member.getId());
-
-        assertAll(
-                () -> assertThat(memberCoupon.getCouponId()).isEqualTo(coupon.getId()),
-                () -> assertThat(memberCoupon.getMemberId()).isEqualTo(member.getId())
-        );
-    }
-
-    @Test
-    void 발급시_회원의_쿠폰이_5개_이상이면_예외가_발생한다() {
-        Coupon coupon = couponRepository.save(Fixture.createCoupon());
-        Member member = memberRepository.save(Fixture.createMember());
-
-        for (int i = 0; i < 5; i++) {
-            memberCouponService.issue(coupon.getId(), member.getId());
+            assertThatThrownBy(() -> memberCouponService.issue(invalidCouponId, validMemberId))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("존재하지 않는 couponId 입니다.");
         }
 
-        assertThatThrownBy(() -> memberCouponService.issue(coupon.getId(), member.getId()))
-                .isInstanceOf(IllegalArgumentException.class);
+        @Test
+        void 존재하지_않는_member_이면_예외가_발생한다() {
+            long validCouponId = couponRepository.save(Fixture.createCoupon()).getId();
+            long invalidMemberId = 0;
+
+            assertThatThrownBy(() -> memberCouponService.issue(validCouponId, invalidMemberId))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("존재하지 않는 memberId 입니다.");
+        }
+
+        @Test
+        void 회원의_쿠폰이_5개_미만이면_성공한다() {
+            Coupon coupon = couponRepository.save(Fixture.createCoupon());
+            Member member = memberRepository.save(Fixture.createMember());
+
+            MemberCoupon memberCoupon = memberCouponService.issue(coupon.getId(), member.getId());
+
+            assertAll(
+                    () -> assertThat(memberCoupon.getCouponId()).isEqualTo(coupon.getId()),
+                    () -> assertThat(memberCoupon.getMemberId()).isEqualTo(member.getId())
+            );
+        }
+
+        @Test
+        void 회원의_쿠폰이_5개_이상이면_예외가_발생한다() {
+            Coupon coupon = couponRepository.save(Fixture.createCoupon());
+            Member member = memberRepository.save(Fixture.createMember());
+
+            for (int i = 0; i < 5; i++) {
+                memberCouponService.issue(coupon.getId(), member.getId());
+            }
+
+            assertThatThrownBy(() -> memberCouponService.issue(coupon.getId(), member.getId()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
