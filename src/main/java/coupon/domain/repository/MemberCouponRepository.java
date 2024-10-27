d9637f7f95d1cd1a44f373d57144efda67c21474
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-10-27 23:02:08 +0900

    feat: Member, MemberCoupon Repository 생성

---
+++ src/main/java/coupon/domain/repository/MemberCouponRepository.java
@@ -0,0 +1,8 @@
+package coupon.domain.repository;
+
+import coupon.domain.MemberCoupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long> {
+
+}
