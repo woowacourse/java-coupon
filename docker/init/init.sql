7708e54c930c497dd2034218ae565620837020b5
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-28 07:25:24 +0900

    docs: init.sql 수정

--- docker/init/init.sql
+++ docker/init/init.sql
@@ -9,6 +9,16 @@ CREATE TABLE IF NOT EXISTS coupon
     discount_price   INT          NOT NULL,
     category         varchar(256) NOT NULL,
     sale_order_price INT          NOT NULL,
-    start_at         datetime     NOT NULL,
-    end_at           datetime     NOT NULL
+    start_at         DATETIME     NOT NULL,
+    end_at           DATETIME     NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS member_coupon
+(
+    id               BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id        BIGINT,
+    coupon_id        BIGINT,
+    used             TINYINT(1)    NOT NULL,
+    issued_at        DATETIME     NOT NULL,
+    expired_at       DATEITME     NOT NULL
+)
