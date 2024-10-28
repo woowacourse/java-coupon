8279d6bf03be53a6d4f229346121b23b473bc655
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-28 14:02:47 +0900

    feat(RedisCacheConfig): 레디스 CacheManager 설정

---
+++ src/main/java/coupon/infrastructure/cache/redis/RedisCacheConfig.java
@@ -0,0 +1,42 @@
+package coupon.infrastructure.cache.redis;
+
+import java.time.Duration;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableCaching
+@EnableConfigurationProperties(RedisProperties.class)
+public class RedisCacheConfig {
+
+    private static final Duration DEFAULT_TTL = Duration.ofDays(7L);
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory cf) {
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(cf)
+                .cacheDefaults(redisCacheConfiguration())
+                .build();
+    }
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(DEFAULT_TTL);
+    }
+}
