e24d9aa8f1845225bc1110d9db1347d1494b9eb0
reddevilmidzy <midzy0228@gmail.com>
2024-10-28 18:03:41 +0900

    chore: init.sql 수정

--- docker/init/init.sql
+++ docker/init/init.sql
@@ -5,11 +5,12 @@ USE coupon;
 CREATE TABLE IF NOT EXISTS coupon
 (
     id                     BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name                   VARCHAR(255) NOT NULL,
-    discount_amount        INT          NOT NULL,
-    min_order_amount       INT          NOT NULL,
-    issued_start_date_time DATETIME     NOT NULL,
-    issued_end_date_time   DATETIME     NOT NULL,
+    name                   VARCHAR(255)                                             NOT NULL,
+    discount_amount        INT                                                      NOT NULL,
+    min_order_amount       INT                                                      NOT NULL,
+    issued_start_date_time DATETIME                                                 NOT NULL,
+    issued_end_date_time   DATETIME                                                 NOT NULL,
+    category               ENUM ('FOOD', 'FURNITURE', 'HOME_APPLIANCES', 'FASHION') NOT NULL,
     created_at             DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at             DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
