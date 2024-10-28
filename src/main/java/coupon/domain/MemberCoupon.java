aee27ff57bdc9ddcbd3fb9a005b06a3b49856629
SungKyum Kim <sungkyum1@naver.com>
2024-10-28 17:04:56 +0900

    feat: MemberCoupon 을 Jpa Entity 로 변경, Member Entity 구현, MemberCouponRepository, MemberRepository 구현

--- src/main/java/coupon/domain/MemberCoupon.java
+++ src/main/java/coupon/domain/MemberCoupon.java
@@ -3,18 +3,42 @@ package coupon.domain;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Entity
+@Table(name = "member_coupon")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberCoupon {
 
     private static final int EXPIRATION_DAYS = 6;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "coupon_id")
     private long couponId;
+
+    @Column(name = "member_id")
     private long memberId;
+
+    @Column(name = "is_used")
     private boolean isUsed;
+
+    @Column(name = "issued_at")
     private LocalDateTime issuedAt;
+
+    @Column(name = "expires_at")
     private LocalDateTime expiresAt;
 
     public MemberCoupon(Long id, long couponId, long memberId, boolean isUsed, LocalDateTime issuedAt) {
@@ -26,10 +50,10 @@ public class MemberCoupon {
         this.expiresAt = initExpiresAt(issuedAt);
     }
 
-    public MemberCoupon(long couponId, long memberId, boolean isUsed, LocalDateTime issuedAt) {
+    public MemberCoupon(long couponId, long memberId, LocalDateTime issuedAt) {
         this.couponId = couponId;
         this.memberId = memberId;
-        this.isUsed = isUsed;
+        this.isUsed = false;
         this.issuedAt = issuedAt;
         this.expiresAt = initExpiresAt(issuedAt);
     }
