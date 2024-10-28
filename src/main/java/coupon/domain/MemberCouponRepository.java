7d01e6785cf835a7e711c141d4baab304ce8eeb9
le2sky <leehaneul990623@gmail.com>
2024-10-28 15:00:14 +0900

    feat: memberId 기반 멤버쿠폰 조회 쿼리 구횬

--- src/main/java/coupon/domain/MemberCouponRepository.java
+++ src/main/java/coupon/domain/MemberCouponRepository.java
@@ -1,8 +1,11 @@
 package coupon.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long> {
 
     long countMemberCouponByMemberIdAndCouponId(Long memberId, Long couponId);
+
+    List<MemberCoupon> findAllByMemberId(Long memberId);
 }
