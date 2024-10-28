b9df73e4e3f4baca7929e20f62c424e9075fef3a
Choo <choo000407@naver.com>
2024-10-28 16:44:20 +0900

    refactor: 고려치 않아도 될 회원 엔티티 제거

--- src/main/java/coupon/repository/member/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package coupon.repository.member;
-
-import coupon.entity.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
