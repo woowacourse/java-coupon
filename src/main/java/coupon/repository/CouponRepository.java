16e56ba573da5e31e9a631ee230044301b441660
재우 <Akaps6768@gmail.com>
2024-10-28 14:35:34 +0900

    메서드명 변경

--- src/main/java/coupon/repository/CouponRepository.java
+++ src/main/java/coupon/repository/CouponRepository.java
@@ -6,7 +6,7 @@ import coupon.domain.Coupon;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
-    default Coupon fetchCouponById(Long id) {
+    default Coupon fetchById(Long id) {
         return findById(id).orElseThrow(() -> new IllegalArgumentException(id + "에 해당하는 쿠폰을 찾을 수 없습니다."));
     }
 }
