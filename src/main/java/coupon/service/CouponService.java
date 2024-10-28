1db128a044eaa98ea64a8eacfa8ade29b19d6ab1
nayonsoso <panda0329@naver.com>
2024-10-28 16:33:45 +0900

    style: 사용하지 않는 코드 삭제

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -7,11 +7,9 @@ import coupon.mapper.EntityDomainMapper;
 import coupon.repository.CouponReaderRepository;
 import coupon.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CouponService {
