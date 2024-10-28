9ed9593b2c44c8e03d3c9d9a794c96e415d71d95
leegwichan <cnddkscndgus@naver.com>
2024-10-28 10:55:45 +0900

    feat: 회원에게 쿠폰을 발급하는 기능 구현
    - 한 명의 회원은 동일한 쿠폰을 최대 5장까지 발급할 수 있도록 설정

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -2,8 +2,11 @@ package coupon.service;
 
 import coupon.config.datasource.UsingWriterSource;
 import coupon.domain.Coupon;
+import coupon.domain.MemberCoupon;
 import coupon.repository.CouponRepository;
+import coupon.repository.MemberCouponRepository;
 import coupon.service.exception.CouponBusinessLogicException;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,7 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class CouponService {
 
+    private static final long MAX_SAME_COUPON = 5;
+
     private final CouponRepository couponRepository;
+    private final MemberCouponRepository memberCouponRepository;
 
     @Transactional(readOnly = true)
     @UsingWriterSource
@@ -24,5 +30,16 @@ public class CouponService {
     @Transactional
     public void create(Coupon coupon) {
         couponRepository.save(coupon);
+    }
+
+    @Transactional
+    @UsingWriterSource
+    public void issueCoupon(Long memberId, Long couponId) {
+        if (memberCouponRepository.countByMemberIdAndCouponId(memberId, couponId) >= MAX_SAME_COUPON) {
+            throw new CouponBusinessLogicException("The coupon can no longer be issued to that user");
+        }
+
+        MemberCoupon issuedCoupon = new MemberCoupon(memberId, getCoupon(couponId), LocalDateTime.now());
+        memberCouponRepository.save(issuedCoupon);
     }
 }
