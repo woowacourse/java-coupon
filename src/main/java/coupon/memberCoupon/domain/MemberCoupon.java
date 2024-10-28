caa8ecbdeaa7dbfdbf6a630c894eb1286d2cff4b
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-28 14:10:26 +0900

    refactor: 정적 팩토리 메서드 추가

--- src/main/java/coupon/memberCoupon/domain/MemberCoupon.java
+++ src/main/java/coupon/memberCoupon/domain/MemberCoupon.java
@@ -3,14 +3,21 @@ package coupon.memberCoupon.domain;
 import coupon.coupon.domain.Coupon;
 import coupon.member.domain.Member;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class MemberCoupon {
 
     @Id
@@ -30,4 +37,19 @@ public class MemberCoupon {
     private LocalDateTime createdAt;
 
     private LocalDateTime expiredAt;
+
+    public static MemberCoupon create(Coupon coupon, Member member) {
+        LocalDate createdDate = LocalDate.now();
+        LocalDateTime createdAt = LocalDateTime.of(createdDate, LocalTime.MIDNIGHT);
+
+        return new MemberCoupon(coupon, member, false, createdAt, createdAt.plusDays(7L));
+    }
+
+    public MemberCoupon(Coupon coupon, Member member, boolean isUsed, LocalDateTime createdAt, LocalDateTime expiredAt) {
+        this.coupon = coupon;
+        this.member = member;
+        this.isUsed = isUsed;
+        this.createdAt = createdAt;
+        this.expiredAt = expiredAt;
+    }
 }
