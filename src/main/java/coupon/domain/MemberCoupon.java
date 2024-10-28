3a4ea8f98ab0f47129b3854a148b1c5e1ce90fc0
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-10-28 12:40:01 +0900

    feat: memberCoupon 구현

--- src/main/java/coupon/domain/MemberCoupon.java
+++ src/main/java/coupon/domain/MemberCoupon.java
@@ -1,24 +1,29 @@
 package coupon.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
 @Entity
-public class Member {
+public class MemberCoupon {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @ManyToOne
+    private Coupon coupon;
 
-    private String email;
+    @ManyToOne
+    private Member member;
 
-    private String password;
+    private boolean used;
 }
