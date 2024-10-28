b53eed4a6323f977969d073461ca2116f0eff02f
fromitive <fromitive@gmail.com>
2024-10-28 15:03:42 +0900

    refactor: 쿠폰 사용 가능일 검증 기능 제거

--- src/test/java/coupon/membercoupon/domain/AvailablePeriodTest.java
+++ src/test/java/coupon/membercoupon/domain/AvailablePeriodTest.java
@@ -1,7 +1,6 @@
 package coupon.membercoupon.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import coupon.coupon.domain.IssuedPeriod;
 import java.time.LocalDate;
@@ -10,34 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AvailablePeriodTest {
-
-    @DisplayName("시작일이 발급일 보다 늦으면 쿠폰을 발급할 수 없다")
-    @Test
-    void validateStartDate() {
-        // given
-        LocalDateTime start = LocalDateTime.now().plusSeconds(1);
-        LocalDateTime end = LocalDateTime.now().plusSeconds(8);
-
-        IssuedPeriod issuedPeriod = new IssuedPeriod(start, end);
-
-        // when
-        assertThatThrownBy(() -> new AvailablePeriod(issuedPeriod))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("만료일이 발급일 보다 늦으면 쿠폰을 발급할 수 없다")
-    @Test
-    void validateEndDate() {
-        // given
-        LocalDateTime start = LocalDateTime.now().minusSeconds(2);
-        LocalDateTime end = LocalDateTime.now().minusSeconds(1);
-
-        IssuedPeriod issuedPeriod = new IssuedPeriod(start, end);
-
-        // when
-        assertThatThrownBy(() -> new AvailablePeriod(issuedPeriod))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
     @DisplayName("쿠폰 발급 시 사용 가능일이 종료일 보다 작을 때 발급일로부터 7일간 사용 가능하다")
     @Test
