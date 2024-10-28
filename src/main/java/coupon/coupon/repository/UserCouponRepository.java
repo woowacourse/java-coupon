b9343a476b91ef268b4ff1d2ae9f531a779dceca
tackyu <tack970913@gmail.com>
2024-10-28 15:13:49 +0900

    feat: 유저 당 중복 쿠폰 갯수 조회

---
+++ src/main/java/coupon/coupon/repository/UserCouponRepository.java
@@ -0,0 +1,13 @@
+package coupon.coupon.repository;
+
+import coupon.coupon.domain.Coupon;
+import coupon.coupon.domain.UserCoupon;
+import coupon.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
+
+    int countByUserAndCoupon(User user, Coupon coupon);
+}
