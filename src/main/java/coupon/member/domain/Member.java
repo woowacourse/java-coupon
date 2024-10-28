489120abc7c58d22b846e3d16d96ae8fe6d8e673
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-28 14:10:50 +0900

    refactor: lombok 사용

--- src/main/java/coupon/member/domain/Member.java
+++ src/main/java/coupon/member/domain/Member.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class Member {
 
     @Id
