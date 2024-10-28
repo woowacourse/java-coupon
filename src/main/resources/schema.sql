bcc1b6bc046f444a1e2487760b6fd1d79a0d5b25
fram1998 <koust6u@pusan.ac.kr>
2024-10-28 14:24:49 +0900

    chore: 스키마 정리

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,18 +7,28 @@ create table COUPON (
     ID bigint not null auto_increment,
     PAYMENT_ID bigint,
     START_AT datetime(6) not null,
-    NAME varchar(255) not null,
+    COUPON_NAME varchar(255) not null,
     primary key (ID)
-) engine=InnoDB;
+) engine=InnoDB
+
+create table MEMBER (
+    ID bigint not null auto_increment,
+    USERNAME varchar(255) not null,
+    primary key (ID)
+) engine=InnoDB
 
 create table PAYMENT (
     ID bigint not null auto_increment,
     PRICE bigint not null,
     category enum ('FASHION','APPLIANCE','FURNITURE','FOOD'),
     primary key (ID)
-) engine=InnoDB;
+) engine=InnoDB
+
 
 alter table COUPON
+add constraint UK_ixi9q4v2im2tm0rutrmwb61rt unique (COUPON_NAME)
+
+ alter table COUPON
 add constraint FK4qhoccm36wusi0qgg5ow85l79
 foreign key (PAYMENT_ID)
-references PAYMENT (ID);
+references PAYMENT (ID)
