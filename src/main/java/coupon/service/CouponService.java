538166aa093656a72c79af76f2002b2902b9ee49
재우 <Akaps6768@gmail.com>
2024-10-28 14:38:16 +0900

    캐시로 복제지연 해결

--- src/main/java/coupon/service/CouponService.java
+++ src/main/java/coupon/service/CouponService.java
@@ -1,5 +1,7 @@
 package coupon.service;
 
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,20 +14,16 @@ import lombok.RequiredArgsConstructor;
 public class CouponService {
 
     private final CouponRepository couponRepository;
-    private final TransactionService transactionService;
 
     @Transactional
+    @CachePut(key = "#result.id", value = "coupon")
     public Coupon create(Coupon coupon) {
         return couponRepository.save(coupon);
     }
 
     @Transactional(readOnly = true)
+    @Cacheable(value = "coupon", key = "#id")
     public Coupon getCoupon(long id) {
-        return couponRepository.findById(id)
-                .orElseGet(() -> getCouponFromWriter(id));
-    }
-
-    private Coupon getCouponFromWriter(long id) {
-        return transactionService.run(() -> couponRepository.fetchCouponById(id));
+        return couponRepository.fetchById(id);
     }
 }
