c5b30b5aa97b6373c14937a105504d6893b76fbc
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-10-28 17:36:32 +0900

    chore: member_coupon,member 스키마 변경

--- docker/init.sql
+++ docker/init.sql
@@ -1,20 +1,25 @@
 -- 쿠폰 테이블 생성
-create table coupon IF NOT EXISTS(
+create table IF NOT EXISTS coupon(
                         discount_price_amount decimal(38,2),
                         minimum_order_price_amount decimal(38,2),
                         end_at datetime(6),
                         started_at datetime(6),
-                        id binary(16) not null,
+                        id VARCHAR(36),
                         name varchar(255),
                         category enum ('FASHION','HOME_APPLIANCE','FURNITURE','FOOD'),
                         primary key (id)
 ) engine=InnoDB;
-create table member_coupon IF NOT EXISTS(
+create table IF NOT EXISTS member_coupon(
                                isUsed bit not null,
                                expiredAt datetime(6),
-                               id bigint not null,
+                               id VARCHAR(36),
                                issuedAt datetime(6),
-                               couponId binary(16),
-                               memberId binary(16),
+                               couponId VARCHAR(36),
+                               memberId bigint(16),
+                               primary key (id)
+) engine=InnoDB;
+
+create table IF NOT EXISTS member(
+                               id bigint not null auto_increment,
                                primary key (id)
 ) engine=InnoDB;
