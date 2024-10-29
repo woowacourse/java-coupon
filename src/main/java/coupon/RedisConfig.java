d91dc95d36478822b7ff0df5b13d41f7e668aa2f
le2sky <leehaneul990623@gmail.com>
2024-10-29 17:52:24 +0900

    fix: redis cache manager 설정 변경

--- src/main/java/coupon/RedisConfig.java
+++ src/main/java/coupon/RedisConfig.java
@@ -4,8 +4,11 @@ import static org.springframework.data.redis.cache.RedisCacheConfiguration.defau
 import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
 
 import java.time.Duration;
-import java.util.Collections;
 import java.util.Map;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,19 +24,27 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager couponCacheManager(RedisConnectionFactory connectionFactory) {
-        Map<String, RedisCacheConfiguration> initialCacheConfig = Collections.singletonMap(
+        RedisCacheConfiguration defaultCacheConfiguration = getDefaultCacheConfiguration();
+        Map<String, RedisCacheConfiguration> initialCacheConfig = Map.of(
                 "coupon",
-                defaultCacheConfig().entryTtl(Duration.ofSeconds(30L))
+                defaultCacheConfiguration.entryTtl(Duration.ofSeconds(30L))
         );
-
-        RedisCacheConfiguration defaultCacheConfiguration = defaultCacheConfig()
-                .serializeKeysWith(fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
         return RedisCacheManager.builder(connectionFactory)
                 .transactionAware()
                 .cacheDefaults(defaultCacheConfiguration)
                 .withInitialCacheConfigurations(initialCacheConfig)
                 .build();
+    }
+
+    private RedisCacheConfiguration getDefaultCacheConfiguration() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+
+        return defaultCacheConfig()
+                .serializeKeysWith(fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)));
     }
 }
