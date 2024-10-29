294894fb0ff09ee74053387fc48da04bc06033c3
le2sky <leehaneul990623@gmail.com>
2024-10-29 20:16:59 +0900

    feat: 멤버 쿠폰 사용 기능 구현

--- src/main/java/coupon/domain/MemberCoupon.java
+++ src/main/java/coupon/domain/MemberCoupon.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberCoupon {
 
-    private static final int USABLE_DAY = 7;
+    private static final int USABLE_DAY = 6;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,6 +41,28 @@ public class MemberCoupon {
     public static MemberCoupon issue(Long couponId, Long memberId) {
         LocalDateTime today = LocalDateTime.now();
         LocalDateTime expiresAt = today.plusDays(USABLE_DAY);
+
         return new MemberCoupon(null, couponId, memberId, false, today, expiresAt);
+    }
+
+    public void use() {
+        validateAlreadyUsed();
+        validateUsableDate();
+
+        this.isUsed = true;
+    }
+
+    private void validateAlreadyUsed() {
+        if (isUsed) {
+            throw new IllegalStateException("이미 사용된 쿠폰입니다.");
+        }
+    }
+
+    private void validateUsableDate() {
+        LocalDateTime now = LocalDateTime.now();
+
+        if (now.isAfter(expiresAt)) {
+            throw new IllegalStateException("사용 기간이 지난 쿠폰입니다.");
+        }
     }
 }
--- src/test/java/coupon/domain/MemberCouponTest.java
+++ src/test/java/coupon/domain/MemberCouponTest.java
@@ -1,6 +1,7 @@
 package coupon.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
@@ -9,13 +10,46 @@ import org.junit.jupiter.api.Test;
 class MemberCouponTest {
 
     @Test
-    @DisplayName("쿠폰의 발급 기간은 발급일 포함 7일 동안 사용 가능하다.")
-    void validateDuration() {
+    @DisplayName("쿠폰을 사용할 수 있다.")
+    void use() {
         MemberCoupon memberCoupon = MemberCoupon.issue(1L, 1L);
-        LocalDateTime today = LocalDateTime.now();
-        LocalDateTime expiresAt = memberCoupon.getExpiresAt();
 
-        LocalDateTime expected = today.plusDays(7);
-        assertThat(expiresAt.getDayOfMonth()).isEqualTo(expected.getDayOfMonth());
+        memberCoupon.use();
+
+        assertThat(memberCoupon.getIsUsed()).isTrue();
+    }
+
+    @Test
+    @DisplayName("한 번 사용된 쿠폰은 다시 사용이 불가능하다.")
+    void alreadyUsed() {
+        MemberCoupon memberCoupon = MemberCoupon.issue(1L, 1L);
+        memberCoupon.use();
+
+        assertThatThrownBy(memberCoupon::use)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 사용된 쿠폰입니다.");
+    }
+
+    @Test
+    @DisplayName("쿠폰의 사용 기간이 지나면 쿠폰을 사용할 수 없다.")
+    void validateDuration() {
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime weekAgo = today.minusDays(7);
+        LocalDateTime expiresAt = weekAgo.plusDays(6);
+        MemberCoupon memberCoupon = new MemberCoupon(null, 1L, 1L, false, weekAgo, expiresAt);
+
+        assertThatThrownBy(memberCoupon::use)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("사용 기간이 지난 쿠폰입니다.");
+    }
+
+    @Test
+    @DisplayName("쿠폰의 사용 가능 기간은 발행일 포함 7일이다.")
+    void usableDuration() {
+        MemberCoupon memberCoupon = MemberCoupon.issue(1L, 1L);
+
+        LocalDateTime afterSixDay = LocalDateTime.now().plusDays(6);
+        int actual = memberCoupon.getExpiresAt().getDayOfMonth();
+        assertThat(actual).isEqualTo(afterSixDay.getDayOfMonth());
     }
 }
