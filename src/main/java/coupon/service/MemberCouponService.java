5bf2f4fbe7fb784bbe5eb5b091166325f1ab8b64
hangillee <skfcb10@naver.com>
2024-10-30 17:25:46 +0900

    refactor: MemberCoupon을 조회하는 로직과 개수를 세는 로직이 Writer DB를 바로 조회하도록 리팩터링

--- src/main/java/coupon/service/MemberCouponService.java
+++ src/main/java/coupon/service/MemberCouponService.java
@@ -2,10 +2,11 @@ package coupon.service;
 
 import coupon.domain.MemberCoupon;
 import coupon.dto.MemberCouponRequest;
-import coupon.service.reader.MemberCouponReaderService;
-import coupon.service.writer.MemberCouponWriterService;
+import coupon.repository.MemberCouponRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,24 +14,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class MemberCouponService {
 
+    private static final Logger log = LoggerFactory.getLogger(MemberCouponService.class);
     public static final int MAXIMUM_ISSUABLE_COUPONS_COUNT = 5;
-    private final MemberCouponReaderService readerService;
-    private final MemberCouponWriterService writerService;
 
-    @Transactional(readOnly = true)
-    public List<MemberCoupon> getIssuedMemberCoupons(Long memberId) {
-        return readerService.getIssuedMemberCoupon(memberId, () -> writerService.getIssuedCouponsWithWriter(memberId));
-    }
+    private final MemberCouponRepository memberCouponRepository;
 
     @Transactional
     public MemberCoupon issueCoupon(MemberCouponRequest memberCouponRequest) {
         validateIssuable(memberCouponRequest.memberId());
-        return writerService.create(memberCouponRequest.toMemberCoupon());
+        return memberCouponRepository.save(memberCouponRequest.toMemberCoupon());
     }
 
     private void validateIssuable(Long memberId) {
-        if (readerService.countMemberCoupon(memberId) > MAXIMUM_ISSUABLE_COUPONS_COUNT) {
+        int issuedCouponCount = memberCouponRepository.countByMemberId(memberId);
+        if (issuedCouponCount > MAXIMUM_ISSUABLE_COUPONS_COUNT) {
             throw new IllegalArgumentException("해당 사용자에게 발행할 수 있는 쿠폰의 최대 개수를 초과했습니다.");
         }
+    }
+
+    @Transactional
+    public List<MemberCoupon> getIssuedMemberCoupons(Long memberId) {
+        log.info("Find issued member coupons by member id: {}", memberId);
+
+        return memberCouponRepository.findAllByMemberId(memberId);
     }
 }
