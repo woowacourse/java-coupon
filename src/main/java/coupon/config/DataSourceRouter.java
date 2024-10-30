e731ab966250900d2b61107ef83dad158b90de6b
KIMGYUTAE <kiimwf0819@gmail.com>
2024-10-30 11:08:12 +0900

    refactor: DataSourceRouter log 추가

--- src/main/java/coupon/config/DataSourceRouter.java
+++ src/main/java/coupon/config/DataSourceRouter.java
@@ -1,21 +1,26 @@
 package coupon.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 public class DataSourceRouter extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            log.info("Use dataSource : READER");
             return DataSourceType.READER;
         }
 
         DataSourceType dataSource = DataSourceContextHolder.getDataSource();
         if (dataSource != null) {
+            log.info("Use dataSource : " + dataSource.name());
             return dataSource;
         }
 
+        log.info("Use dataSource : WRITER");
         return DataSourceType.WRITER;
     }
 }
