a3f92d626db2df53f736068103f1aeae03e5fd74
tackyu <tack970913@gmail.com>
2024-10-30 23:39:11 +0900

    refactor: 테스트 메서드명 의도 드러내도록 수정

--- src/test/java/coupon/coupon/service/CouponServiceTest.java
+++ src/test/java/coupon/coupon/service/CouponServiceTest.java
@@ -15,13 +15,13 @@ class CouponServiceTest {
     private CouponService couponService;
 
     @Test
-    void 복제지연테스트() throws InterruptedException {
+    void 복제지연테스트() {
         Coupon coupon = new Coupon(
                 "test",
                 1000,
                 10000,
                 null,
-                LocalDate.of(2024,1,1),
+                LocalDate.of(2024, 1, 1),
                 LocalDate.now());
         couponService.create(coupon);
         Coupon savedCoupon = couponService.getCoupon(coupon.getId());
