f4308b4480c16121cd64a9d94359776a26a0f82f
alstn113 <alstn113@gmail.com>
2024-10-30 20:36:45 +0900

    chore: 사용하지 않는 메서드 제거

--- src/main/java/coupon/membercoupon/application/MemberCouponMapper.java
+++ src/main/java/coupon/membercoupon/application/MemberCouponMapper.java
@@ -1,7 +1,5 @@
 package coupon.membercoupon.application;
 
-import java.util.List;
-import java.util.Map;
 import coupon.coupon.application.CouponResponse;
 import coupon.membercoupon.domain.MemberCoupon;
 import org.springframework.stereotype.Component;
@@ -9,19 +7,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class MemberCouponMapper {
 
-    public List<MemberCouponWithCouponResponse> toWithCouponResponses(
-            List<MemberCoupon> memberCoupons,
-            Map<Long, CouponResponse> couponMap
-    ) {
-        return memberCoupons.stream()
-                .map(memberCoupon -> {
-                    CouponResponse couponResponse = couponMap.get(memberCoupon.getCouponId());
-                    return toWithCouponResponse(memberCoupon, couponResponse);
-                })
-                .toList();
-    }
 
-    public MemberCouponWithCouponResponse toWithCouponResponse(MemberCoupon memberCoupon, CouponResponse couponResponse) {
+    public MemberCouponWithCouponResponse toWithCouponResponse(
+            MemberCoupon memberCoupon,
+            CouponResponse couponResponse
+    ) {
         return new MemberCouponWithCouponResponse(
                 memberCoupon.getId(),
                 memberCoupon.getCouponId(),
