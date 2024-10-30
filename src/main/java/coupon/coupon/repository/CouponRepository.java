1327c985bae58c61b740b6f61212b08bde2797cf
hyeonjilee <skylar12200@gmail.com>
2024-10-30 16:08:16 +0900

    feat: 쿠폰 조회에 캐시 적용

--- src/main/java/coupon/coupon/repository/CouponRepository.java
+++ src/main/java/coupon/coupon/repository/CouponRepository.java
@@ -1,9 +1,14 @@
 package coupon.coupon.repository;
 
 import coupon.coupon.domain.CouponEntity;
+import java.util.Optional;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CouponRepository extends JpaRepository<CouponEntity, Long> {
+
+    @Cacheable(value = "coupon")
+    Optional<CouponEntity> findById(long id);
 }
