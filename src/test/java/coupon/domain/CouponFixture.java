756139e118d24e2d7d6f7c6d866f8c52d44c7b4f
seunghye218 <seunghye@student.42seoul.kr>
2024-10-30 15:41:45 +0900

    refactor(CouponServiceTest): getMemberCoupons 테스트 추가
    - 테스트 설정 분리
    - CouponFixture 추가

--- src/test/java/coupon/domain/CouponFixture.java
+++ src/test/java/coupon/domain/CouponFixture.java
@@ -4,14 +4,29 @@ import coupon.domain.coupon.Category;
 import coupon.domain.coupon.Coupon;
 import java.time.LocalDate;
 
-public class CouponFixture {
+public enum CouponFixture {
 
-    public static final Coupon FOOD_COUPON = new Coupon(
-            "food coupon",
-            10000,
-            1000,
-            Category.FOOD,
-            LocalDate.now(),
-            LocalDate.now().plusDays(7)
-    );
+    FOOD_COUPON("food coupon", 10000, 1000, Category.FOOD, LocalDate.now(), LocalDate.now().plusDays(7)),
+    FASHION_COUPON("fashion coupon", 20000, 2000, Category.FASHION, LocalDate.now(), LocalDate.now().plusDays(7));
+
+    private final String name;
+    private final int minimumOrderAmount;
+    private final int discountAmount;
+    private final Category category;
+    private final LocalDate beginDate;
+    private final LocalDate endDate;
+
+    CouponFixture(String name, int minimumOrderAmount, int discountAmount, Category category, LocalDate beginDate,
+                  LocalDate endDate) {
+        this.name = name;
+        this.discountAmount = discountAmount;
+        this.minimumOrderAmount = minimumOrderAmount;
+        this.category = category;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+    }
+
+    public Coupon create() {
+        return new Coupon(name, minimumOrderAmount, discountAmount, category, beginDate, endDate);
+    }
 }
--- src/test/java/coupon/service/coupon/CouponServiceTest.java
+++ src/test/java/coupon/service/coupon/CouponServiceTest.java
@@ -1,26 +1,48 @@
 package coupon.service.coupon;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import coupon.domain.CouponFixture;
 import coupon.domain.coupon.Coupon;
 import coupon.domain.coupon.MemberCoupon;
 import coupon.exception.CouponException;
+import coupon.service.coupon.dto.MemberCouponsResponse;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 public class CouponServiceTest {
 
     @Autowired
     private CouponService couponService;
 
+    @Autowired
+    private RedisTemplate<String, Coupon> redisTemplate;
+
+    @Value("${coupon.cache.prefix}")
+    private String couponKeyPrefix;
+
+    @AfterEach
+    void tearDown() {
+        Set<String> keys = redisTemplate.keys(couponKeyPrefix + "*");
+        if (keys != null && !keys.isEmpty()) {
+            redisTemplate.delete(keys);
+        }
+    }
+
     @Test
     void replicationLagTest() {
-        Coupon coupon = CouponFixture.FOOD_COUPON;
+        Coupon coupon = CouponFixture.FOOD_COUPON.create();
 
         couponService.create(coupon);
 
@@ -31,7 +53,7 @@ public class CouponServiceTest {
     @DisplayName("한 회원에게 동일한 쿠폰을 5개 발급한다.")
     @Test
     void issue() {
-        Coupon coupon = CouponFixture.FOOD_COUPON;
+        Coupon coupon = CouponFixture.FOOD_COUPON.create();
         couponService.create(coupon);
 
         for (int i = 0; i < 5; i++) {
@@ -43,7 +65,7 @@ public class CouponServiceTest {
     @DisplayName("한 회원에게 동일한 쿠폰을 6개 이상 발급하면 예외가 발생한다.")
     @Test
     void issueFailed() {
-        Coupon coupon = CouponFixture.FOOD_COUPON;
+        Coupon coupon = CouponFixture.FOOD_COUPON.create();
         couponService.create(coupon);
         for (int i = 0; i < 5; i++) {
             couponService.issue(1L, coupon.getId());
@@ -51,5 +73,22 @@ public class CouponServiceTest {
 
         assertThatThrownBy(() -> couponService.issue(1L, coupon.getId()))
                 .isInstanceOf(CouponException.class);
+    }
+
+    @DisplayName("쿠폰 및 회원에게 발급된 쿠폰의 정보를 조회한다.")
+    @Test
+    void getMemberCoupons() {
+        Coupon foodCoupon = CouponFixture.FOOD_COUPON.create();
+        Coupon fashionCoupon = CouponFixture.FASHION_COUPON.create();
+        couponService.create(foodCoupon);
+        couponService.create(fashionCoupon);
+        Long memberId = 1L;
+        for (int i = 0; i < 5; i++) {
+            couponService.issue(memberId, foodCoupon.getId());
+            couponService.issue(memberId, fashionCoupon.getId());
+        }
+
+        List<MemberCouponsResponse> memberCoupons = couponService.getMemberCoupons(memberId);
+        assertThat(memberCoupons).hasSize(10);
     }
 }
