8490a980613f4598d8efafde2f38f46af101f55b
KIMGYUTAE <kiimwf0819@gmail.com>
2024-10-30 11:07:33 +0900

    refactor: spring cache 내부 캐싱 사용

--- src/main/java/coupon/config/CacheConfig.java
+++ src/main/java/coupon/config/CacheConfig.java
@@ -1,25 +1,17 @@
 package coupon.config;
 
-import java.time.Duration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofDays(1));
-
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(redisCacheConfiguration)
-                .build();
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager();
     }
 }
