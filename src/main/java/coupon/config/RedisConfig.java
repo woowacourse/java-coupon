7df2fe10332c1eeaf0daa0a6f44470bdfeb08dec
tsulocalize <tsulocalizing@gmail.com>
2024-10-31 18:02:54 +0900

    refactor: JedisPool -> RedisCacheManager

--- src/main/java/coupon/config/RedisConfig.java
+++ src/main/java/coupon/config/RedisConfig.java
@@ -2,21 +2,40 @@ package coupon.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import redis.clients.jedis.JedisPooled;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableCaching
 public class RedisConfig {
 
     private final RedisProperties redisProperties;
 
     @Bean
-    JedisPooled jedisPool() {
-        return new JedisPooled(
-                redisProperties.getHost(),
-                redisProperties.getPort()
-        );
+    RedisCacheManager redisCacheManager() {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableKeyPrefix()
+                .entryTtl(Duration.ofDays(1))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory())
+                .cacheDefaults(configuration)
+                .build();
+    }
+
+    private RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        connectionFactory.start();
+        return connectionFactory;
     }
 }
