ff9e1ea1b58ed31d0b0c52adc89bea2be8c482b0
tsulocalize <tsulocalizing@gmail.com>
2024-10-31 18:03:13 +0900

    refactor: 캐싱 조회 기능 수정

--- src/main/java/coupon/repository/CouponInMemoryRepository.java
+++ src/main/java/coupon/repository/CouponInMemoryRepository.java
@@ -2,43 +2,21 @@ package coupon.repository;
 
 import coupon.domain.Coupon;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
-import redis.clients.jedis.JedisPooled;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.NoSuchElementException;
 
 @Repository
 @RequiredArgsConstructor
 public class CouponInMemoryRepository {
 
-    private static final RedisSerializer<Coupon> serializer = new Jackson2JsonRedisSerializer<>(Coupon.class);
-
-    private final JedisPooled jedis;
     private final CouponRepository couponRepository;
 
+    @Transactional(readOnly = true)
+    @Cacheable(key = "#id", value = "coupon", cacheManager = "redisCacheManager")
     public Coupon getCoupon(Long id) {
-        String couponJson = jedis.get(String.valueOf(id));
-
-        if (couponJson == null) {
-            Coupon coupon = couponRepository.findById(id)
-                    .orElseThrow(() -> new NoSuchElementException("해당 쿠폰이 존재하지 않습니다. id = " + id));
-            jedis.set(String.valueOf(id), couponToJson(coupon));
-            return coupon;
-        }
-
-        return jsonToCoupon(couponJson);
-    }
-
-    protected void clearCache() {
-        jedis.flushDB();
-    }
-
-    private String couponToJson(Coupon coupon) {
-        return new String(serializer.serialize(coupon));
-    }
-
-    private Coupon jsonToCoupon(String couponJson) {
-        return serializer.deserialize(couponJson.getBytes());
+        return couponRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("해당 쿠폰이 존재하지 않습니다. id = " + id));
     }
 }
