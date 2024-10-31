29f32ae558471de4c377c1b957b1148afb32ecb8
haiseong <wjdgotjd9908@gmail.com>
2024-10-31 15:58:05 +0900

    refactor: 불필요한 메서드 정의 제거

--- src/main/java/coupon/repository/MemberRepository.java
+++ src/main/java/coupon/repository/MemberRepository.java
@@ -1,9 +1,7 @@
 package coupon.repository;
 
 import coupon.domain.Member;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findById(Long id);
 }
