fd8ea2cd137d1f8334e040833ba2c8622aedd64e
nayonsoso <panda0329@naver.com>
2024-10-31 17:55:04 +0900

    refactor: allArgsConstructor 제거
    - id 를 주입해서 entity 를 만들어줄 필요가 없으므로 반드시 필요한 값만 초기화하는 생성자 추가

--- src/main/java/coupon/mapper/DomainEntityMapper.java
+++ src/main/java/coupon/mapper/DomainEntityMapper.java
@@ -1,17 +1,12 @@
 package coupon.mapper;
 
 import coupon.domain.coupon.Coupon;
-import coupon.domain.user.User;
-import coupon.domain.usercoupon.UserCoupon;
 import coupon.entity.CouponEntity;
-import coupon.entity.UserCouponEntity;
-import coupon.entity.UserEntity;
 
 public class DomainEntityMapper {
 
     public static CouponEntity mapToCouponEntity(Coupon coupon) {
         return new CouponEntity(
-                coupon.getId(),
                 coupon.getName().getName(),
                 coupon.getDiscountAmount().getDiscountAmount(),
                 coupon.getMinOrderAmount().getMinOrderAmount(),
