80e119a11cdbcc01480a0a224c8f6f0c8969cd73
mzeong <mjeong@sookmyung.ac.kr>
2024-10-31 17:22:09 +0900

    feat: 회원 쿠폰 발급 동시 요청 시 개수 제한 처리

--- src/main/java/coupon/config/RedisConfig.java
+++ src/main/java/coupon/config/RedisConfig.java
@@ -12,6 +12,7 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -29,6 +30,15 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, ?> redisTemplate() {
+        RedisTemplate<String, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(mapper()));
+        return redisTemplate;
     }
 
     @Bean
--- src/main/java/coupon/service/MemberCouponService.java
+++ src/main/java/coupon/service/MemberCouponService.java
@@ -6,7 +6,9 @@ import coupon.dto.response.FindMemberCouponResponse;
 import coupon.repository.MemberCouponRepository;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,24 +16,22 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberCouponService {
 
+    private static final String MEMBER_COUPON_COUNT_CACHE_FORMAT = "member-coupons-count::%d::%d";
+    private static final int CACHE_EXPIRATION_MINUTES = 10;
     private static final int ISSUANCE_LIMIT = 5;
 
+    private final RedisTemplate<String, Integer> redisTemplate;
     private final ReadCouponService readCouponService;
     private final MemberCouponRepository memberCouponRepository;
 
+
     @Transactional
     public void save(long couponId, long memberId) {
-        MemberCoupon memberCoupon = new MemberCoupon(couponId, memberId);
         validateIssuanceLimit(couponId, memberId);
         validateIssuancePeriod(couponId);
-        memberCouponRepository.save(memberCoupon);
-    }
 
-    private void validateIssuanceLimit(long couponId, long memberId) {
-        int issuanceCount = memberCouponRepository.countByCouponIdAndMemberId(couponId, memberId);
-        if (issuanceCount >= ISSUANCE_LIMIT) {
-            throw new IllegalArgumentException("회원은 동일한 쿠폰을 최대 " + ISSUANCE_LIMIT + "장까지 발급할 수 있습니다.");
-        }
+        MemberCoupon memberCoupon = new MemberCoupon(couponId, memberId);
+        memberCouponRepository.save(memberCoupon);
     }
 
     private void validateIssuancePeriod(long couponId) {
@@ -42,6 +42,24 @@ public class MemberCouponService {
 
         if (now.isBefore(startAt) || now.isAfter(endAt)) {
             throw new IllegalArgumentException("해당 쿠폰은 " + startAt + "부터 " + endAt + "까지 발급할 수 있습니다.");
+        }
+    }
+
+    private void validateIssuanceLimit(long couponId, long memberId) {
+        String key = MEMBER_COUPON_COUNT_CACHE_FORMAT.formatted(couponId, memberId);
+        Integer issuanceCount = redisTemplate.opsForValue().get(key);
+        if (issuanceCount == null) {
+            issuanceCount = memberCouponRepository.countByCouponIdAndMemberId(couponId, memberId);
+            redisTemplate.opsForValue().set(key, issuanceCount, CACHE_EXPIRATION_MINUTES, TimeUnit.MINUTES);
+        }
+
+        Long updatedIssuanceCount = redisTemplate.opsForValue().increment(key);
+        if (updatedIssuanceCount == null) {
+            throw new IllegalStateException("회원 쿠폰 발급을 위한 Redis 연산 중 문제가 발생했습니다.");
+        }
+        if (updatedIssuanceCount > ISSUANCE_LIMIT) {
+            redisTemplate.opsForValue().decrement(key);
+            throw new IllegalArgumentException("회원은 동일한 쿠폰을 최대 " + ISSUANCE_LIMIT + "장까지 발급할 수 있습니다.");
         }
     }
 
