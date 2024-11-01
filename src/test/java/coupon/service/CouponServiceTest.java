50a941f31bb7a998d6659cc9d8fc467d7acff7c6
zangsu <zangsu_@naver.com>
2024-11-01 09:17:08 +0900

    refactor: 복제지연 문제 해결

--- src/test/java/coupon/service/CouponServiceTest.java
+++ src/test/java/coupon/service/CouponServiceTest.java
@@ -8,7 +8,9 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("writer")
 @SpringBootTest
 public class CouponServiceTest {
 
