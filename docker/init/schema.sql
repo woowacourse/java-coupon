c8229ee308c2023b35f54b19778b383e42739516
linirini <2001yerin@naver.com>
2024-11-01 11:28:33 +0900

    refactor: 스키마 타입 수정

--- docker/init/schema.sql
+++ docker/init/schema.sql
@@ -14,8 +14,8 @@ create table if not exists coupon
     discount_amount      DECIMAL(10, 2)                                           NOT NULL,
     minimum_order_amount DECIMAL(10, 2)                                           NOT NULL,
     category             ENUM ('FASHION', 'HOME_APPLIANCES', 'FURNITURE', 'FOOD') NOT NULL,
-    start_at             TIMESTAMP                                                NOT NULL,
-    end_at               TIMESTAMP                                                NOT NULL
+    start_at             DATETIME(6)                                              NOT NULL,
+    end_at               DATETIME(6)                                              NOT NULL
 );
 
 create table if not exists member_coupon
@@ -24,6 +24,6 @@ create table if not exists member_coupon
     coupon_id  BIGINT    NOT NULL,
     member_id  BIGINT    NOT NULL,
     used       BOOLEAN   NOT NULL,
-    issued_at  TIMESTAMP NOT NULL,
-    expired_at TIMESTAMP NOT NULL
+    issued_at  DATETIME(6) NOT NULL,
+    expired_at DATETIME(6) NOT NULL
 );
