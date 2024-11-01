a6a2bead3374ffa461631428f59380b0efaff2a8
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-11-01 16:35:17 +0900

    feat(/domain): EqualsAndHashCode 구

--- src/main/java/coupon/membercoupon/domain/MemberCoupon.java
+++ src/main/java/coupon/membercoupon/domain/MemberCoupon.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import coupon.infrastructure.audit.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,6 +19,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id", callSuper = false)
 public class MemberCoupon extends BaseTimeEntity {
 
     @Id
