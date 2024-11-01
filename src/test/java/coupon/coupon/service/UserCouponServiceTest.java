22b38bb6b52baf6bab603a1df3dfab7f8b0c2cba
tackyu <tack970913@gmail.com>
2024-11-01 13:59:15 +0900

    test: 회원 쿠폰 목록 조회 기능 테스트

--- src/test/java/coupon/coupon/service/UserCouponServiceTest.java
+++ src/test/java/coupon/coupon/service/UserCouponServiceTest.java
@@ -1,14 +1,20 @@
 package coupon.coupon.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import coupon.coupon.UserCouponResponse;
+import coupon.coupon.domain.Category;
 import coupon.coupon.domain.Coupon;
+import coupon.coupon.domain.UserCoupon;
 import coupon.coupon.repository.UserCouponRepository;
 import coupon.user.domain.User;
+import java.time.LocalDate;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +24,8 @@ class UserCouponServiceTest {
 
     private UserCouponService userCouponService;
 
+    private CouponService couponService;
+
     private User user;
 
     private Coupon coupon;
@@ -25,7 +33,8 @@ class UserCouponServiceTest {
     @BeforeEach
     void setUp() {
         userCouponRepository = mock(UserCouponRepository.class);
-        userCouponService = new UserCouponService(userCouponRepository);
+        couponService = mock(CouponService.class);
+        userCouponService = new UserCouponService(userCouponRepository, couponService);
         user = mock(User.class);
         coupon = mock(Coupon.class);
     }
@@ -45,5 +54,24 @@ class UserCouponServiceTest {
                 .thenReturn(4);
 
         assertDoesNotThrow(() -> userCouponService.issue(coupon, user));
+    }
+
+    @Test
+    void getUserCouponInfo() {
+        Coupon coupon1 = new Coupon("test", 1000, 10000, Category.FASHION, LocalDate.now(), LocalDate.now());
+        Coupon coupon2 = new Coupon("test2", 1000, 10000, Category.FASHION, LocalDate.now(), LocalDate.now());
+        UserCoupon userCoupon1 = new UserCoupon(1L, user, false, LocalDate.now());
+        UserCoupon userCoupon2 = new UserCoupon(2L, user, false, LocalDate.now());
+
+        List<UserCoupon> userCoupons = List.of(userCoupon1, userCoupon2);
+        when(userCouponRepository.findByUser(user)).thenReturn(userCoupons);
+        when(couponService.getCoupon(1L)).thenReturn(coupon1);
+        when(couponService.getCoupon(2L)).thenReturn(coupon2);
+
+        List<UserCouponResponse> responses = userCouponService.getUserCouponInfo(user);
+        UserCouponResponse actual1 = UserCouponResponse.of(userCoupon1, coupon1);
+        UserCouponResponse actual2 = UserCouponResponse.of(userCoupon2, coupon2);
+
+        assertThat(responses).contains(actual1, actual2);
     }
 }
